405843c694123305a84e204ceae5f18bea21c798
Thiago Teixeira <thiago@streamlit.io>
2020-11-02 22:54:01 -0800

    Fix typo!

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,8 @@ import streamlit as st
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
-If you have any questions, checkout out our [documentation](https://docs.streamlit.io) and
-[community forums](https://discuss.streamlit.io).
+If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
