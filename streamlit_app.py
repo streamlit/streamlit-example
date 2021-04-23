48efdecca5803418071c5fb2e4f436b0f149c779
ilaydayildirimm <64201166+ilaydayildirimm@users.noreply.github.com>
2021-04-24 02:43:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,6 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
-pip install streamlit
 streamlit hello
 
 with st.echo(code_location='below'):
