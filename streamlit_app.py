fee4b5c1286ebac7f227c281116bdeaa5bfbb1e9
ilaydayildirimm <64201166+ilaydayildirimm@users.noreply.github.com>
2021-04-24 18:59:45 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,1 @@
-import pandas as pd # used to load, manipulate the data and for one-hot encoding
-import numpy as np # data manipulation
-import seaborn as sns 
-import matplotlib.pyplot as plt
-import matplotlib.pyplot as plt
-import matplotlib.colors as colors
-from sklearn.utils import resample # for downsample the dataset
-from sklearn.model_selection import train_test_split # for splitting the dataset into train and test split
-from sklearn.preprocessing import scale # scale and center the data
-from sklearn.svm import SVC # will make a SVM for classification
-from sklearn.model_selection import GridSearchCV # will do the cross validation
-from sklearn.metrics import plot_confusion_matrix # will draw the confusion matrix
-from sklearn.decomposition import PCA # to perform PCA to plot the data
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from sklearn.model_selection import cross_val_score
-from sklearn.metrics import confusion_matrix, precision_score, accuracy_score, recall_score, roc_curve, auc
-import seaborn as sns
 print("İlayda")
