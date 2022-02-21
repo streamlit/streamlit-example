2c8d4c626c75dca3284b0e60850245aa16ff4c70
KilianGir <97527152+KilianGir@users.noreply.github.com>
2022-02-21 13:41:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
-from collections import namedtuple
-import altair as alt
-import math
+import fastf1 as ff1
+from fastf1 import plotting
+from matplotlib import pyplot as plt
+from matplotlib.pyplot import figure
+from matplotlib.collections import LineCollection
+from matplotlib import cm
+import numpy as np
 import pandas as pd
 import streamlit as st
 
@@ -15,24 +19,135 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+# Setup plotting
+plotting.setup_mpl()
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# Enable the cache
+ff1.Cache.enable_cache('cache') 
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+# Get rid of some pandas warnings that are not relevant for us at the moment
+pd.options.mode.chained_assignment = None
 
-    points_per_turn = total_points / num_turns
+# Load the session data
+race = ff1.get_session(2021, 'Russia', 'R')
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+# Get the laps
+laps = race.load_laps(with_telemetry=True)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Calculate RaceLapNumber (LapNumber minus 1 since the warmup lap is included in LapNumber)
+laps['RaceLapNumber'] = laps['LapNumber'] - 1
+
+# Starting from lap 45 it started raining
+laps = laps.loc[laps['RaceLapNumber'] >= 45]
+
+# Get all drivers
+drivers = pd.unique(laps['Driver'])
+
+telemetry = pd.DataFrame()
+
+# Telemetry can only be retrieved driver-by-driver
+for driver in drivers:
+    driver_laps = laps.pick_driver(driver)
+    
+    # Since we want to compare distances, we need to collect telemetry lap-by-lap to reset the distance
+    for lap in driver_laps.iterlaps():
+        driver_telemetry = lap[1].get_telemetry().add_distance()
+        driver_telemetry['Driver'] = driver
+        driver_telemetry['Lap'] = lap[1]['RaceLapNumber']
+        driver_telemetry['Compound'] = lap[1]['Compound']
+    
+        telemetry = telemetry.append(driver_telemetry)
+
+# Only keep required columns
+telemetry = telemetry[['Lap', 'Distance', 'Compound', 'Speed', 'X','Y']]
+
+# Everything that's not intermediate will be "slick"
+telemetry['Compound'].loc[telemetry['Compound'] != 'INTERMEDIATE'] = 'SLICK'        
+        
+# We want 25 mini-sectors
+num_minisectors = 30
+
+# What is the total distance of a lap?
+total_distance = max(telemetry['Distance'])
+
+# Generate equally sized mini-sectors 
+minisector_length = total_distance / num_minisectors
+
+minisectors = [0]
+
+for i in range(0, (num_minisectors - 1)):
+    minisectors.append(minisector_length * (i + 1))
+    
+# Assign minisector to every row in the telemetry data
+telemetry['Minisector'] =  telemetry['Distance'].apply(
+  lambda z: (
+    minisectors.index(
+      min(minisectors, key=lambda x: abs(x-z)))+1
+  )
+)
+
+# Calculate fastest tyre per mini sector
+average_speed = telemetry.groupby(['Lap', 'Minisector', 'Compound'])['Speed'].mean().reset_index()
+
+# Select the compound with the highest average speed
+fastest_compounds = average_speed.loc[average_speed.groupby(['Lap', 'Minisector'])['Speed'].idxmax()]
+
+
+# Get rid of the speed column and rename the Compound column
+fastest_compounds = fastest_compounds[['Lap', 'Minisector', 'Compound']].rename(columns={'Compound': 'Fastest_compound'})
+    
+# Join the fastest compound per minisector with the full telemetry
+telemetry = telemetry.merge(fastest_compounds, on=['Lap', 'Minisector'])
+
+# Order the data by distance to make matploblib does not get confused
+telemetry = telemetry.sort_values(by=['Distance'])
+
+# Assign integer value to the compound because that's what matplotlib wants
+telemetry.loc[telemetry['Fastest_compound'] == "INTERMEDIATE", 'Fastest_compound_int'] = 1
+telemetry.loc[telemetry['Fastest_compound'] == "SLICK", 'Fastest_compound_int'] = 2
+
+#telemetry.loc[telemetry['Lap'] == 48]    
+    
+def generate_minisector_plot(lap, save=False, details=True):
+    single_lap = telemetry.loc[telemetry['Lap'] == lap]
+
+    x = np.array(single_lap['X'].values)
+    y = np.array(single_lap['Y'].values)
+
+    points = np.array([x, y]).T.reshape(-1, 1, 2)
+    segments = np.concatenate([points[:-1], points[1:]], axis=1)
+    compound = single_lap['Fastest_compound_int'].to_numpy().astype(float)
+    
+    
+    cmap = cm.get_cmap('rainbow', 2)
+    lc_comp = LineCollection(segments, norm=plt.Normalize(1, cmap.N+1), cmap=cmap)
+    lc_comp.set_array(compound)
+    lc_comp.set_linewidth(2)
+
+    st.plt.rcParams['figure.figsize'] = [12, 5]
+    
+    if details:
+        title = st.plt.suptitle(
+            f"2021 Russian GP \n Lap {lap} - Slicks vs. Inters"
+        )
+        
+    st.plt.gca().add_collection(lc_comp)
+    st.plt.axis('equal')
+    st.plt.tick_params(labelleft=False, left=False, labelbottom=False, bottom=False)
+    
+    if details:
+        cbar = st.plt.colorbar(mappable=lc_comp, boundaries=np.arange(1, 4))
+        cbar.set_ticks(np.arange(1.5, 9.5))
+        cbar.set_ticklabels(['Inters', 'Slicks'])
+    
+    if save:
+        st.plt.savefig(f"img/minisectors_lap_{lap}.png", dpi=300)
+
+    st.plt.show()
+    
+    
+# Plotting
+
+lapChosen = st.slider("Choose lap", 1, 50)
+
+generate_minisector_plot(lapChosen, save=True, details=True)
