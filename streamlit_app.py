4ce45c071b55d738cfaf39734e99c9e9eb367709
pedro-pauletti <paulettipedrohenrique@gmail.com>
2022-03-10 23:17:22 -0300

    change tiltle

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,8 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# Valor Caronas
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
 gas_price = st.text_input('Valor Combustível', '')
