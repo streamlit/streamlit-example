157552dad9e94d27d6cba226f64d1b0f385b3fde
pedro-pauletti <paulettipedrohenrique@gmail.com>
2022-03-10 23:32:45 -0300

    change rides inputs

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,12 +18,12 @@ carlos_rides_count = []
 ### Caronas Pedro
 
 """
-pedro_rides_count[0] = st.number_input('GABRIEL', min_value=0, step=int)
-pedro_rides_count[1] = st.number_input('GIOVANNA', min_value=0, step=int)
-pedro_rides_count[2] = st.number_input('GIOVANA', min_value=0, step=int)
-pedro_rides_count[3] = st.number_input('LUCAS', min_value=0, step=int)
-pedro_rides_count[4] = st.number_input('LEO', min_value=0, step=int)
-pedro_rides_count[5] = st.number_input('CARLOS', min_value=0, step=int)
+pedro_rides_count[0] = st.number_input('GABRIEL',  step=int)
+pedro_rides_count[1] = st.number_input('GIOVANNA', step=int)
+pedro_rides_count[2] = st.number_input('GIOVANA', step=int)
+pedro_rides_count[3] = st.number_input('LUCAS',  step=int)
+pedro_rides_count[4] = st.number_input('LEO', step=int)
+pedro_rides_count[5] = st.number_input('CARLOS', step=int)
 
 
 """
@@ -31,8 +31,8 @@ pedro_rides_count[5] = st.number_input('CARLOS', min_value=0, step=int)
 
 """
 
-carlos_rides_count[0] = st.number_input('GABRIEL', min_value=0, step=int)
-carlos_rides_count[1] = st.number_input('PEDRO', min_value=0, step=int)
-carlos_rides_count[3] = st.number_input('LUCAS', min_value=0, step=int)
-carlos_rides_count[4] = st.number_input('LEO', min_value=0, step=int)
-carlos_rides_count[5] = st.number_input('CARLOS', min_value=0, step=int)
+carlos_rides_count[0] = st.number_input('GABRIEL', step=int)
+carlos_rides_count[1] = st.number_input('PEDRO',  step=int)
+carlos_rides_count[3] = st.number_input('LUCAS', step=int)
+carlos_rides_count[4] = st.number_input('LEO',  step=int)
+carlos_rides_count[5] = st.number_input('CARLOS', step=int)
