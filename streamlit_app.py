b1b1ec45af1c0ddce339406339ecf82b51eb6744
pedro-pauletti <paulettipedrohenrique@gmail.com>
2022-03-10 23:29:31 -0300

    add rides input

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,30 @@ import streamlit as st
 
 """
 
-gas_price = st.text_input('Valor Combustível', '')
+gas_price = st.number_input('Valor Combustível', min_value=0, step=float)
 
+pedro_rides_count = []
+carlos_rides_count = []
+
+"""
+### Caronas Pedro
+
+"""
+pedro_rides_count[0] = st.number_input('GABRIEL', min_value=0, step=int)
+pedro_rides_count[1] = st.number_input('GIOVANNA', min_value=0, step=int)
+pedro_rides_count[2] = st.number_input('GIOVANA', min_value=0, step=int)
+pedro_rides_count[3] = st.number_input('LUCAS', min_value=0, step=int)
+pedro_rides_count[4] = st.number_input('LEO', min_value=0, step=int)
+pedro_rides_count[5] = st.number_input('CARLOS', min_value=0, step=int)
+
+
+"""
+### Caronas Carlos
+
+"""
+
+carlos_rides_count[0] = st.number_input('GABRIEL', min_value=0, step=int)
+carlos_rides_count[1] = st.number_input('PEDRO', min_value=0, step=int)
+carlos_rides_count[3] = st.number_input('LUCAS', min_value=0, step=int)
+carlos_rides_count[4] = st.number_input('LEO', min_value=0, step=int)
+carlos_rides_count[5] = st.number_input('CARLOS', min_value=0, step=int)
