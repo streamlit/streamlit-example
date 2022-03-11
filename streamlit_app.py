02ba3e6659fa5cd7877cc16f5e77d4ce5af42d2c
pedro-pauletti <paulettipedrohenrique@gmail.com>
2022-03-10 23:46:39 -0300

    add writes and columns

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,28 +11,38 @@ import streamlit as st
 
 gas_price = st.number_input('Valor Combustível')
 
-pedro_rides_count = [None] * 5
-carlos_rides_count = [None] * 5
+pedro_rides_count = [None] * 6
+carlos_rides_count = [None] * 6
 
-"""
-### Caronas Pedro
+col1, col2 = st.columns(2)
 
-"""
-pedro_rides_count[0] = st.number_input('GABRIEL', min_value=0, step=1)
-pedro_rides_count[1] = st.number_input('GIOVANNA', min_value=0, step=1)
-pedro_rides_count[2] = st.number_input('GIOVANA', min_value=0, step=1)
-pedro_rides_count[3] = st.number_input('LUCAS', min_value=0, step=1)
-pedro_rides_count[4] = st.number_input('LEO', min_value=0, step=1)
-pedro_rides_count[5] = st.number_input('CARLOS', min_value=0, step=1)
+with col1:
+    st.header("Caronas Pedro")
+    pedro_rides_count[0] = st.number_input('GABRIEL', min_value=0, step=1)
+    st.write("Valor a pagar = ")
+    pedro_rides_count[1] = st.number_input('GIOVANNA', min_value=0, step=1)
+    st.write("Valor a pagar = ")
+    pedro_rides_count[2] = st.number_input('GIOVANA', min_value=0, step=1)
+    st.write("Valor a pagar = ")
+    pedro_rides_count[3] = st.number_input('LUCAS', min_value=0, step=1)
+    st.write("Valor a pagar = ")
+    pedro_rides_count[4] = st.number_input('LEO', min_value=0, step=1)
+    st.write("Valor a pagar = ")
+    pedro_rides_count[5] = st.number_input('CARLOS', min_value=0, step=1)
+    st.write("Valor a pagar = ")
+
+with col2:
+    set.header("Caronas Carlos")
+    carlos_rides_count[0] = st.number_input('GABRIEL', min_value=0, step=1)
+    st.write("Valor a pagar = ")
+    carlos_rides_count[1] = st.number_input('PEDRO', min_value=0, step=1)
+    st.write("Valor a pagar = ")
+    carlos_rides_count[3] = st.number_input('LUCAS', min_value=0, step=1)
+    st.write("Valor a pagar = ")
+    carlos_rides_count[4] = st.number_input('LEO', min_value=0, step=1)
+    st.write("Valor a pagar = ")
+    carlos_rides_count[5] = st.number_input('CARLOS', min_value=0, step=1)
+    st.write("Valor a pagar = ")
 
 
-"""
-### Caronas Carlos
-
-"""
-
-carlos_rides_count[0] = st.number_input('GABRIEL', min_value=0, step=1)
-carlos_rides_count[1] = st.number_input('PEDRO', min_value=0, step=1)
-carlos_rides_count[3] = st.number_input('LUCAS', min_value=0, step=1)
-carlos_rides_count[4] = st.number_input('LEO', min_value=0, step=1)
-carlos_rides_count[5] = st.number_input('CARLOS', min_value=0, step=1)
+st.button('SALVAR')
