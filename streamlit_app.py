7ff1665a1a9f103ff5d957f5580239d1a5be80e0
charlottecacheux <charlotte.cacheux@student-cs.fr>
2022-04-13 11:46:59 -0700

    my first american commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,7 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+# General visualition
 """
 
 
