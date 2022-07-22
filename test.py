9c864af7d36637c9aa706a43a29c44dd709ad807
amelievert <99830156+amelievert@users.noreply.github.com>
2022-07-22 15:59:25 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 
 import streamlit as st
 
-st.write("Ce jeu de données est composé de données personnelles sur des clients d’une banque qui ont été “télémarketés” p")
+st.write("Ce jeu de données")
