79556db3b504c37f95b21dd38b4118c5a266966f
amelievert <99830156+amelievert@users.noreply.github.com>
2022-07-22 15:55:25 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+
+import streamlit as st
+
+st.markdown("Ce jeu de données est composé de données personnelles sur des clients d’une banque qui ont été “télémarketés” pour souscrire à un produit que l’on appelle un 'dépôt à terme'.\n"
+            "Lorsqu’un client souscrit à ce produit, il place une quantité d’argent dans un compte spécifique et ne pourra pas toucher ces fonds avant l’expiration du terme.\n"
+            "En échange, le client reçoit des intérêts de la part de la banque à la fin du terme.\n"
+            "Le jeu de données est téléchargeable au lien suivant :\n"
+            "https://www.kaggle.com/janiobachmann/bank-marketing-dataset")
