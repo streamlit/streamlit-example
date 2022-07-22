8ec71a9232b29150342959dbdd7facd90775fc0b
amelievert <99830156+amelievert@users.noreply.github.com>
2022-07-22 09:42:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ import pandas as pd
 #import seaborn as sns
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import plotly.express as px
 
 from sklearn.model_selection import train_test_split
