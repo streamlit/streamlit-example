2135ed6fb288f6853bc72b88cbfe1fecd8afca16
amelievert <99830156+amelievert@users.noreply.github.com>
2022-07-27 10:28:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -596,7 +596,7 @@ if page==pages[3]:
   fig.add_trace(go.Scatter(x=fpr_dtc, y=tpr_dtc , mode='lines', name='Modèle DTC (auc = %0.2f)' % roc_auc_dtc))
   fig.add_trace(go.Scatter(x=fpr_rfc, y=tpr_rfc , mode='lines', name='Modèle RFC (auc = %0.2f)' % roc_auc_rfc))
   fig.add_trace(go.Scatter(x=[0, 1], y=[0, 1], name='Aléatoire (auc = 0.5)', line = dict(color='black', width=2, dash='dot')))
-  fig.update_layout(height=400, width=500, legend=dict(yanchor="top", y=0.9, xanchor="left", x=0.4))
+  fig.update_layout(height=500, width=800, legend=dict(yanchor="top", y=0.1, xanchor="left", x=0.5))
   tab2.plotly_chart(fig)          
          
   with tab2.expander("Plus d'explication sur ce graphique :"):
@@ -705,6 +705,7 @@ if page==pages[4]:
     col5.write(classifieur)
 
     y_pred = classifieur.predict(feats_modif_x)
+    col5.write(y_pred)    
     probas=pd.DataFrame(y_pred, columns=['Prédictions'], index=feats_modif_x.index)
     col5.write(probas)         
          
@@ -712,6 +713,6 @@ if page==pages[4]:
     #probas = pd.DataFrame(probas,columns = ['Prédictions', 'Total']).reset_index()  
     #col5.write(probas)
          
-    pie = px.pie(probas, values=probas.value_counts(), names="Prédictions", hole=.3, title='Répartition des prédictions')
-    col5.plotly_chart(pie)     
+    #pie = px.pie(probas, values=probas.value_counts(), names="Prédictions", hole=.3, title='Répartition des prédictions')
+    #col5.plotly_chart(pie)     
 
