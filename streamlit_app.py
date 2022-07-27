eda344b352eb2f0509984375a96930cbde6e3681
amelievert <99830156+amelievert@users.noreply.github.com>
2022-07-27 15:59:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -733,9 +733,10 @@ if page==pages[5]:
     col11.write(" ") 
     col11.write(" ") 
     col11.subheader("Chiffres clés")
-    col11.metric("Performance de la campagne *", "70 °F", "1.2 °F")
+    col11.metric("Performance présumée de la campagne *", "70 °F", "1.2 °F")
     col11.metric("Nombre de clients scorés positifs", "9 mph", "-8%")
-    col11.metric("Précison de la prédiction", "86%", "4%")
+    col11.metric("Score du modèle **", "86%", "4%")
          
     st.write(" ") 
-    st.write("* Performance : Pourcentage estimé de clients susceptibles d'effectuer un dépôt lors de la campagne.") 
+    st.write("*Performance : Pourcentage estimé de clients susceptibles d'effectuer un dépôt lors de la campagne.") 
+    st.write("*Score du modèle : Mesure du taux de prédictions correctes effectuées par le modèle utilisé. Le modèle Random Forest est utilisé comme référence.") 
