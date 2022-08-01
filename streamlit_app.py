277de06aae5a4913ab14aee952fad52244ab8547
MariusHofgaard <mariushofgaard@gmail.com>
2022-08-01 12:58:07 +0200

    altered elements pre-planning

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,50 @@ def check_password():
         # Password correct.
         return True
 
+
+
+def main_app():
+    st.subheader('Overview of active rules')
+
+    with st.expander("Overview of product scoring rules"):
+        cols = st.columns(4)
+        cols[0].write(f'**Layer name**')
+        cols[1].write(f'**Rule Type**')
+        cols[2].write(f'**Min Value**')
+        cols[3].write(f'**Max Value**')
+
+        for filename in filenames: 
+
+            
+
+
+
+            if activated_layers_dict[filename]['active']:
+
+                cols = st.columns(4)
+                cols[0].write(f'{filename}')
+                cols[1].write(f'{ activated_layers_dict[filename]["type of analysis"] }')
+
+
+                # st.write('For: ', activated_layers_dict[filename]['name'])
+                # st.write('Type of analysis selected: ', activated_layers_dict[filename]['type of analysis'])
+
+                for key,value in activated_layers_dict[filename]['parameters'].items():
+                    # st.write(key, value)
+                    if key == "min_val":
+                        cols[2].write(f'{value}')
+
+                    elif key == 'max_val':
+                        cols[3].write(f'{value}')
+
+    return None 
+
+
+
+
+
 if check_password():
-    st.write("Here goes your normal Streamlit app...")
-    st.button("Click me")
+
+
+    st.write("Here goes your normal Streamlit app...")'
+    main_app()
