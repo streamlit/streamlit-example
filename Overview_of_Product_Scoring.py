a667d51af649cd1639141cca586f3066f9ebf7e3
MariusHofgaard <mariushofgaard@gmail.com>
2022-08-01 19:51:36 +0200

    rem

--- Overview_of_Product_Scoring.py
+++ Overview_of_Product_Scoring.py
@@ -99,14 +99,6 @@ def main_app():
         col3.metric("Humidity", "86%", "4%")
         col4.metric("Humidity", "86%", "4%")
 
-        # col1, col2, col3 = st.columns(4)
-        col1.metric("Temperature", "70 °F", "1.2 °F")
-        col2.metric("Wind", "9 mph", "-8%")
-        col3.metric("Humidity", "86%", "4%")
-        col4.metric("Humidity", "86%", "4%")
-
-        
-
         st.write("---")
     
 
