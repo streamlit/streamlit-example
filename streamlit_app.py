755f0eea1a203a387073a546e874e46b0713e8e8
amelievert <99830156+amelievert@users.noreply.github.com>
2022-08-04 12:26:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,8 +56,9 @@ rlc = joblib.load('Regression logistique.joblib')
 knn = joblib.load('K plus proches voisins.joblib')
 dtc = joblib.load('Decision Tree Classifier.joblib')
 rfc = joblib.load('Random Forest Classifier.joblib')
-xgbc = xgb.XGBClassifier()
-xgbc.load_model("XG Boost Classifier.json")
+#xgbc = xgb.XGBClassifier()
+#xgbc.load_model("XG Boost Classifier.json")
+#voir ligne 135
 
 compare = pd.read_csv('compare_scores.csv', sep = ',')
 
@@ -130,6 +131,11 @@ X_train, X_test, y_train, y_test = train_test_split(feats, target, test_size=0.2
 scaler = MinMaxScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
+
+# XGBoost
+xgbc = xgb.XGBClassifier(max_depth=12,subsample=0.33,objective='binary:logistic',n_estimators=300,learning_rate = 0.01)
+eval_set = [(X_train, y_train), (X_test, y_test)]
+xgbc.fit(X_train, y_train.values.ravel(), early_stopping_rounds=15, eval_metric=["error", "logloss"], eval_set=eval_set, verbose=False)
 
 # Regression logistique
 rlc_y_pred = rlc.predict(X_test)
