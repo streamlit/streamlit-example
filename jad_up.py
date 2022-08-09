803d5410f8a6d6c8d198e5c2d3ad677f40b8d882
amelievert <99830156+amelievert@users.noreply.github.com>
2022-08-09 17:06:36 +0200

    Update jad_up.py

--- jad_up.py
+++ jad_up.py
@@ -52,7 +52,7 @@ rlc = load('Regression logistique.joblib')
 knn = load('K plus proches voisins.joblib')
 dtc = load('Decision Tree Classifier.joblib')
 rfc = load('Random Forest Classifier.joblib')
-#xgbc = load('Random Forest Classifier.joblib')
+#xgbc = load('XG Boost Classifier.joblib')
 xgbc = rfc
 compare = pd.read_csv('compare_scores.csv', sep = ',')
 
