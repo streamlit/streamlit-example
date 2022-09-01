bddd414a190fd7b7a1a19f60408b3921d50be61f
vytenisd <vytenisd+datame@gmail.com>
2022-09-01 12:05:41 +0300

    Testing html button

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Welcome to Streamlit!
+# Welcome to Spike!
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+If you have any questions, checkout Streamlit [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,9 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
+
+CLIENT_ID = st.secrets["CLIENT_ID"]
+
 """
 # Welcome to Spike Demo App!
 
@@ -41,6 +44,22 @@ end_time = st.sidebar.slider(
     max_value=datetime(2022, 7, 31, 0, 0),
     format="YYYY/MM/DD")
 st.write("Start time:", start_time, "End time:", end_time)
+
+st.sidebar.markdown('<style>.btn {font-family: "Poppins", Sans-serif;font-size: 12px;font-weight: 500;border-radius: 50px 50px 50px 50px;box-shadow: 0px 4px 10px -1px rgb(0 0 0 / 27%);padding: 14px 35px 14px 35px;width: 130px;border: unset;color: #f2f5f8;background-color: #1b2836;}</style>',
+    unsafe_allow_html=True)
+st.sidebar.markdown(
+    f"""
+    <div style='display: block; text-align: center; font-family: "Roboto", Sans-serif;
+              font-weight: 400; color: #1b2836'">Connect Device</div>
+    <form name="REQUEST"  method="post" action="https://api.spikeapi.com/init-user-integration/">
+        <input type="hidden" id="provider" name="provider" value="fitbit">
+        <input type="hidden" id="user_id" name="user_id" value="ELSz1ND0Oc">
+        <input type="hidden" id="client_id" name="client_id" value="{CLIENT_ID}">
+        <button type="submit" class="btn">Fitbit</button>
+    </form>    
+    """,
+    unsafe_allow_html=True,
+)
 
 
 with st.echo(code_location='below'):
