282d41733b9f6a1a5309ab9d6c3dd940113467a5
Hexzzard <111305695+Hexzzard@users.noreply.github.com>
2022-10-07 01:56:59 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,47 +1,27 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import sympy as sym
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
 import streamlit as st
+st.title("Título: Tutorial de Streamlit")
 
-st.title("Ejercicio numero 6")
-st.text("Obtenga la Interpolación de Chebyshev desde cheby.npy. ¿Qué conclusiones obtiene? ¿Escriba el polinomio con sus coeficientes.")
-st.text("cheby.npy = [[ 1  2  3  4  5  6  7  8  9 10][ 1  3  5  4  7  6 12 10 13 12]]")
+with st.echo(code_location='below'):
+    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-#puntos
-data = np.load('cheby.npy')
-#data = [[0,0.2,0.3,0.4],[1,1.6,1.7,2.0]]
-plt.plot(data[0],data[1],'o', label='Puntos',color='r')
-plt.xlabel('F(x)')
-plt.ylabel('T(s)')
+    Point = namedtuple('Point', 'x y')
+    data = []
 
-#matriz de vandermounde
-n = len(data[0])
-D = np.zeros((n,n),dtype=float)
+    points_per_turn = total_points / num_turns
 
-for i in range(n):
-    for j in range(n):
-        exp = n - 1 -j
-        D[i,j]= data[0][i]**exp
+    for curr_point_num in range(total_points):
+        curr_turn, i = divmod(curr_point_num, points_per_turn)
+        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+        radius = curr_point_num / total_points
+        x = radius * math.cos(angle)
+        y = radius * math.sin(angle)
+        data.append(Point(x, y))
 
-#resolver la matriz
-var = np.linalg.solve(D,data[1])
-x = sym.Symbol('x')
-pol = 0
-for i in range(0,n,1):
-    exp = (n-1)-i   # Derecha a izquierda
-    t = var[i]*(x**exp)
-    pol = pol + t
-
-sym.pprint(pol)
-#graficar el polinomio
-fx = sym.lambdify(x,pol)
-a = np.min(data[0]) #intervalo [a,b]
-b = np.max(data[0])
-px = np.linspace(a,b,100)
-py = fx(px)
-plt.plot(px,py,label='Interpolacion Chebyshev',color='b')
-plt.grid()
-plt.xticks([1,2,3,4,5,6,7,8,9,10])
-plt.legend()
-
-st.pyplot()
+    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+        .mark_circle(color='#0068c9', opacity=0.5)
+        .encode(x='x:Q', y='y:Q'))
