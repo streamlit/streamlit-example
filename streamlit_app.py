c8b505d0d25f89e1be8cf4858f2e70e610a4a827
FaberNovum <wrighttaylor67@gmail.com>
2022-10-09 21:56:48 -0500

    Linear Regression

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,34 +5,111 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# Introduction to Linear Regression
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+I've been thinking of how to teach data science tools in an approachable way.
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+I recently found Streamlit and think it might be a good fit.
 
-In the meantime, below is an example of what you can do with just a few lines of code:
+So I'm going to test it out with a few concepts and see how it sticks.
+
+Let me know what you think.
+
+## What's linear regression?
+
+Linear regression is a simple and easy to understand algorithm used in machine learning.
+
+As with a lot of algorithms in machine learning, linear regression is stolen from statistics.
+
+Here's the equation:
+"""
+st.latex(body= "y=B0 + B1*x")
+
+latext = r'''
+Where:
+
+$${y}=$$ Output
+
+$${B0}=$$ First coefficient
+
+$${B1}=$$ Second coefficient
+
+$${x}=$$ Input values
+'''
+
+st.write(latext)
+
+st.write("Note that there will be as many coefficients as there are input columns.")
+st.write("With the information you've learned thus far, can you guess what the model will do with these coefficients?")
+
+"""
+The linear regression model will estimate the values of the coefficients with the data (input columns) you have.
+
+
 """
 
+"""
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+## Why use linear regression?
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+Several reasons:
+- It's simple
+- Easy to learn
+- Easy to implement
 
-    points_per_turn = total_points / num_turns
+"""
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+"""
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+## When to use linear regression?
+
+When you need to predict a continuous value (a number an infinite number of values between any two values).
+
+It's really that simple.
+
+Here are a couple famous use cases:
+
+- Housing prices
+
+- Stock prices
+
+"""
+
+"""
+## How to optimize linear regression
+
+First, what does optimization mean?
+
+Simply put:
+
+Optimization is the iterative improvement of the model.
+
+This improvement happens by using different methods.
+
+
+Linear regression has a couple well-known optimization options:
+- Least Squares
+- Gradient Descent
+- Regularization
+
+Don't worry about exactly what these are.
+
+We can cover that at another time.
+
+"""
+
+"""
+## Rules of Thumb for Linear Regression
+
+1. Linearity
+    - Your input and output values have a linear relationship.
+2. Noise
+    - Your input and output values are not noisy.
+3. Collinearity
+    - Your input columns aren't highly correlated.
+4. Gaussian
+    - Your input and output values have a normal (Gaussian) distribution.
+5. Scale
+    - Your input values share the same scale.
+
+"""
