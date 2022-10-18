f38a5b68e21767320f7852e353758fcbe85e0ae7
Jake W <jwadd00@gmail.com>
2022-10-18 16:28:44 -0500

    okay

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,31 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+# libraries
 import streamlit as st
+import pandas as pd
+from datetime import datetime
 
-"""
-# Welcome to Streamlit!
+# read data
+df = pd.read_csv('./data/final_simulated_schedule.csv')
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# page title
+st.title("Schedule")
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# select / filter routes
+all_routes = df['route'].unique()
+routes = st.sidebar.multiselect("Select Route", default = all_routes, options = all_routes)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# select / filter dates
+min_date = df['date'].min()
+min_date = datetime.strptime(min_date, '%Y-%m-%d') 
+max_date = df['date'].max()
+max_date = datetime.strptime(max_date, '%Y-%m-%d')
 
+all_dates = df['date'].unique()
+dates = st.sidebar.date_input('Select Date', datetime.today(), min_date, max_date)
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# result set
+df['date']= pd.to_datetime(df['date']).dt.date
+output = df[df['date'] == dates]
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+output = output[output['route'].isin(routes)]
 
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+st.table(output)
