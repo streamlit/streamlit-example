be335d59b6c60a3cb865c699c43094842a33db51
pgiaphu <99589333+pgiaphu@users.noreply.github.com>
2022-10-23 23:30:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 
 ###################################
-#from st_aggrid import AgGrid
-#from st_aggrid.grid_options_builder import GridOptionsBuilder
-#from st_aggrid.shared import JsCode
+from st_aggrid import AgGrid
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+from st_aggrid.shared import JsCode
 
 ###################################
 
