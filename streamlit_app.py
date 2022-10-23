bef63da8d2df7f8690df0b800d3ae569cf79ee10
pgiaphu <99589333+pgiaphu@users.noreply.github.com>
2022-10-23 23:57:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,15 +65,11 @@ c29, c30, c31 = st.columns([1, 6, 1])
 
 with c30:
 
-    uploaded_file = st.file_uploader(
-        "",
-        key="1",
-        help="To activate 'wide mode', go to the hamburger menu > Settings > turn on 'wide mode'",
-    )
+    uploaded_file = st.file_uploader("Choose a XLSX file", type="xlsx")
 
     if uploaded_file is not None:
         file_container = st.expander("Check your uploaded .csv")
-        shows = pd.read_csv(uploaded_file)
+        shows = pd.read_excel(uploaded_file)
         uploaded_file.seek(0)
         file_container.write(shows)
 
