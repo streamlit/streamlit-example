a19a3bd6328a683aeecee6b7fce41b68e683a1fe
pgiaphu <99589333+pgiaphu@users.noreply.github.com>
2022-10-24 14:47:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,7 +69,7 @@ gb = GridOptionsBuilder.from_dataframe(shows)
 # enables pivoting on all columns, however i'd need to change ag grid to allow export of pivoted/grouped data, however it select/filters groups
 gb.configure_default_column(enablePivot=True, enableValue=True, enableRowGroup=True)
 gb.configure_selection(selection_mode="multiple",use_checkbox=True)
-gb.configure_column(headerCheckboxSelection=True)
+gb.configure_column(shows.columns[0],headerCheckboxSelection=True)
 gb.configure_side_bar()  # side_bar is clearly a typo :) should by sidebar
 gridOptions = gb.build()
 
