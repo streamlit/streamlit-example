59455b7c4b864d4cfb0055fb7425a57b180397a1
pgiaphu <99589333+pgiaphu@users.noreply.github.com>
2022-10-24 16:42:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,8 @@ with c30:
         shows = pd.melt(shows, id_vars=shows.columns[0])
         shows.sort_values(by=['Date'],inplace=True)
         shows["Date"] = shows["Date"].apply(lambda x: datetime.strftime(x, '%m-%Y'))
-        shows = pd.pivot_table(shows, values="value",index="variable",columns="Date").reset_index()
+        shows = shows.unstack('Date').reset_index()
+        #shows = pd.pivot_table(shows, values="value",index="variable",columns="Date").reset_index()
         st.write(shows)
     else:
         st.info(
