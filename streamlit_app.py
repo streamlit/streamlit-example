72e97ea58e0c1a91cf4619d49ed1300a6bd0da75
pgiaphu <99589333+pgiaphu@users.noreply.github.com>
2022-10-24 16:26:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,11 +69,11 @@ with c30:
 
 from st_aggrid import GridUpdateMode, DataReturnMode
 
-gb = GridOptionsBuilder.from_dataframe(df_unpivot)
+gb = GridOptionsBuilder.from_dataframe(shows)
 # enables pivoting on all columns, however i'd need to change ag grid to allow export of pivoted/grouped data, however it select/filters groups
-gb.configure_default_column(df_unpivot['variable'],enablePivot=False, enableValue=False, enableRowGroup=False)
+gb.configure_default_column(enablePivot=True, enableValue=True, enableRowGroup=True)
 gb.configure_selection(selection_mode="multiple",use_checkbox=True)
-gb.configure_column(df_unpivot['variable'],headerCheckboxSelection=True)
+gb.configure_column(shows.columns[0],headerCheckboxSelection=True)
 gb.configure_side_bar()  # side_bar is clearly a typo :) should by sidebar
 gridOptions = gb.build()
 
