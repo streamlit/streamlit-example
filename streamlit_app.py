5ffe35f3aa25304df6ebebf7e65edbb030a91a42
pgiaphu <99589333+pgiaphu@users.noreply.github.com>
2022-10-24 17:07:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ with c30:
         #unpivot & pivot to WIDE-FORMAT
         shows = pd.melt(shows, id_vars=shows.columns[0])
         #shows.sort_values(by=['Date'],inplace=True)
-        shows["Date"] = shows["Date"].apply(lambda x: date())
+        shows["Date"] = shows["Date"].apply(lambda x: date(x))
         #shows.set_index(['Date', 'variable'],inplace=True)
         #shows = shows.unstack('Date').reset_index()
         shows = pd.pivot_table(shows, values="value",index="variable",columns="Date").reset_index()
