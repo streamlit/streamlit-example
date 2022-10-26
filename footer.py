174106322576d9a779ac93b2642d3e251ff4e5fa
pgiaphu <99589333+pgiaphu@users.noreply.github.com>
2022-10-26 10:56:01 +0700

    Update footer.py

--- footer.py
+++ footer.py
@@ -10,7 +10,7 @@ def layout(*args):
     <style>
       # MainMenu {visibility: hidden;}
       footer {visibility: hidden;}
-     .stApp { bottom: 50px; }
+     .stApp { bottom: 102px; }
     </style>
     """
 
@@ -28,7 +28,7 @@ def layout(*args):
 
     style_hr = styles(
         display="block",
-        margin=px(4, 4, "auto", "auto"),
+        margin=px(8, 8, "auto", "auto"),
         border_style="inset",
         border_width=px(2)
     )
