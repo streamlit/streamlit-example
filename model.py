076e1522dc2c1caa32c2f453da878cb04e2f075b
pgiaphu <99589333+pgiaphu@users.noreply.github.com>
2022-10-27 14:50:36 +0700

    Update model.py

--- model.py
+++ model.py
@@ -53,7 +53,7 @@ def HoltWinter(df: pd.DataFrame):
  for sku in df.columns:
         fitHW = sm.tsa.ExponentialSmoothing(np.asarray(df[sku]), initialization_method="heuristic",seasonal_periods=12,trend='add', seasonal='add',damped_trend=True).fit(optimized=True)
         arr_forecast = fitHW.forecast(fcperiod)
-        df_HW[sku] = arr_forecast
+        df_HW['fc_'+sku] = arr_forecast
         df_HW.set_index(future_index,inplace=True)
     
     
