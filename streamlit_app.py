c6b1ceeec64b2894c205b8a3d1c14a6c54513e87
Venkatakrishnan Ramesh <ch.en.u4aie20078@ch.students.amrita.edu>
2022-11-04 10:15:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ def predict_emotions(docx):
 
 
 def get_prediction_proba(docx):
-    results = pipeline.predict_proba([docx])
+    results = pipeline.predict_proba(docx)
     return results
 
 
@@ -29,13 +29,6 @@ def get_prediction_proba(docx):
 # Main Application
 def main():
     st.title("Emotion Classifier App")
-    menu = ["Home"]
-    choice = st.sidebar.selectbox("Menu", menu)
-
-    if choice == "Home":
-        ("Home", datetime.now())
-        st.subheader("Emotion In Text")
-
         with st.form(key="emotion_clf_form"):
             raw_text = st.text_area("Type Here")
             submit_text = st.form_submit_button(label="Submit")
@@ -45,30 +38,11 @@ def main():
 
             # Apply the linkage function here
             prediction = predict_emotions(raw_text)
-            probability = get_prediction_proba(raw_text)
+    
 
-            with col1:
-                st.success("Original Text")
-                st.write(raw_text)
 
-                st.success("Prediction")
-                st.write("{}:{}".format(prediction)
-                st.write("Confidence:{}".format(np.max(probability)))
 
-            with col2:
-                st.success("Prediction Probability")
-                # st.write(probability)
-                proba_df = pd.DataFrame(probability, columns=pipeline.classes_)
-                # st.write(proba_df.T)
-                proba_df_clean = proba_df.T.reset_index()
-                proba_df_clean.columns = ["emotions", "probability"]
 
-                fig = (
-                    alt.Chart(proba_df_clean)
-                    .mark_bar()
-                    .encode(x="emotions", y="probability", color="emotions")
-                )
-                st.altair_chart(fig, use_container_width=True)
 
 
 if __name__ == "__main__":
