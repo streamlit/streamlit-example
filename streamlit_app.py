7d9f17b25d9582801fc5adc6a14a5f74abaa3717
lpmfimis18 <34491937+lpmfimis18@users.noreply.github.com>
2022-11-09 10:25:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,9 @@ In the meantime, below is an example of what you can do with just a few lines of
 """
 
 user_input = st.text_input("Please enter your name", "")
-st.text("Hello " + user_input)
+
+if len(user_input) > 0:
+    st.text("Hello " + user_input)   
 
 total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
 num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
