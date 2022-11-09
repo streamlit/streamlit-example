c7db3f0ed21caabcb2d47cb0876d56ff6763d1c6
lpmfimis18 <34491937+lpmfimis18@users.noreply.github.com>
2022-11-09 11:40:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,7 @@ import math
 import pandas as pd
 import streamlit as st
 
-age_param = st.number_input("Please enter your age", min_value=1, max_value=None)
-st.text(age_param)
+age_param = st.number_input("Please enter your age", min_value=0, max_value=None)
+if age_param > 0:
+  st.text(age_param)
+  
