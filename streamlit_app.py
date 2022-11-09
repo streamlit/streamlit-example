90665fc224a074ea02c8389b2e505aa079dcb5b8
lpmfimis18 <34491937+lpmfimis18@users.noreply.github.com>
2022-11-09 11:43:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,8 @@ import streamlit as st
 age_param = st.number_input("Please enter your age", min_value=0, max_value=None)
 if age_param > 0:
   st.text(age_param)
-  
+ 
+cbsa_options = ['Test1', 'test2']
+cbsa = st.selectbox("Please select CBSA", options=cbsa_options)
+if cbsa is defined:
+  st.text(cbsa)
