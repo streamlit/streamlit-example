f443439f4282f2a5517868cc60a0d3c6f52e2ec6
Venkatakrishnan Ramesh <venkatkrishnan2003@gmail.com>
2022-11-15 13:23:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ st.set_page_config(page_title="Movie Reviewer", layout="wide")
 
 # Main Application
 def main():
-    st.title("😉  DR Soumyendra  & Team One's Movie Review classifier App 😉")
+    st.title("😉  Movie Review classifier App 😉")
     with st.form(key="emotion_clf_form"):
         raw_text1 = st.text_area(" Your latest movie name")
         raw_text = st.text_area("Feed your review here for your latest movie")
