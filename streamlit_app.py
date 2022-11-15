604eb45cb1501fa69bebc31669b57900b4350b60
lpmfimis18 <34491937+lpmfimis18@users.noreply.github.com>
2022-11-14 22:12:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,16 +8,16 @@ import pymysql
 conn = pymysql.connect(host='cse6242.czj7hqwhnoml.us-east-1.rds.amazonaws.com', user="admin",
                                         password="cse6242110", port=3306, database="realestate"
                                         )
-query = "SELECT DISTINCT cbsatitle FROM listings_enriched_final"
+query = "SELECT '' AS cbsatitle UNION SELECT DISTINCT cbsatitle FROM listings_enriched_final ORDER BY 1"
 cbsa_data = pd.read_sql(query, conn)
-conn.close()
 
 st.selectbox("Please select CBSA", options=cbsa_data)
+
 # if cbsa != '':
 #   st.text(cbsa)
-
-st.dataframe(cbsa_data)
 
 age_param = st.number_input("Please enter your age", min_value=0, max_value=None)
 if age_param > 0:
   st.text(age_param)
+
+conn.close()
