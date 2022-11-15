999ee49288a2ade7c6da000aaa794a5934d598f8
lpmfimis18 <34491937+lpmfimis18@users.noreply.github.com>
2022-11-14 22:30:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,14 +10,36 @@ conn = pymysql.connect(host='cse6242.czj7hqwhnoml.us-east-1.rds.amazonaws.com', 
                                         )
 query = "SELECT '' AS cbsatitle UNION SELECT DISTINCT cbsatitle FROM listings_enriched_final ORDER BY 1"
 cbsa_data = pd.read_sql(query, conn)
+cbsa_param = st.selectbox("Please select CBSA", options=cbsa_data)
 
-st.selectbox("Please select CBSA", options=cbsa_data)
+bedroom_param = st.number_input("Please enter your desired # of bedrooms", min_value=0, max_value=7)
 
-# if cbsa != '':
-#   st.text(cbsa)
+bathroom_param = st.number_input("Please enter your desired # of baathrooms", min_value=0, max_value=7)
 
-age_param = st.number_input("Please enter your age", min_value=0, max_value=None)
-if age_param > 0:
-  st.text(age_param)
+age_param = st.number_input("Please enter your age", min_value=0, max_value=100)
+
+school_param = st.number_input("Please enter your importance of school (higher is more important)", min_value=0, max_value=10)
+
+income_param = st.number_input("Please enter your income", min_value=0, max_value=300000)
+
+year_built_param = st.number_input("Please enter your minimum desired year built", min_value=1900, max_value=2022)
+
+square_feet_param = st.number_input("Please enter your minimum desired square feet", min_value=0, max_value=10000)
+
+centrality_param = st.number_input("Please enter your desired closeness to downtown (higher is closer)", min_value=0.0, max_value=1.0)
+
+education_options = ['', 'Graduate', 'Bachelors', 'High School Diploma', 'Associates or Equivelant', 'No Diploma']
+education_options_param = st.selectbox("Please select your education level", options=education_options)
+
+home_type_options = ['', 'Condo', 'Single Family', 'Townhouse']
+home_type_param = st.selectbox("Please select your home type", options=home_type_options)
+
+family_options = ['', 'Yes', 'No']
+family_param = st.selectbox("Do you have a family", options=family_options)
+
+price_param = st.number_input("Please enter your desired price", min_value=0, max_value=1000000)
+
+# if age_param > 0:
+#   st.text(age_param)
 
 conn.close()
