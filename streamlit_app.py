be07329a5a2b6692342f621331e27a3fcd9bb639
lpmfimis18 <34491937+lpmfimis18@users.noreply.github.com>
2022-11-14 21:24:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,16 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import pymysql
+
+conn = pymysql.connect(host='cse6242.czj7hqwhnoml.us-east-1.rds.amazonaws.com', user="admin",
+                                        password="cse6242110", port=3306, database="realestate"
+                                        )
+query = "SELECT * FROM listings_enriched_final"
+df = pd.read_sql(query, conn)
+conn.close()
+
+st.dataframe(df.head(1))
 
 age_param = st.number_input("Please enter your age", min_value=0, max_value=None)
 if age_param > 0:
