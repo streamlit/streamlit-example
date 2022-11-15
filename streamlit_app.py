f65d28fdac800c9aad6e9ddcde79dc5492f9cffd
lpmfimis18 <34491937+lpmfimis18@users.noreply.github.com>
2022-11-14 22:04:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,13 +12,17 @@ query = "SELECT * FROM listings_enriched_final"
 df = pd.read_sql(query, conn)
 conn.close()
 
-st.dataframe(df.head(1))
+
+cbsa_options = []
+cbsa_data = df.loc[:, 'cbsatitle']
+# st.selectbox("Please select CBSA", options=cbsa_options)
+# if cbsa != '':
+#   st.text(cbsa)
+
+st.dataframe(cbsa_data)
 
 age_param = st.number_input("Please enter your age", min_value=0, max_value=None)
 if age_param > 0:
   st.text(age_param)
  
-cbsa_options = ['', 'Test1', 'test2']
-cbsa = st.selectbox("Please select CBSA", options=cbsa_options)
-if cbsa != '':
-  st.text(cbsa)
+
