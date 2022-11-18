982f239bdf5c912f79925bafac4ae2e8ae4a3bbc
lpmfimis18 <34491937+lpmfimis18@users.noreply.github.com>
2022-11-17 20:58:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,6 @@ with col2:
   square_feet_param = st.number_input("Min Square Feet", min_value=0, max_value=10000)
   
   price_param = st.number_input("Min price", min_value=0, max_value=1000000)  
-  
-  income_param = st.number_input("Income", min_value=0, max_value=300000)
 
   
 
@@ -40,18 +38,16 @@ with col3:
   
   education_options = ['', 'Graduate', 'Bachelors', 'High School Diploma', 'Associates or Equivelant', 'No Diploma']
   education_options_param = st.selectbox("Education level", options=education_options)  
-
-  family_options = ['', 'Yes', 'No']
-  family_param = st.selectbox("Do you have a family?", options=family_options)  
+  
+  income_param = st.number_input("Income", min_value=0, max_value=300000)
   
 
 with col4:
+  family_options = ['', 'Yes', 'No']
+  family_param = st.selectbox("Do you have a family?", options=family_options)    
+  
   school_param = st.number_input("Importance of School (higher = important)", min_value=0, max_value=10)  
   
   centrality_param = st.number_input("Closeness to Downtown (higher = closer)", min_value=0.0, max_value=1.0)  
-
-
-# if age_param > 0:
-#   st.text(age_param)
 
 conn.close()
