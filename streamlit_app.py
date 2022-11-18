48ae660051cde1173fed1a77237ebc1a8ecdfb27
lpmfimis18 <34491937+lpmfimis18@users.noreply.github.com>
2022-11-17 21:20:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,6 +56,6 @@ with col4:
 
 if cbsa_param is not None and cbsa_param != '':
   zipcode = '123456'
-  st.markdown("<h2 style='text-align: center; color: white;'>We reccomend you live in zipcode: </h2>", unsafe_allow_html=True)
+  st.markdown("<h2 style='text-align: center; color: white;'>We recommend you live in zipcode </h2>" + zipcode, unsafe_allow_html=True)
   
 conn.close()
