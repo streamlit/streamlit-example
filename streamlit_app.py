3aa26b9d6afea21483f84bfb7e926750115b0d23
lpmfimis18 <34491937+lpmfimis18@users.noreply.github.com>
2022-11-17 20:49:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,13 +19,20 @@ with col1:
   bedroom_param = st.number_input("Please enter your desired # of bedrooms", min_value=0, max_value=7)
 
   bathroom_param = st.number_input("Please enter your desired # of bathrooms", min_value=0, max_value=7)
-
+  
+  year_built_param = st.number_input("Please enter your minimum desired year built", min_value=1900, max_value=2022)
   
   
 with col2:
   income_param = st.number_input("Please enter your income", min_value=0, max_value=300000)
 
-  year_built_param = st.number_input("Please enter your minimum desired year built", min_value=1900, max_value=2022)
+  square_feet_param = st.number_input("Please enter your minimum desired square feet", min_value=0, max_value=10000)
+  
+  price_param = st.number_input("Please enter your desired price", min_value=0, max_value=1000000)
+
+  education_options = ['', 'Graduate', 'Bachelors', 'High School Diploma', 'Associates or Equivelant', 'No Diploma']
+  education_options_param = st.selectbox("Please select your education level", options=education_options)
+  
 
 with col3:
   age_param = st.number_input("Please enter your age", min_value=0, max_value=100)  
@@ -36,12 +43,6 @@ with col3:
   family_options = ['', 'Yes', 'No']
   family_param = st.selectbox("Do you have a family", options=family_options)  
   
-  square_feet_param = st.number_input("Please enter your minimum desired square feet", min_value=0, max_value=10000)
-  
-  price_param = st.number_input("Please enter your desired price", min_value=0, max_value=1000000)
-
-  education_options = ['', 'Graduate', 'Bachelors', 'High School Diploma', 'Associates or Equivelant', 'No Diploma']
-  education_options_param = st.selectbox("Please select your education level", options=education_options)
 
 with col4:
   school_param = st.number_input("Please enter your importance of school (higher is more important)", min_value=0, max_value=10)  
