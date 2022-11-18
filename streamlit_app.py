ff7557c265bd761a741e17d62ab44179cfedaa73
lpmfimis18 <34491937+lpmfimis18@users.noreply.github.com>
2022-11-17 20:57:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,29 +16,30 @@ cbsa_param = st.selectbox("Select CBSA", options=cbsa_data)
 col1, col2, col3, col4 = st.columns(4)
 
 with col1:
+  home_type_options = ['', 'Condo', 'Single Family', 'Townhouse']
+  home_type_param = st.selectbox("Home Type", options=home_type_options)  
+  
   bedroom_param = st.number_input("# of Bedrooms", min_value=0, max_value=7)
 
   bathroom_param = st.number_input("# of Bathrooms", min_value=0, max_value=7)
   
-  year_built_param = st.number_input("Min Year Built", min_value=1900, max_value=2022)
-  
   
 with col2:
-  income_param = st.number_input("Income", min_value=0, max_value=300000)
-
+  year_built_param = st.number_input("Min Year Built", min_value=1900, max_value=2022)  
+  
   square_feet_param = st.number_input("Min Square Feet", min_value=0, max_value=10000)
   
-  price_param = st.number_input("PMin price", min_value=0, max_value=1000000)
+  price_param = st.number_input("Min price", min_value=0, max_value=1000000)  
+  
+  income_param = st.number_input("Income", min_value=0, max_value=300000)
 
-  education_options = ['', 'Graduate', 'Bachelors', 'High School Diploma', 'Associates or Equivelant', 'No Diploma']
-  education_options_param = st.selectbox("Education level", options=education_options)
   
 
 with col3:
-  age_param = st.number_input("=Age", min_value=0, max_value=100)  
+  age_param = st.number_input("Age", min_value=0, max_value=100)  
   
-  home_type_options = ['', 'Condo', 'Single Family', 'Townhouse']
-  home_type_param = st.selectbox("Home Type", options=home_type_options)
+  education_options = ['', 'Graduate', 'Bachelors', 'High School Diploma', 'Associates or Equivelant', 'No Diploma']
+  education_options_param = st.selectbox("Education level", options=education_options)  
 
   family_options = ['', 'Yes', 'No']
   family_param = st.selectbox("Do you have a family?", options=family_options)  
