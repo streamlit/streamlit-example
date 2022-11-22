49e704147d422fab9b8e60eb755d29a8f491c0f8
Hong, Hyewon <hhong96@gatech.edu>
2022-11-21 19:44:33 -0500

    feature added, readme edited

--- README.md
+++ README.md
@@ -1,6 +1,50 @@
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire. :heart:
+# Neighborhood Recommender System :house:
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+This app recommend the best neighborhood to live in based on user input related to quality of living factors. It is deployed using Streamlit.
+
+## Overview
+
+  [!Example](/image/example.png)
+ 
+
+### Dataset
+
+- Property Listing data from multiple MLS Source via third-party scrape
+
+- Census Data from United States Census Bureau including the American Community Survey Data
+
+- Yelp business reviews from Yelp API
+
+- Smart Location Mapping from United States Environment Protection Agency
+
+### Model
+
+This model was trained to recommend a subset of You can find the details on training algorithm in ____.
+
+  
+  
+
+## Setup
+
+  
+
+- Install [python3](https://www.python.org/downloads/) if not already installed
+
+- Create a virtual environment for working with Python
+
+- `python3 -m venv cse6242-team110`
+
+- Activate your virtual environment
+
+- `cd cse6242-team110 && source bin/activate`
+
+- Within the activated virtual environment install dependencies
+
+- `pip3 install -r requirements.txt`
+
+- Place separately provided key.py file in the main folder and fill api_key variable with your Google Geolocation API Key
+
+	> API Key is necessary to obtain longitude / latitude from listing street address. You can sign up in the [link](https://developers.google.com/maps/documentation/geolocation/overview). $300 credit is provided to First-time user, which is equivalent to about 6,000 refresh in the current set-up.
+
+- Run the app with `streamlit run streamlit_app.py` in the `streamlit-example` directory
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,57 +1,126 @@
-from collections import namedtuple
+# %%
 import altair as alt
-import math
 import pandas as pd
 import streamlit as st
 import pymysql
+import googlemaps
+import key
 
-conn = pymysql.connect(host='cse6242.czj7hqwhnoml.us-east-1.rds.amazonaws.com', user="admin",
-                                        password="cse6242110", port=3306, database="realestate"
-                                        )
+pd.set_option('display.max_columns', None)
 
-zipcode = None
+# Connection
+def data_read(query):
+      conn = pymysql.connect(host=key.host, user=key.user,
+                                              password=key.password, port=key.port, database=key.database
+                                              )
+      df = pd.read_sql(query, conn)
+      conn.close()
+      return df
+    
+    
+ls_en = data_read("select * from listings_enriched_final")
 
-st.markdown("<h1 style='text-align: center;'>Where should you live?</h1>", unsafe_allow_html=True)
 
-query = "SELECT '' AS cbsatitle UNION SELECT DISTINCT cbsatitle FROM listings_enriched_final ORDER BY 1"
-cbsa_data = pd.read_sql(query, conn)
-cbsa_param = st.selectbox("Select Geographic Area", options=cbsa_data)
+for i in range(len(ls_en.columns)):
+  if ls_en[ls_en.columns[i]].dtype == 'object' and ls_en.columns[i] != 'cbsatitle':
+    ls_en[ls_en.columns[i]] = ls_en[ls_en.columns[i]].str.title()
 
+label = {}
+for i in range(len(ls_en.columns)):
+    if (ls_en[ls_en.columns[i]].dtype == 'object') or ('bucket' in ls_en.columns[i]):
+      ls_en[ls_en.columns[i]].replace('_', ' ', regex=True, inplace=True)
+      ls_en[ls_en.columns[i]].replace('Population ', '', regex=True, inplace=True)
+      ls_en[ls_en.columns[i]].replace('Income ', '', regex=True, inplace=True)
+      ls_en[ls_en.columns[i]].replace('Between ', '', regex=True, inplace=True)
+      ls_en[ls_en.columns[i]].replace('And', '-', regex=True, inplace=True)
+      label[ls_en.columns[i]] = ls_en[ls_en.columns[i]].unique().tolist()
+    elif 'dummy' in ls_en.columns[i] and ('bucket' not in ls_en.columns[i]):
+      label[ls_en.columns[i]] = ls_en[ls_en.columns[i]].unique().tolist()
+    elif 'final' in ls_en.columns[i]:
+      label[ls_en.columns[i]] = ls_en[ls_en.columns[i]].unique().tolist()
+
+
+st.markdown("<div><h1 style='text-align: center;'>Where should you live?</h1></div>", unsafe_allow_html=True)
+
+
+output = []
+
+cbsa_param = st.selectbox("Select Geographic Area", options=sorted(label['cbsatitle']))
 col1, col2, col3, col4 = st.columns(4)
 
 with col1:
-  home_type_options = ['', 'Condo', 'Single Family', 'Townhouse']
-  home_type_param = st.selectbox("Home Type", options=home_type_options)  
-  
-  bedroom_param = st.number_input("# of Bedrooms", min_value=0, max_value=7)
+  home_type_param = st.selectbox("Home Type", options=sorted(label['homeType'])) 
+  income_param = st.selectbox("Income", options=sorted(label['majority_income_final']))
+  bedroom_param = st.select_slider("Num of Bedrooms", options=sorted(label['bedrooms_final']), on_change=None)
 
-  bathroom_param = st.number_input("# of Bathrooms", min_value=0, max_value=7)
-  
 with col2:
-  price_param = st.number_input("Min price", min_value=0, max_value=1000000)    
-  
-  square_feet_param = st.number_input("Min Square Feet", min_value=0, max_value=10000) 
-  
-  year_built_param = st.number_input("Min Year Built", min_value=1900, max_value=2022)  
+  price_param = st.selectbox("Ideal Price Range", options=sorted(label['price_bucket']))
+  square_feet_param = st.selectbox("Size Preference", options=sorted(label['sqft_buckets_dummy']))
+  year_built_param = st.select_slider("Recently-Built Home", options=sorted(label['yearbuilt_buckets_dummy']), on_change=None)
+
   
 with col3:
-  age_param = st.number_input("Age", min_value=0, max_value=100)  
-  
-  education_options = ['', 'Graduate', 'Bachelors', 'High School Diploma', 'Associates or Equivelant', 'No Diploma']
-  education_options_param = st.selectbox("Education level", options=education_options)  
-  
-  income_param = st.number_input("Income", min_value=0, max_value=300000)
-  
-with col4:
-  family_options = ['', 'Yes', 'No']
-  family_param = st.selectbox("Do you have a family?", options=family_options)    
-  
-  important_options = ['', 'Not Important', 'Little Important', 'Important', 'Very Important']
-  school_param = st.selectbox("Importance of School", options=important_options)  
-  centrality_param = st.selectbox("Closeness to Downtown", options=important_options)  
+  age_param = st.selectbox("Age", options=sorted(label['median_age_bucket']))
+  education_options_param = st.selectbox("Education level", options=sorted(label['majority_education_final']))
+  bathroom_param = st.select_slider("Num of Bathrooms", options=sorted(label['bathrooms_final']), on_change=None)
 
-if cbsa_param is not None and cbsa_param != '':
-  zipcode = '123456'
-  st.markdown("<h2 style='text-align: center;'>We recommend you live in zipcode " + zipcode + "!</h2>", unsafe_allow_html=True)
+with col4:
+  family_param = st.selectbox("Family-Oriented Neighborhood", options=sorted(label['family_type_dummy']))   
+  ind_param = st.selectbox("Occupation", options=sorted(label['majority_industry_final']))
+  centrality_param = st.select_slider("Closeness to Downtown", options=sorted(label['centrality_index_dummy']), on_change=None)  
   
-conn.close()
+
+if st.button('Predict'):
+  
+  query = """
+  select pred
+  from output_test
+  where 
+  cbsatitle = '{}'
+  and bedroom_bucket_dummy = replace(replace(replace(replace(replace(replace(cast('{}' as unsigned),2,1),3,2),4,2),5,2),6,3),7,3)
+  and sqft_buckets_dummy = cast('{}' as unsigned) 
+  and yearbuilt_buckets_dummy = cast('{}' as unsigned) 
+  and family_type_dummy = cast('{}' as unsigned) 
+  and centrality_index_dummy = cast('{}' as unsigned)
+  
+  and price_bucket = lower('{}')
+  and homeType = upper('{}')
+  and median_age_bucket =lower('{}') 
+  and majority_education_final = lower('{}') 
+  and majority_income_final = lower('{}')
+  """.format(cbsa_param, bedroom_param, 
+             square_feet_param, year_built_param, family_param, centrality_param,
+             price_param, home_type_param, age_param, education_options_param, 
+             income_param)
+  
+  # if data_read(query)['pred'].values[0] != None:
+  #   zipcode = data_read(query)['pred']
+  # else:
+  zipcode = 22314
+    
+  # %%
+  st.markdown("<div2><h2 style='text-align: center;'>We recommend you live in zipcode " + str(zipcode) + "!</h2><div2>", unsafe_allow_html=True)
+  
+  ls = data_read("select * from listing where zipcode = {} limit 10".format(zipcode))
+  ls['address'] = ls.apply(lambda x: '%s, %s, %s, %s' % (x['streetAddress'], x['city'], x['state'], x['zipcode']), axis=1)
+  ls['lat'] = 0
+  ls['lon'] = 0
+  
+  gmaps_key = googlemaps.Client(key=key.api_key)
+  
+  for i in range(len(ls)):
+    g = gmaps_key.geocode(ls['address'][i])
+    if g[0]["geometry"]["location"]["lat"] != 0:
+      ls['lat'][i] = g[0]["geometry"]["location"]["lat"]
+      ls['lon'][i] = g[0]["geometry"]["location"]["lng"]
+    
+  # %%
+  with st.container():
+    st.map(ls[['lat', 'lon']])
+    
+def main():
+    pass
+  
+if __name__ == '__main__':
+	main()
+ 
