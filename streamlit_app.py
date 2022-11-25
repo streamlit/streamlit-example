baddba0853d651aca68bdb87fadfe9e33a601c2c
David A. Thatcher <david.austin.thatcher@gmail.com>
2022-11-25 10:18:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,17 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+import spacy
 import streamlit as st
+nlp = spacy.load("en_core_web_sm")
 
-"""
-# Welcome to Streamlit!
+def return_NER(value):
+    doc = nlp(value)
+    return [(X.text, X.label_) for X in doc.ents]
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Add title on the page
+st.title("Spacy - Named Entity Recognition")
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Ask user for input text
+input_sent = st.text_input("Input Sentence", "Your input sentence goes here")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Display named entities
+for res in return_NER(input_sent):
+    st.write(res[0], "-->", res[1])
