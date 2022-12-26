0cc718b03cdce4e652404804ca256befc04e3dea
Delroso <st091628@student.spbu.ru>
2022-12-27 00:17:29 +0600

    1st attempt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,40 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
+import easyocr as ocr  # OCR
+import streamlit as st  # Web App
+from PIL import Image  # Image Processing
+import numpy as np  # Image Processing
 
-"""
-# Welcome to Streamlit!
+# title
+st.title("Easy OCR - Extract Text from Images")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# subtitle
+st.markdown("## Optical Character Recognition - Using `easyocr`, `streamlit`")
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# image uploader
+image = st.file_uploader(label="Upload your image here", type=['png', 'jpg', 'jpeg'])
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+@st.cache
+def load_model():
+    reader = ocr.Reader(['en'], model_storage_directory='.')
+    return reader
 
-    Point = namedtuple('Point', 'x y')
-    data = []
 
-    points_per_turn = total_points / num_turns
+reader = load_model()  # load model
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+if image is not None:
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    input_image = Image.open(image)  # read image
+    st.image(input_image)  # display image
+
+    with st.spinner(" AI is at Work! "):
+
+        result = reader.readtext(np.array(input_image))
+
+        result_text = []  # empty list for results
+
+        for text in result:
+            result_text.append(text[1])
+
+        st.write(result_text)
+else:
+    st.write("Upload an Image")
