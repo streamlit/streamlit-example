c27e598c67e391c521f308c7c318ea9e1e1f90d2
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-01-07 15:25:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -116,13 +116,13 @@ ax2.xaxis.set_major_locator(mdates.HourLocator(interval=1))
 ax2.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
 
 # Filter the data to only include volume above 1500
-data_filtered = data[data['Volume'] > 1500]
+mask = data[data['Volume'] > 1500]
 
 # Shade the region of the subplot where the mask is True
-ax2.fill_between(data.index, data['volume'], where=data_filtered, alpha=0.25, color='green')
+ax2.fill_between(data.index, data['volume'], where=mask, alpha=0.25, color='green')
 
 # Plot the filtered data in green
-ax2.plot(data.index, data['Volume'], linestyle='-', linewidth=1, color='green')
+#ax2.plot(data.index, data['Volume'], linestyle='-', linewidth=1, color='green')
 
 # Adjust the spacing between the subplots
 fig.subplots_adjust(hspace=.5)
