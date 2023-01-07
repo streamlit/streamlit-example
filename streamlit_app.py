b38ece7d2541203e548e62c1c1d4f530caaf8c14
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-01-07 13:42:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,75 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+st.set_page_config(page_icon = ":rocket:", layout = "wide")
 
-"""
-# Welcome to Streamlit!
+import pandas as pd
+import matplotlib.pyplot as plt
+import yfinance as yf
+import plotly.graph_objects as go
+from datetime import datetime
+import matplotlib.ticker as ticker
+from matplotlib.dates import MinuteLocator, ConciseDateFormatter
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Set the interval to 1 minute and the symbol to SPY
+interval = "1m"
+period = '1d'
+symbol = "SPY221223C00380000"
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.title(symbol)
+st.title('Ichimoku Cloud Indicator')
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Connect to the TradeStation API and retrieve the price data for the specified symbol and interval
+ticker = yf.Ticker(symbol)
+data = ticker.history(period=period, interval=interval)
 
+# Calculate the Ichimoku Cloud indicator using the data
+data["tenkan_sen"] = data["High"].rolling(window=9).mean()
+data["kijun_sen"] = data["Low"].rolling(window=26).mean()
+data["senkou_span_a"] = (data["tenkan_sen"] + data["kijun_sen"]) / 2
+data["senkou_span_b"] = data["Low"].rolling(window=52).mean()
+data["chikou_span"] = data["Close"].shift(-26)
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# Initialize empty lists to store the long and short positions
+long_positions = []
+short_positions = []
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+# Iterate through the data and determine the trading criteria for go long and go short positions
+for index, row in data.iterrows():
+    # Go long if the market is above the open and the chikou span is above the current price
+    if row["Close"] > row["Open"] and row["chikou_span"] > row["Close"]:
+        long_positions.append(index)
+    # Go short if the market is below the open and the chikou span is below the current price
+    elif row["Close"] < row["Open"] and row["chikou_span"] < row["Close"]:
+        short_positions.append(index)
 
-    points_per_turn = total_points / num_turns
+#data.insert(loc=1, column='TIME', value=pd.to_datetime(data.index,format='%H:%M')
+data['TIME'] = pd.to_datetime(data.index)
+data['TIME'] = data['TIME'].dt.strftime('%H:%M')
+#time_data = data['TIME'] 
+data = data.drop(columns=['Dividends', 'Stock Splits'])
+# Plot the results
+fig, ax = plt.subplots()
+#ax.set_xlim(data.index)
+ax.fill_between(data.index, data['senkou_span_a'], data['senkou_span_b'], where=data['senkou_span_a'] >= data['senkou_span_b'], facecolor='green', alpha=0.25, interpolate=True)  # green fill for bullish trend 
+ax.fill_between(data.index, data['senkou_span_a'], data['senkou_span_b'], where=data['senkou_span_a'] < data['senkou_span_b'], facecolor='red', alpha=0.25, interpolate=True)  # red fill for bearish trend 
+ax.set_xlabel('Time')
+ax.set_ylabel('Price')
+ax.xaxis.set_major_locator(MinuteLocator (interval=15))
+#ax.xaxis.set_major_formatter(ConciseDateFormatter(ax.xaxis.get_major_locator())
+for label in ax.get_xticklabels():
+        label.set_rotation(40)
+        label.set_horizontalalignment('right')
+#ax.set_xticks(time_data.values)
+#ax.set_xticklabels(labels = time_data.values, rotation=45, fontstyle='italic', color='black')
+ax.plot(data["Close"], label="Close", color='dimgrey', linewidth=1)
+ax.plot(data["tenkan_sen"], label="tenkan_sen" , color='blue', linewidth=0.75)
+ax.plot(data["kijun_sen"], label="kijun_sen" , color='saddlebrown', linewidth=0.75)
+ax.plot(data["senkou_span_a"], label="senkou_span_a" , color='limegreen', linewidth=0.75)
+ax.plot(data["senkou_span_b"], label="senkou_span_b" , color='red', linewidth=0.75)
+ax.plot(data["chikou_span"], label="chikou_span" , color='magenta', linewidth=0.75)
+ax.scatter(long_positions, data.loc[long_positions]["Close"], label="Buy", color='green')
+ax.scatter(short_positions, data.loc[short_positions]["Close"], label="Sell" , color='red')
+plt.legend(fontsize=6)
+plt.show()
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+st.pyplot()
+data
