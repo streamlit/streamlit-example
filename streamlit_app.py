3a5a41cbe4e87b513b2e005e21f06d41a45d6288
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-01-07 18:06:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -209,8 +209,8 @@ ax3.plot(macd_df['MACD signal'], label='MACD signal')
 #ax3.bar(macd_df.index, macd_df['MACD histogram'], label='MACD histogram')
 
 #ax3.plot(macd_df1)
-ax3.fill_between(macd_df['MACD'], macd_df['Signal'], where=macd_df['MACD'] > macd_df['Signal'], facecolor='green', interpolate=True)
-ax3.fill_between(macd_df['MACD'], macd_df['Signal'], where=macd_df['MACD'] < macd_df['Signal'], facecolor='red', interpolate=True)
+#ax3.fill_between(macd_df['MACD'], macd_df['Signal'], where=macd_df['MACD'] > macd_df['Signal'], facecolor='green', interpolate=True)
+#ax3.fill_between(macd_df['MACD'], macd_df['Signal'], where=macd_df['MACD'] < macd_df['Signal'], facecolor='red', interpolate=True)
 
 ax3.set_xlabel('Time')
 ax3.set_ylabel('MACD')
