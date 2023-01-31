55aaa5259e5efb9ffd7d853a57406ecb97276eb6
Courtney Orcutt <18cro@queensu.ca>
2023-01-31 11:27:23 -0500

    changes to ensure working

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,35 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import numpy as np
+import tensorflow as tf
+
+from sklearn.model_selection import train_test_split
 
 """
-# Welcome to Streamlit!
+# Abnormal Heartbeat Classification
+
+Detection of normal vs abnormal heartbeats in humans. Then algorithm will be applied to canines based on accuracy.
+
+There are two columns with the last column being either a 0 (normal) or a 1 (abnormal). 
+
+# Arrhythmia Dataset
+Number of Samples: 109446
+Number of Categories: 5
+Sampling Frequency: 125Hz
+Data Source: Physionet's MIT-BIH Arrhythmia Dataset
+Classes: ['N': 0, 'S': 1, 'V': 2, 'F': 3, 'Q': 4]
+
+# The PTB Diagnostic ECG Database
+Number of Samples: 14552
+Number of Categories: 2
+Sampling Frequency: 125Hz
+Data Source: Physionet's PTB Diagnostic Database
+
+All the samples are cropped, downsampled and padded with zeroes if necessary to the fixed dimension of 188.
+
+
+Using a Recurrent Neural Network...
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
