f588ff95c2a0f9deb857240988be32ad9b47c9ea
Dhanun R Nair <109233637+dhanunR@users.noreply.github.com>
2023-02-01 16:04:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,38 +10,10 @@ from pyspark.sql.types import StructType,StructField, StringType, IntegerType
 from pyspark.sql.types import ArrayType, DoubleType, BooleanType
 from pyspark.sql.functions import col,array_contains
 
-st.title("Quality Checker")
-st.write("This application will allow you to upload your dataset and run a quality check on it.")
-st.markdown("---")
 
-st.subheader("Upload your files here : ")
+st.write("# :tada: Hello Pyspark")
 
 spark, sc = _initialize_spark()
-        
-upload_data = st.file_uploader("Choose a CSV file", type = ['CSV'])
-if upload_data is not None:
-    read_data = spark.read.csv(upload_data)
+st.write("[Link to Spark window](http://localhost:4040)")
 
-#read_data = read_data
-#st.subheader('Data Quality Dashboard')
-#null_values = read_data.isnull().sum()/len(read_data)*100
-#st.progress(null_values)
-st.markdown("---")
-
-st.write("Dataset Overview : ")
-try:
-    number_of_rows = st.slider("No of rows:",5,10)
-    head = st.radio("View from Top or Bottom",('Head','Tail'))
-    if head=='Head':
-        st.dataframe(read_data.head(number_of_rows))
-    else:
-        st.dataframe(read_data.tail(number_of_rows))
-except:
-    st.info("KINDLY UPLOAD YOUR CSV FILE !!!")
-    st.stop()
-st.markdown("---")
-
-
-st.write("Dataset Schema ")
-schema = read_data.show()
-st.markdown("---")
+st.write("## Create RDD from a Python list")
