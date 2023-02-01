aae0645ce22fc45e8beb0f7fd8a93bbc511ac774
Dhanun R Nair <109233637+dhanunR@users.noreply.github.com>
2023-02-01 15:23:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,9 +11,7 @@ st.markdown("---")
 
 st.subheader("Upload your files here : ")
 
-spark = SparkSession.builder().master("local[1]")
-          .appName("SparkByExamples.com")
-          .getOrCreate()
+spark = SparkSession.builder().master("local[1]").appName("SparkByExamples.com").getOrCreate()
         
 upload_data = st.file_uploader("Choose a CSV file", type = ['CSV'])
 if upload_data is not None:
