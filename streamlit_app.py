c3e3eb83c9a77d72e367e150b55eeb5fa90001b3
Dhanun R Nair <109233637+dhanunR@users.noreply.github.com>
2023-02-01 14:56:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,29 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import streamlit.components.v1 as components
+import pandas as pd
+import numpy as np
+import pyspark
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.master("local[1]") \
+                    .appName('SparkByExamples.com') \
+                    .getOrCreate()
 
-"""
-# Welcome to Streamlit!
+st.title("Quality Checker")
+st.write("This application will allow you to upload your dataset and run a quality check on it.")
+st.markdown("---")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+st.subheader("Upload your files here : ")
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+upload_data = st.file_uploader("Choose a CSV file", type = ['CSV'])
+if upload_data is not None:
+    read_data = pd.read_csv(upload_data, encoding='latin-1',on_bad_lines='skip')
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+#read_data = read_data
+#st.subheader('Data Quality Dashboard')
+#null_values = read_data.isnull().sum()/len(read_data)*100
+#st.progress(null_values)
+st.markdown("---")
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+schema = read_data.printSchema()
 
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
