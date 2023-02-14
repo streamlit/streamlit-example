c695ea0df2f1c12d26be16412d1f41a839f330ab
Cullin Tripp <cullin.tripp@snowflake.com>
2023-02-14 16:36:49 -0500

    adding a password check

--- global_functions.py
+++ global_functions.py
@@ -20,3 +20,31 @@ def create_connection():
 @st.experimental_memo(ttl=300) # 5 minute object cache, or when query changes. Applies to all usage of this func.
 def cache_local_dataframe(local_dataframe):
     return pd.DataFrame(local_dataframe)
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["admin_password"] == st.secrets["admin_password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["admin_password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="admin_password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="admin_password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
