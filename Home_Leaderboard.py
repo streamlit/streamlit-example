e8d423dee6220747ad903be30ca53c713e117043
Cullin Tripp <cullin.tripp@snowflake.com>
2023-02-16 14:17:55 -0500

    adding last refreshed

--- Home_Leaderboard.py
+++ Home_Leaderboard.py
@@ -4,7 +4,7 @@ from snowflake.snowpark import Session
 import plotly.express as px
 from global_functions import get_session
 from snowflake.snowpark import functions as F
-
+from datetime import datetime
 from st_aggrid import AgGrid, GridUpdateMode
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 
@@ -20,9 +20,10 @@ st.write(f"# {tournament}")
 leaderboard_display_df = session.table('leaderboard_display_vw').filter(F.col('TOURNAMENT') == tournament).drop(['TOURNAMENT'])
 
 picks_df = session.table('POOL_COLUMNAR_VW').filter(F.col('TOURNAMENT') == tournament)[["ENTRY_NAME","GOLFER"]]
-selection_df = picks_df.group_by(F.col("GOLFER")).agg(F.count("ENTRY_NAME")).with_column_renamed(F.col("COUNT(ENTRY_NAME)"),"SELECTIONS")
+selection_df = picks_df.group_by(F.col("GOLFER")).agg(F.count("ENTRY_NAME")).with_column_renamed(F.col("COUNT(ENTRY_NAME)"),"SELECTIONS")  # type: ignore
 player_df = session.table('PLAYER_LEADERBOARD_VW').filter(F.col('EVENT_NAME') == tournament)
 player_leaderboard_df = selection_df.join(player_df,F.col("FULL_NAME") == F.col("GOLFER"))
+last_refresh = session.table('SCOREBOARD').filter(F.col('EVENT_NAME') == tournament).distinct().agg(F.max("LAST_UPDATED"))
 
 # PROTOTYPE FOR SELECT AND HIGHLIGHT FUNCTIONALITY
 # gd = GridOptionsBuilder.from_dataframe(leaderboard_display_df.to_pandas())
@@ -46,18 +47,15 @@ except TypeError:
 
 if leaderboard_display_df.count() > 0:
   with st.spinner('Getting yardages...'):
-      st.write('#### Member Leaderboard ')
+      st.write('#### Member Leaderboard',f""" - ```{last_refresh.collect()[0][0].strftime("%A %b %d %I:%M %p")}```""")
       st.dataframe(leaderboard_display_df)
       st.write(f"#### Cut = {tournament_cut_line}")
       st.write(f"All golfers who miss the cut will reflect as __{cut_player_score}__ for scoring")
       st.write("")
-      st.write('#### Selected Player Leaderboard ')
+      st.write('#### Selected Player Leaderboard')
       st.dataframe(player_leaderboard_df[['POSITION','GOLFER','TOTAL','THRU','SELECTIONS']],height=825)
-
 
 
 else:
   st.write(f"Whoops...the players are still warming up! {tournament} hasn't started yet...come back later!")
   st.image('assets/tiger-woods-gif.gif')
-
-
