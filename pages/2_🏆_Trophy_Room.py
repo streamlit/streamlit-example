0871e5416d3fe33e7465c028ce275415164059ff
Cullin Tripp <cullin.tripp@gmail.com>
2023-02-17 21:54:44 -0500

    trophy room

--- pages/2_🏆_Trophy_Room.py
+++ pages/2_🏆_Trophy_Room.py
@@ -4,9 +4,11 @@ from global_functions import get_session
 
 session = get_session()
 
-st.write('## 🏆 The Race For The 80 Yard Bombs Cup')
+st.title('Trophy Room')
 
 with st.spinner('Checking the trophy room!'):
+
+    st.write('## 🏆 The Race For The 80 Yard Bombs Cup')
     cup_standings_df = session.table('cup_standings')
     st.dataframe(cup_standings_df)
 
