e436717d9b97001f921b921c07bc6286cef7700e
Cullin Tripp <cullin.tripp@gmail.com>
2023-03-05 23:25:16 -0500

    adding Major handling for winners circle

--- pages/2_🏆_Trophy_Room.py
+++ pages/2_🏆_Trophy_Room.py
@@ -10,8 +10,12 @@ with st.spinner('Checking the trophy room!'):
 
     st.write('## 🏆 The Race For The 80 Yard Bombs Cup')
     cup_standings_df = session.table('cup_standings')
-    st.dataframe(cup_standings_df)
+    st.dataframe(cup_standings_df.to_pandas().set_index('OVERALL_RANK'))
 
     st.write("## 🥇 Winner's Circle")
-    winners_circle_df = session.table('winners_circle_vw')
+    winners_circle_df = session.table('winners_circle_vw').to_pandas()
+    winners_circle_df['TOURNAMENT'].replace({'The Masters': 'The Masters 🏅',
+                                             'PGA Championship': 'PGA Championship 🏅',
+                                             'U.S. Open': 'U.S. Open 🏅',
+                                             'The Open Championship': 'The Open Championship 🏅'}, inplace=True)
     st.dataframe(winners_circle_df)
