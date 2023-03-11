6d4bbf45fdebebbe9d1dd0874e699bdf4f10214d
paulomiguelmg <paulomiguelmg@gmail.com>
2023-03-11 18:35:55 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,22 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
 import streamlit as st
+import forex_python.converter as fpc
 
-"""
-# Welcome to Streamlit!
+currencies = ['USD', 'EUR', 'JPY', 'GBP', 'AUD', 'CAD', 'CHF']
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+st.title('Conversor de moedas')
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+base_currency = st.selectbox('Selecione a moeda base:', currencies)
+target_currency = st.selectbox('Selecione a moeda de destino:', currencies)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+amount = st.number_input('Insira a quantidade a ser convertida:', value=1.00)
 
+result = fpc.CurrencyRates().convert(base_currency, target_currency, amount)
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+st.write(f'{amount:.2f} {base_currency} = {result:.2f} {target_currency}')
