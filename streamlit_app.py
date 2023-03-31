05746d66267271bb8cff10d7fc1bb5f7226c0826
Kumru Orkun <62724247+kumruo@users.noreply.github.com>
2023-03-31 16:22:14 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,6 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-import plotly.graph_objs as go
-
 # The code below is for the title and logo for this page.
 st.set_page_config(page_title="Cross Feedback Meetings", page_icon="🥡")
 
@@ -60,107 +56,7 @@ For more information abaout d3c calture please visit [this wiki page](https://co
 
     st.write("")
 
-# loading dataset XLSX
-df = pd.read_excel("datasets/relay-foods.xlsx", sheet_name=1)
 
-df["OrderPeriod"] = df.OrderDate.apply(lambda x: x.strftime("%Y-%m"))
-df.set_index("UserId", inplace=True)
-
-df["CohortGroup"] = (
-    df.groupby(level=0)["OrderDate"].min().apply(lambda x: x.strftime("%Y-%m"))
-)
-df.reset_index(inplace=True)
-# df.head()
-
-# loading dataset CSV
-relay_foods_csv = pd.read_csv("datasets/relay-foods.csv")
-
-with st.expander("Show the `Food` dataframe"):
-    st.write(relay_foods_csv)
-
-grouped = df.groupby(["CohortGroup", "OrderPeriod"])
-
-# count the unique users, orders, and total revenue per Group + Period
-cohorts = grouped.agg(
-    {"UserId": pd.Series.nunique, "OrderId": pd.Series.nunique, "TotalCharges": np.sum}
-)
-
-# make the column names more meaningful
-cohorts.rename(columns={"UserId": "TotalUsers", "OrderId": "TotalOrders"}, inplace=True)
-cohorts.head()
-
-
-def cohort_period(df):
-    """
-    Creates a `CohortPeriod` column, which is the Nth period based on the user's first purchase.
-    """
-    df["CohortPeriod"] = np.arange(len(df)) + 1
-    return df
-
-
-with st.form("my_form"):
-    # st.write("Inside the form")
-    # slider_val = st.slider("Form slider")
-    # checkbox_val = st.checkbox("Form checkbox")
-
-    cole, col1, cole = st.columns([0.1, 1, 0.1])
-
-    with col1:
-
-        TotalCharges_slider = st.slider(
-            "Total Charges (in $)", step=50, min_value=2, max_value=690
-        )
-        # Every form must have a submit button.
-
-    submitted = st.form_submit_button("Refine results")
-
-# st.write("slider", slider_val, "checkbox", checkbox_val)
-
-cohorts = cohorts[cohorts["TotalCharges"] > TotalCharges_slider]
-
-cohorts = cohorts.groupby(level=0).apply(cohort_period)
-cohorts.head()
-
-# reindex the DataFrame
-cohorts.reset_index(inplace=True)
-cohorts.set_index(["CohortGroup", "CohortPeriod"], inplace=True)
-
-# create a Series holding the total size of each CohortGroup
-cohort_group_size = cohorts["TotalUsers"].groupby(level=0).first()
-cohort_group_size.head()
-
-user_retention = cohorts["TotalUsers"].unstack(0).divide(cohort_group_size, axis=1)
-user_retention.head(10)
-
-user_retention[["2009-06", "2009-07", "2009-08"]].plot(figsize=(10, 5))
-plt.title("Cohorts: User Retention")
-plt.xticks(np.arange(1, 12.1, 1))
-plt.xlim(1, 12)
-plt.ylabel("% of Cohort Purchasing")
-cohorts["TotalUsers"].head()
-
-user_retention = cohorts["TotalUsers"].unstack(0).divide(cohort_group_size, axis=1)
-user_retention.head(10)
-
-user_retention[["2009-06", "2009-07", "2009-08"]].plot(figsize=(10, 5))
-plt.title("Cohorts: User Retention")
-plt.xticks(np.arange(1, 12.1, 1))
-plt.xlim(1, 12)
-plt.ylabel("% of Cohort Purchasing")
-
-user_retention = user_retention.T
-
-fig = go.Figure()
-
-fig.add_heatmap(
-    x=user_retention.columns,
-    y=user_retention.index,
-    z=user_retention,
-    # colorscale="Reds",
-    # colorscale="Sunsetdark",
-    colorscale="Redor"
-    # colorscale="Viridis",
-)
 
 fig.update_layout(title_text="Monthly cohorts showing retention rates", title_x=0.5)
 fig.layout.xaxis.title = "Cohort Group"
