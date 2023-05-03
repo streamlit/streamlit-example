78e3b103a9d3286e97028dde47f351093b0861e4
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-05-03 10:51:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,9 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# Welcome to Streamlit IC TEAM!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+Editing `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
