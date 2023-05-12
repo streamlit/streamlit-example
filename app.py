60951a0b06d19641104eb0e088cef4e16d2a7e04
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-05-12 13:54:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -77,9 +77,9 @@ select = st.selectbox('Injured people', ['Pedestrian','Cyclists','Motorists'])
 
 if select == 'Pedestrians':
     st.write(original_data.query("injured_pedestrians >= 1") [["on_street_name","injured_pedestrians"]].sort_values(by=['injured_pedestrians'], ascending=False).dropna(how='any')[:5]
-elif select == 'Cyclists':
+if select == 'Cyclists':
     st.write(original_data.query("injured_cyclists >= 1") [["on_street_name","injured_cyclists"]].sort_values(by=['injured_cyclists'], ascending=False).dropna(how='any')[:5]
-else:
+if select == 'Motorists':
     st.write(original_data.query("injured_motorists >= 1") [["on_street_name","injured_motorists"]].sort_values(by=['injured_motorists'], ascending=False).dropna(how='any')[:5]
 
 
