269bbcba2c73b144cb99c10109c8a91b7193568f
Philipp Schoenbucher <philipp@previ.se>
2023-05-22 16:01:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,61 @@
 import streamlit as st
 import sass
 import math
-from datetime import date
+from datetime import date, datetime, timezone
 import base64
+
+
+#########################################################################################################################
+# Layout and Previse look and feel
+
+# getting the image file for the logo - encoding it ourselves because streamlit is really blurring it too much...
+@st.cache_data()
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
+# here is where the logo magic happens
+def set_png_as_top_rhs_logo(png_file = "./logo_title.png"):
+# Load the compiled CSS from the SCSS file
+    with open("./style.scss") as f:
+        st.markdown(f"<style>{sass.compile(string=f.read())}</style>", unsafe_allow_html=True)
+
+    bin_str = get_base64_of_bin_file(png_file)
+    footer_bg_img = """
+                <style>
+                .css-1dp5vir { 
+                    position: absolute;
+                    top: 0px;
+                    right: 0px;
+                    left: 0px;
+                    height: 0.125rem;
+                    background-image: linear-gradient(90deg, #fedb00, #fedb00);
+                    z-index: 999990;
+                }
+                footer { 
+                   visibility: hidden;
+                }
+                header{ 
+                    padding: 0.4rem;
+                }
+                header:after {
+                    display: block;
+                    content: "";
+                    width: 94%%;
+                    height: 80%%;
+                    position: absolute;
+                    background-image: url("data:image/png;base64,%s");
+                    background-position-x: 100%%;
+                    background-position-y: 0%%;
+                    background-repeat: no-repeat;
+                    background-size: contain;
+                    background_attachment: fixed;
+                    }
+                </style>
+                """ % bin_str
+    st.markdown(footer_bg_img, unsafe_allow_html=True)
 
 
 # configuring the page and menu items
@@ -18,13 +71,10 @@ st.set_page_config(
     }
 )
 
-from pages.payopt_tools.ps_tools import set_png_as_top_rhs_logo
-set_png_as_top_rhs_logo()
-
 
 # ========================================================================================================
 
-
+set_png_as_top_rhs_logo()
 
 ref_date = date.fromisoformat('2023-02-10')
 
