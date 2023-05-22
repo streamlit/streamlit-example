70c2ad85c5c475dec4ed2a33d237b90682240126
Philipp Schoenbucher <philipp@previ.se>
2023-05-22 15:58:42 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,61 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+import sass
+import math
+from datetime import date
+import base64
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# configuring the page and menu items
+st.set_page_config(
+    page_title="The Previse supplier categorisation tool",
+    page_icon="",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://previ.se',
+        'Report a bug': "https://previ.se",
+        'About': "(c) 2023 Previse Ltd."
+    }
+)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+from pages.payopt_tools.ps_tools import set_png_as_top_rhs_logo
+set_png_as_top_rhs_logo()
 
-    points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+# ========================================================================================================
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+
+
+ref_date = date.fromisoformat('2023-02-10')
+
+KPIs = {
+    "Reference Date" : ref_date.isoformat(),
+    "Total Payables outstanding" :	"{:,}".format(767462641),
+    "Total transaction costs":	"{:,}".format(1840107),
+    "Average Working Capital":	"{:,}".format(154159060),
+    "DSO" :	10 
+}
+
+# Define the main function to run the app
+def main():
+    # Set the app title
+    st.title("Payment Strategy Optimiser")
+
+    st.header("KPIs")
+    # Calculate the number of rows needed to display all the KPIs
+    num_rows = math.ceil(len(KPIs) / 3)
+
+    # Create a Streamlit column layout for each row
+    cols = [st.columns(3) for _ in range(num_rows)]
+
+    # Iterate over the KPIs and add them to the columns
+    for i, (kpi_name, kpi_value) in enumerate(KPIs.items()):
+        row_idx = i // 3
+        col_idx = i % 3
+        kpi_col = cols[row_idx][col_idx]
+        
+        # Add the KPI as a Streamlit metric to the column
+        kpi_col.metric(kpi_name, kpi_value)
+# Run the app
+if __name__ == "__main__":
+    main()
