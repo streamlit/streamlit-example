671de96d2c679ac8e00b70a193116f4b150d4c02
dozzky <57999040+dozzky@users.noreply.github.com>
2023-05-22 17:21:47 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,9 @@ with open('new_model.pkl', 'rb') as alc_model_pkl:
 with open('drug_model.pkl', 'rb') as drug_model_pkl:
     lr_drug = pd.read_pickle(drug_model_pkl)
 
+f1 = 0
+f2 = 0
+    
 # Данные введенные пользователем
 unseen = st.slider("Количество безработных (в тыс. человек)", min_value = 20.0, max_value = 200.0, step = 0.1)
 decimal = st.slider("Знаки после запятой", min_value = 0, max_value = 10, step = 1)
@@ -25,14 +28,14 @@ result_drug = lr_drug.predict(X_test_sm)[0]
 
 # Вывод
 if (result_alc > 0) and (result_drug > 0):
-    st.metric(label="Количество алкоголиков", value=str(result_alc)[:(len(str(int(result_alc)))+decimal+1)], delta="1.2 °F")
-    st.metric(label="Количество наркоманов", value=str(str(result_drug)[:(len(str(int(result_drug)))+decimal+1)]), delta="1.2 °F")
+    st.metric(label="Количество алкоголиков", value=str(result_alc)[:(len(int(result_alc))+decimal+1)], delta="1.2")
+    st.metric(label="Количество наркоманов", value=str(str(result_drug)[:(len(str(int(result_drug)))+decimal+1)]), delta="1.2")
     source = pd.DataFrame({
     'a': ['Алкаши', 'Наркоши'],
     'b': [result_alc, result_drug]})
 else:
-    st.metric(label="Количество алкоголиков", value=0, delta="1.2 °F")
-    st.metric(label="Количество наркоманов", value=0, delta="1.2 °F")
+    st.metric(label="Количество алкоголиков", value=0, delta="1.2")
+    st.metric(label="Количество наркоманов", value=0, delta="1.2")
     source = pd.DataFrame({
     'a': ['Алкаши', 'Наркоши'],
     'b': [0, 0]})
