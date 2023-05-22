a73346d5c2f2d228b3e91c8c14540e9b0def36fc
dozzky <57999040+dozzky@users.noreply.github.com>
2023-05-22 18:50:51 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,7 @@ else:
 
 st.altair_chart(alt.Chart(pd.DataFrame(source), height = 500, width = 500)
                 .mark_bar()
-                .encode(x='', y='Количество людей (в тыс.)'))
+                .encode(x=' ', y='Количество людей (в тыс.)'))
 
 
 
