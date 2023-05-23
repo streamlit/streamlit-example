8d5099899e04c3e76b6c525e7063665515979061
dozzky <57999040+dozzky@users.noreply.github.com>
2023-05-23 22:13:24 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,33 +19,33 @@ with open('models/gboost_drug.pkl', 'rb') as drug_model2_pkl:
     
 image = Image.open('media/alcohol.jpg')
 
-st.header("Настраиваемые данные")   
+#st.header("Настраиваемые данные")   
 # Данные введенные пользователем
-unseen = st.slider("Количество безработных (в тыс. человек)", min_value = 20.0, max_value = 200.0, step = 0.1)
-decimal = st.slider("Знаки после запятой", min_value = 0, max_value = 10, step = 1)
+#unseen = st.slider("Количество безработных (в тыс. человек)", min_value = 20.0, max_value = 200.0, step = 0.1)
+#decimal = st.slider("Знаки после запятой", min_value = 0, max_value = 10, step = 1)
 
 # Прогноз
-X_test_lr = [[float(1.0)], [float(unseen)]]
-X_test_lr = np.squeeze(X_test_lr)
-result_alc_model1 = lr_alc.predict(X_test_lr)[0]
-result_drug_model1 = lr_drug.predict(X_test_lr)[0]
 
-X_test_gb = [[float(unseen)]]
-X_test_gb = np.squeeze(X_test_gb)
-X_test_gb = X_test_gb.reshape(-1,1)
-result_alc_model2 = gb_alc.predict(X_test_gb)[0]
-result_drug_model2 = gb_drug.predict(X_test_gb)[0]
+#X_test_gb = [[float(unseen)]]
+#X_test_gb = np.squeeze(X_test_gb)
+#X_test_gb = X_test_gb.reshape(-1,1)
+#result_alc_model2 = gb_alc.predict(X_test_gb)[0]
+#result_drug_model2 = gb_drug.predict(X_test_gb)[0]
 
 @st.cache_data
-def delta_calc():
+def delta_calc(x):
     a1 = result_alc_model1
     b1 = result_drug_model1
     a2 = result_alc_model2
     b2 = result_drug_model2
-    c = unseen
-    return a1, b1, a2, b2, c
+    c1 = unseen_lr
+    c2 = unseen_gb
+    if x == "lr":
+        return a1, b1, c1
+    else:
+        return a2, b2, c2
 
-delta_alc_model1, delta_drug_model1, delta_alc_model2, delta_drug_model2, delta_unseen_model= delta_calc()
+#delta_alc_model1, delta_drug_model1, delta_alc_model2, delta_drug_model2, delta_unseen_lr, delta_unseen_gb= delta_calc()
 
 
 st.write("Нажмите на кнопку, затем укажите сверху данные (количество безработных), которые хотите сравнивать.")
@@ -57,27 +57,38 @@ if st.button("Сравнить"):
 tab_model_1, tab_model_2 = st.tabs(["Линейная регрессия", "Градиентный бустинг"])
 
 with tab_model_1:
-
+    
+    st.header("Настраиваемые данные")   
+    # Данные введенные пользователем
+    unseen_lr = st.slider("Количество безработных (в тыс. человек)", min_value = 20.0, max_value = 200.0, step = 0.1)
+    decimal_lr = st.slider("Знаки после запятой", min_value = 0, max_value = 10, step = 1)
+    # Прогноз
+    X_test_lr = [[float(1.0)], [float(unseen_lr)]]
+    X_test_lr = np.squeeze(X_test_lr)
+    result_alc_model1 = lr_alc.predict(X_test_lr)[0]
+    result_drug_model1 = lr_drug.predict(X_test_lr)[0]
+    delta_alc_model1, delta_drug_model1, delta_unseen_lr = delta_calc('lr')
+    # Данные введенные пользователем
     st.header("Метрика")    
     col1, col2, col3= st.columns(3)    
 
     if (result_alc_model1 > 0) and (result_drug_model1 > 0):
-        col1.metric(label = "Количество алкоголиков", value = str(result_alc_model1)[:(len(str(int(result_alc_model1))) + decimal + 1)], delta = str(result_alc_model1-delta_alc_model1)[:(len(str(int(result_alc_model1-delta_alc_model1))) + decimal + 1)], delta_color = "inverse")
-        col2.metric(label = "Количество наркоманов", value = str(result_drug_model1)[:(len(str(int(result_drug_model1))) + decimal + 1)], delta = str(result_drug_model1-delta_drug_model1)[:(len(str(int(result_drug_model1-delta_drug_model1))) + decimal + 1)], delta_color = "inverse")
-        col3.metric(label = "Количество безработных", value = str(unseen)[:(len(str(int(unseen))) + decimal + 1)], delta = str(unseen-delta_unseen_model)[:(len(str(int(unseen-delta_unseen_model))) + decimal + 1)], delta_color = "inverse")
+        col1.metric(label = "Количество алкоголиков", value = str(result_alc_model1)[:(len(str(int(result_alc_model1))) + decimal_lr + 1)], delta = str(result_alc_model1-delta_alc_model1)[:(len(str(int(result_alc_model1-delta_alc_model1))) + decimal_lr + 1)], delta_color = "inverse")
+        col2.metric(label = "Количество наркоманов", value = str(result_drug_model1)[:(len(str(int(result_drug_model1))) + decimal_lr + 1)], delta = str(result_drug_model1-delta_drug_model1)[:(len(str(int(result_drug_model1-delta_drug_model1))) + decimal_lr + 1)], delta_color = "inverse")
+        col3.metric(label = "Количество безработных", value = str(unseen_lr)[:(len(str(int(unseen_lr))) + decimal_lr + 1)], delta = str(unseen_lr-delta_unseen_lr)[:(len(str(int(unseen_lr-delta_unseen_lr))) + decimal_lr + 1)], delta_color = "inverse")
         source1_model1 = pd.DataFrame({
         'Прогноз': ['Безраб.', 'Алк.', 'Нарк.'],
-        'Количество людей в тыс': [unseen, result_alc_model1, result_drug_model1]})
+        'Количество людей в тыс': [unseen_lr, result_alc_model1, result_drug_model1]})
         source2_model1 = pd.DataFrame({
         'Прогноз': ['Алк.', 'Нарк.'],
         'Количество людей в тыс': [result_alc_model1, result_drug_model1]})
     else:
-        col1.metric(label = "Количество алкоголиков", value = 0, delta = str(result_alc_model1-delta_alc_model1)[:(len(str(int(result_alc_model1-delta_alc_model1))) + decimal + 1)], delta_color = "inverse")
-        col2.metric(label = "Количество наркоманов", value = 0, delta = str(result_drug_model1-delta_drug_model1)[:(len(str(int(result_drug_model1-delta_drug_model1))) + decimal + 1)], delta_color = "inverse")
-        col3.metric(label = "Количество безработных", value = str(unseen)[:(len(str(int(unseen))) + decimal + 1)], delta = str(unseen-delta_unseen_model)[:(len(str(int(unseen-delta_unseen_model))) + decimal + 1)], delta_color = "inverse")
+        col1.metric(label = "Количество алкоголиков", value = 0, delta = str(result_alc_model1-delta_alc_model1)[:(len(str(int(result_alc_model1-delta_alc_model1))) + decimal_lr + 1)], delta_color = "inverse")
+        col2.metric(label = "Количество наркоманов", value = 0, delta = str(result_drug_model1-delta_drug_model1)[:(len(str(int(result_drug_model1-delta_drug_model1))) + decimal_lr + 1)], delta_color = "inverse")
+        col3.metric(label = "Количество безработных", value = str(unseen_lr)[:(len(str(int(unseen_lr))) + decimal_lr + 1)], delta = str(unseen_lr-delta_unseen_lr)[:(len(str(int(unseen_lr-delta_unseen_lr))) + decimal_lr + 1)], delta_color = "inverse")
         source1_model1 = pd.DataFrame({
         'Прогноз': ['Безраб.', 'Алк.', 'Нарк.'],
-        'Количество людей в тыс': [unseen, 0, 0]})
+        'Количество людей в тыс': [unseen_lr, 0, 0]})
         source2_model1 = pd.DataFrame({
         'Прогноз': ['Алк.', 'Нарк.'],
         'Количество людей в тыс': [0, 0]})
@@ -91,26 +102,38 @@ with tab_model_1:
 
 with tab_model_2:
     
+    st.header("Настраиваемые данные")   
+    # Данные введенные пользователем
+    unseen_gb = st.slider("Количество безработных (в тыс. человек)", min_value = 20.0, max_value = 200.0, step = 0.1)
+    decimal_gb = st.slider("Знаки после запятой", min_value = 0, max_value = 10, step = 1)
+    
+    # Прогноз
+    X_test_gb = [[float(unseen_gb)]]
+    X_test_gb = np.squeeze(X_test_gb)
+    X_test_gb = X_test_gb.reshape(-1,1)
+    result_alc_model2 = gb_alc.predict(X_test_gb)[0]
+    result_drug_model2 = gb_drug.predict(X_test_gb)[0]
+    delta_alc_model2, delta_drug_model2, delta_unseen_gb= delta_calc('gb')
     st.header("Метрика")    
     col1, col2, col3= st.columns(3)    
 
     if (result_alc_model2 > 0) and (result_drug_model2 > 0):
-        col1.metric(label = "Количество алкоголиков", value = str(result_alc_model2)[:(len(str(int(result_alc_model2))) + decimal + 1)], delta = str(result_alc_model2-delta_alc_model2)[:(len(str(int(result_alc_model2-delta_alc_model2))) + decimal + 1)], delta_color = "inverse")
-        col2.metric(label = "Количество наркоманов", value = str(result_drug_model2)[:(len(str(int(result_drug_model2))) + decimal + 1)], delta = str(result_drug_model2-delta_drug_model2)[:(len(str(int(result_drug_model2-delta_drug_model2))) + decimal + 1)], delta_color = "inverse")
-        col3.metric(label = "Количество безработных", value = str(unseen)[:(len(str(int(unseen))) + decimal + 1)], delta = str(unseen-delta_unseen_model)[:(len(str(int(unseen-delta_unseen_model))) + decimal + 1)], delta_color = "inverse")
+        col1.metric(label = "Количество алкоголиков", value = str(result_alc_model2)[:(len(str(int(result_alc_model2))) + decimal_gb + 1)], delta = str(result_alc_model2-delta_alc_model2)[:(len(str(int(result_alc_model2-delta_alc_model2))) + decimal_gb + 1)], delta_color = "inverse")
+        col2.metric(label = "Количество наркоманов", value = str(result_drug_model2)[:(len(str(int(result_drug_model2))) + decimal_gb + 1)], delta = str(result_drug_model2-delta_drug_model2)[:(len(str(int(result_drug_model2-delta_drug_model2))) + decimal_gb + 1)], delta_color = "inverse")
+        col3.metric(label = "Количество безработных", value = str(unseen_gb)[:(len(str(int(unseen_gb))) + decimal_gb + 1)], delta = str(unseen_gb-delta_unseen_gb)[:(len(str(int(unseen-delta_unseen_gb))) + decimal_gb + 1)], delta_color = "inverse")
         source1_model2 = pd.DataFrame({
         'Прогноз': ['Безраб.', 'Алк.', 'Нарк.'],
-        'Количество людей в тыс': [unseen, result_alc_model2, result_drug_model2]})
+        'Количество людей в тыс': [unseen_gb, result_alc_model2, result_drug_model2]})
         source2_model2 = pd.DataFrame({
         'Прогноз': ['Алк.', 'Нарк.'],
         'Количество людей в тыс': [result_alc_model2, result_drug_model2]})
     else:
-        col1.metric(label = "Количество алкоголиков", value = 0, delta = str(result_alc_model2-delta_alc_model2)[:(len(str(int(result_alc_model2-delta_alc_model2))) + decimal + 1)], delta_color = "inverse")
-        col2.metric(label = "Количество наркоманов", value = 0, delta = str(result_drug_model2-delta_drug_model2)[:(len(str(int(result_drug_model2-delta_drug_model2))) + decimal + 1)], delta_color = "inverse")
-        col3.metric(label = "Количество безработных", value = str(unseen)[:(len(str(int(unseen))) + decimal + 1)], delta = str(unseen-delta_unseen_model)[:(len(str(int(unseen-delta_unseen_model))) + decimal + 1)], delta_color = "inverse")
+        col1.metric(label = "Количество алкоголиков", value = 0, delta = str(result_alc_model2-delta_alc_model2)[:(len(str(int(result_alc_model2-delta_alc_model2))) + decimal_gb + 1)], delta_color = "inverse")
+        col2.metric(label = "Количество наркоманов", value = 0, delta = str(result_drug_model2-delta_drug_model2)[:(len(str(int(result_drug_model2-delta_drug_model2))) + decimal_gb + 1)], delta_color = "inverse")
+        col3.metric(label = "Количество безработных", value = str(unseen_gb)[:(len(str(int(unseen_gb))) + decimal_gb + 1)], delta = str(unseen_gb-delta_unseen_gb)[:(len(str(int(unseen_gb-delta_unseen_gb))) + decimal_gb + 1)], delta_color = "inverse")
         source1_model2 = pd.DataFrame({
         'Прогноз': ['Безраб.', 'Алк.', 'Нарк.'],
-        'Количество людей в тыс': [unseen, 0, 0]})
+        'Количество людей в тыс': [unseen_gb, 0, 0]})
         source2_model2 = pd.DataFrame({
         'Прогноз': ['Алк.', 'Нарк.'],
         'Количество людей в тыс': [0, 0]})
