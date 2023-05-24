b09679337d78f8f086bdbbd85e410ca0163f6ec7
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-05-24 14:58:20 +0200

    Update R.py

--- R.py
+++ R.py
@@ -4,9 +4,12 @@ import numpy as np
 import pydeck as pdk
 import plotly.express as px
 
-DATA_URL = (
+#DATA_URL = (
 "/home/rhyme/Desktop/Project/Motor_Vehicle_Collisions_-_Crashes.csv"
 )
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+  DATA_URL = pd.read_csv(uploaded_file)
 
 st.title("Road Accident in France")
 st.markdown("This application is a Streamlit dashboard that can be use to analyze road accident in France🗼🥐🇫🇷🥖🚗💥🚙")
