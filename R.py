bf0dc2601b193958cacc66774ef098183126adfe
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-05-24 15:07:59 +0200

    Update R.py

--- R.py
+++ R.py
@@ -7,7 +7,7 @@ import plotly.express as px
 #DATA_URL = (
 #"/home/rhyme/Desktop/Project/Motor_Vehicle_Collisions_-_Crashes.csv"
 #)
-uploaded_file = st.file_uploader("Choose a file")
+DATA_URL = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   DATA_URL = pd.read_csv(uploaded_file)
 
