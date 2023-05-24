eae077e29a383ed307e42918e5d7b52405451a32
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-05-24 15:22:50 +0200

    Update R.py

--- R.py
+++ R.py
@@ -7,10 +7,10 @@ import plotly.express as px
 #DATA_URL = (
 #"/home/rhyme/Desktop/Project/Motor_Vehicle_Collisions_-_Crashes.csv"
 #)
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-  DATA_URL = pd.read_csv(uploaded_file).sample(n=30000)
-st.write (DATA_URL)
+DATA_URL = (
+"C:\\Users\\SDI\\OneDrive - Allianz\\Mes Documents\\BureauFiles\\EA 2022\\Training\Datascientist\Coursera\\Motor_Vehicle_Collisions_-_Crashes.csv"
+)
+
 st.title("Road Accident in France")
 st.markdown("This application is a Streamlit dashboard that can be use to analyze road accident in France🗼🥐🇫🇷🥖🚗💥🚙")
 
