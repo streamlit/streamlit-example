8d37022ca124e25e1a82d82b9c2f319b71acd867
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-05-31 00:05:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,97 +1,55 @@
-import streamlit as st
-import pandas as pd
 import numpy as np
-import pydeck as pdk
-import plotly.express as px
+import pandas as pd
+import streamlit as st
+from pandas_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
 
-#DATA_URL = (
-#    "/Users/SDI/OneDrive - Allianz/Desktop/ACM/Motor_Vehicle_Collisions_-_Crashes.csv"
-#)
-with st.sidebar.header('1. Upload your CSV file'):
-    uploaded_file = st.sidebar.file_uploader("Choose your file")
+# Web App Title
+st.markdown('''
+# **The EDA App**
+
+This is the **EDA App** created in Streamlit using the **pandas-profiling** library.
+
+**Credit:** App built in `Python`
+
+---
+''')
+
+# Upload CSV data
+with st.sidebar.header('1. Upload your CSV data'):
+    uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+    st.sidebar.markdown("""
+[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv)
+""")
+
+# Pandas Profiling Report
 if uploaded_file is not None:
-  #@st.cache
-  def load_csv():
-      csv = pd.read_csv(uploaded_file)
-      return csv
-  DATA_URL = load_csv()
-  DATA_URL = DATA_URL.sample(n=30000)
-  
-  
-
-  st.title("Road Accident in France")
-  st.markdown("This application is a Streamlit dashboard that can be use to analyze road accident in France🗼🥐🇫🇷🥖🚗💥🚙")
-
-  @st.cache(persist=True)
-  def load_data(nrows):
-         data = pd.read_csv(DATA_URL,nrows=nrows, parse_dates=[['CRASH_DATE', 'CRASH_TIME']])
-         data.dropna(subset=['LATITUDE', 'LONGITUDE'], inplace=True)
-         lowercase = lambda x: str(x).lower()
-         data.rename(lowercase, axis='columns', inplace=True)
-         data.rename(columns={'crash_date_crash_time': 'date/time'}, inplace=True)
-         return data
-
-
-  data = DATA_URL
-  original_data = data
-
-  st.header("Where are the most people injured in France?")
-  injured_people = st.slider("Number of person injured in road accident",0, 19)
-  st.map(data.query(''injured_persons' >= @injured_people')[["latitude","longitude"]].dropna(how="any"))
-
-
-  st.header("How many road accident during a given time of the day?")
-  hour = st.slider("Hour to look at", 0, 23)
-  data = data[data['date/time'].dt.hour == hour]
-
-  st.markdown("road accident between %i:00 and %i:00" % (hour, (hour + 1) % 24))
-  midpoint = (np.average(data['latitude']), np.average(data['longitude']))
-
-  st.write(pdk.Deck(
-       map_style="mapbox://styles/mapbox/light-v9",
-       initial_view_state={
-           "latitude": midpoint[0],
-           "longitude": midpoint[1],
-           "zoom": 11,
-           "pitch": 50,
-       },
-       layers=[
-           pdk.Layer(
-           "HexagonLayer",
-           data=data[['date/time','latitude','longitude']],
-           get_position=['longitude','latitude'],
-           radius=100,
-           extruded=True,
-           pickable=True,
-           elevation_scale=4,
-           elevation_range=[0,1000],
-           ),
-       ],
-  ))
-
-  st.subheader("Breakdown by minute between %i:00 and %i:00" % (hour, (hour + 1) %24))
-  filtered = data[
-       (data['date/time'].dt.hour >= hour) & (data['date/time'].dt.hour < (hour +1))
-  ]
-  hist = np.histogram(filtered['date/time'].dt.minute, bins=60, range=(0,60))[0]
-  chart_data = pd.DataFrame({'minute':range(60), 'crashes':hist})
-  fig = px.bar(chart_data, x='minute',y='crashes', hover_data=['minute','crashes'], height=400)
-  st.write(fig)
-
-  st.header("Top 5 dangerous city by injury type")
-  select = st.selectbox('Injured people', ['Pedestrian','Cyclists','Motorists'])
-
-  if select == 'Pedestrians':
-      st.write(original_data.query("injured_pedestrians >= 1") [["on_street_name","injured_pedestrians"]].sort_values(by=['injured_pedestrians'], ascending=False).dropna(how='any')[:5])
-  if select == 'Cyclists':
-      st.write(original_data.query("injured_cyclists >= 1") [["on_street_name","injured_cyclists"]].sort_values(by=['injured_cyclists'], ascending=False).dropna(how='any')[:5])
-  if select == 'Motorists':
-      st.write(original_data.query("injured_motorists >= 1") [["on_street_name","injured_motorists"]].sort_values(by=['injured_motorists'], ascending=False).dropna(how='any')[:5])
-
-
-
-
-
-  if st.checkbox("Show Raw Data", False):
-     st.subheader('Raw Data')
-     st.write(data)
+    @st.cache
+    def load_csv():
+        csv = pd.read_csv(uploaded_file)
+        return csv
+    df = load_csv()
+    pr = ProfileReport(df, explorative=True)
+    st.header('**Input DataFrame**')
+    st.write(df)
+    st.write('---')
+    st.header('**Pandas Profiling Report**')
+    st_profile_report(pr)
+else:
+    st.info('Awaiting for CSV file to be uploaded.')
+    if st.button('Press to use Example Dataset'):
+        # Example data
+        @st.cache
+        def load_data():
+            a = pd.DataFrame(
+                np.random.rand(100, 5),
+                columns=['a', 'b', 'c', 'd', 'e']
+            )
+            return a
+        df = load_data()
+        pr = ProfileReport(df, explorative=True)
+        st.header('**Input DataFrame**')
+        st.write(df)
+        st.write('---')
+        st.header('**Pandas Profiling Report**')
+        st_profile_report(pr)
