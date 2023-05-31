589fbc1b1728adfeb3093ed2f8ecaa23a5731bfa
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-06-01 00:34:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,10 @@ Bonsoir nia mouah !!!!!
 
 
 """
-st.image('https://images.app.goo.gl/qwTsY83KPUPpWGWJA')
+#st.image('https://images.app.goo.gl/qwTsY83KPUPpWGWJA')
+
+from PIL import Image
+st.image("https://upload.wikimedia.org/wikipedia/commons/2/2f/Multi_vehicle_accident_-_M4_Motorway%2C_Sydney%2C_NSW_%288076208846%29.jpg",
+            width=600 # Manually Adjust the width of the image as per requirement
+        )
 
