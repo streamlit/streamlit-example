5526d0cd296fa086f7515e55e29f7564c9fcb3b1
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-05-31 19:45:14 +0200

    Update R.py

--- R.py
+++ R.py
@@ -15,7 +15,7 @@ st.title("Road Accident in France")
 st.markdown("This application is a Streamlit dashboard that can be use to analyze road accident in France🗼🥐🇫🇷🥖🚗💥🚙")
 
 from PIL import Image
-st.image("https://s3-us-west-2.amazonaws.com/uw-s3-cdn/wp-content/uploads/sites/6/2017/11/04133712/waterfall.jpg",
+st.image("https://upload.wikimedia.org/wikipedia/commons/2/2f/Multi_vehicle_accident_-_M4_Motorway%2C_Sydney%2C_NSW_%288076208846%29.jpg",
             width=400 # Manually Adjust the width of the image as per requirement
         )
 st.video("https://www.youtube.com/shorts/X5CYrFKcvis")
