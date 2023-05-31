09207aefdeae421b31ad98e0fd051989111f05c4
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-06-01 00:41:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ Bonsoir nia mouah !!!!!
 from PIL import Image
 st.image("https://www.cleverfiles.com/howto/wp-content/uploads/2018/03/minion.jpg",
             width=600 # Manually Adjust the width of the image as per requirement
+        )
 
 
 
