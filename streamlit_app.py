e27687e498bd3b54c5d1a418c6904f7ea1703798
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-06-01 00:28:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,8 @@ import streamlit as st
 # Welcome to YAKOUT TEAM!
 
 Bonsoir nia mouah !!!!!
-"""
 
+
+"""
 st.image('https://en.wikipedia.org/wiki/Belle_%28Beauty_and_the_Beast%29#/media/File:Belle_disney.png')
 
