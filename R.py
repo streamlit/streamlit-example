a70813ea1453a6a9ea59de1c686fb530451e2c87
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-05-31 19:27:05 +0200

    Update R.py

--- R.py
+++ R.py
@@ -17,7 +17,8 @@ st.markdown("This application is a Streamlit dashboard that can be use to analyz
 from PIL import Image
 image = Image.open('sunrise.jpg')
 st.image("https://s3-us-west-2.amazonaws.com/uw-s3-cdn/wp-content/uploads/sites/6/2017/11/04133712/waterfall.jpg",
-            width=400, # Manually Adjust the width of the image as per requirement)
+            width=400, # Manually Adjust the width of the image as per requirement
+        )
 #st.image(product_img[0]), width = 400, caption='Sunrise by the mountains')
 #st.image(image, caption='Sunrise by the mountains')
 
