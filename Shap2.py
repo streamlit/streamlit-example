e310d794cb142a2a6c161057194caa8212949b5e
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-06-01 22:23:49 +0200

    Update Shap2.py

--- Shap2.py
+++ Shap2.py
@@ -9,19 +9,10 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
 
-@st.cache
-def load_data():
-    return shap.df()
-
-
-def st_shap(plot, height=None):
-    shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
-    components.html(shap_html, height=height)
-
-st.title("SHAP in Streamlit")
+st.title("SHAP Interpretation")
 
 # train XGBoost model
-X,y = load_data()
+X,y = df
 model = xgboost.train({"learning_rate": 0.01}, xgboost.DMatrix(X, label=y), 100)
 
 # explain the model's predictions using SHAP
