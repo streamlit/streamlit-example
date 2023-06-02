fffba535e471a039e6be95e7030a69b2ac388607
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-06-02 23:22:27 +0200

    Update Multipage.py

--- Multipage.py
+++ Multipage.py
@@ -37,10 +37,10 @@ def eda():
     from streamlit_pandas_profiling import st_profile_report
     @st.cache_data
     uploaded_file = st.file_uploader("Choose a file")
-    if uploaded_file is not None:
-      df = pd.read_csv(uploaded_file)
+        if uploaded_file is not None:
+          df = pd.read_csv(uploaded_file)
 
-    pr = df.profile_report()
+        pr = df.profile_report()
 
     st_profile_report(pr)
 
