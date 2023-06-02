917d960237be208dd090a1b92e7dbee60e9f29a1
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-06-02 20:49:04 +0200

    Update PANDASPROFILE.py

--- PANDASPROFILE.py
+++ PANDASPROFILE.py
@@ -4,7 +4,10 @@ import streamlit as st
 
 from streamlit_pandas_profiling import st_profile_report
 
-df = pd.read_csv("https://storage.googleapis.com/tf-datasets/titanic/train.csv")
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
+  
 pr = df.profile_report()
 
 st_profile_report(pr)
