77381c11dc352626ab371adfcc64d1560c3d9771
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-06-02 20:21:50 +0200

    Update and rename LIME2.py to PANDASPROFILE.py

---
+++ PANDASPROFILE.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import pandas_profiling
+import streamlit as st
+
+from streamlit_pandas_profiling import st_profile_report
+
+df = pd.read_csv("https://storage.googleapis.com/tf-datasets/titanic/train.csv")
+pr = df.profile_report()
+
+st_profile_report(pr)
