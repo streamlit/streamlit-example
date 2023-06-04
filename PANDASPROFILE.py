af1c60881cbc05aae9d2293558b867df0e0b992f
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-06-04 23:58:31 +0200

    Update PANDASPROFILE.py

--- PANDASPROFILE.py
+++ PANDASPROFILE.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 from streamlit_pandas_profiling import st_profile_report
 
-df = pd.read_csv('https://drive.google.com/file/d/1dLzhkMdx58uzJIjhqyFSQBFPKAIiZXhT/view?usp=sharing')
+df = pd.read_csv('https://drive.google.com/file/d/1TvB62joGvnJjfSfQ5GQSOcJ5vZzBxVar/view?usp=sharing')
 #uploaded_file = st.file_uploader("Choose a file")
 #if uploaded_file is not None:
 #  df = pd.read_csv(uploaded_file)
