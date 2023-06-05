cb1cab3fcfd351d9eda447d4c73c4e2b15972605
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-06-05 22:57:49 +0200

    Update PANDASPROFILE.py

--- PANDASPROFILE.py
+++ PANDASPROFILE.py
@@ -9,15 +9,15 @@ from streamlit_pandas_profiling import st_profile_report
 #    df = pd.read_csv(url)
 #    return df
 
-df = load_data('https://drive.google.com/file/d/1TvB62joGvnJjfSfQ5GQSOcJ5vZzBxVar/view?usp=sharing')
+#df = load_data('https://drive.google.com/file/d/1TvB62joGvnJjfSfQ5GQSOcJ5vZzBxVar/view?usp=sharing')
 #st.dataframe(df)
 
 #st.button("Rerun")
 
-#df = pd.read_csv('https://drive.google.com/file/d/1TvB62joGvnJjfSfQ5GQSOcJ5vZzBxVar/view?usp=sharing')
-#uploaded_file = st.file_uploader("Choose a file")
-#if uploaded_file is not None:
-#  df = pd.read_csv(uploaded_file)
+df = pd.read_csv('https://drive.google.com/file/d/1TvB62joGvnJjfSfQ5GQSOcJ5vZzBxVar/view?usp=sharing')
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
   
 pr = df.profile_report()
 
