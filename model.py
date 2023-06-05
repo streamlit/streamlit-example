1923706129d8d94e51967e95de29846efe8dca17
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-06-05 23:16:24 +0200

    Update model.py

--- model.py
+++ model.py
@@ -12,42 +12,20 @@ from xgboost import XGBClassifier
 import joblib
 import pickle
 
-df = pd.read_csv('https://drive.google.com/file/d/1dLzhkMdx58uzJIjhqyFSQBFPKAIiZXhT/view?usp=sharing')
+#df = pd.read_csv('https://drive.google.com/file/d/1dLzhkMdx58uzJIjhqyFSQBFPKAIiZXhT/view?usp=sharing')
 
-#uploaded_file = st.file_uploader("Choose a file")
-#if uploaded_file is not None:
-#  df = pd.read_csv(uploaded_file)
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
   
 #df = df.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1)
 #df = df.drop(['gravMerged'], axis=1, inplace=True)
 
-#X_cat = df[['Pclass', 'Sex',  'Embarked']]
-#X_quant = df[['Age', 'Fare', 'SibSp', 'Parch']]
-#y = df['Survived']
+
 y =df['grav']
 X = df.drop(['grav','gravMerged'], axis = 1)
 
-#for col in X_cat.columns:
-#    X_cat[col] = X_cat[col].fillna(X_cat[col].mode()[0])
-
-#for col in X_quant.columns:
-#    X_quant[col] = X_quant[col].fillna(X_quant[col].mean())
-
-
-#X_cat_scaled = pd.get_dummies(X_cat, columns=X_cat.columns)
-
-#X = pd.concat([X_cat_scaled, X_quant], axis = 1)
-
-#X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.25)
-  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
-
-#scaler = StandardScaler()
-#X_train[X_quant.columns] = scaler.fit_transform(X_train[X_quant.columns])
-#X_test[X_quant.columns] = scaler.transform(X_test[X_quant.columns])
-
-#scaler = StandardScaler()
-#X_train = scaler.fit_transform(X_train)
-#X_test= scaler.transform(X_test)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 
 loaded_model = pickle.load(open(filename, 'rb'))
 result = loaded_model.score(X_test, Y_test)
