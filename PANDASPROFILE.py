6d1f531c46c782d6644c502021806ebc5f5d59b9
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-06-05 22:56:26 +0200

    Update PANDASPROFILE.py

--- PANDASPROFILE.py
+++ PANDASPROFILE.py
@@ -4,15 +4,15 @@ import streamlit as st
 
 from streamlit_pandas_profiling import st_profile_report
 
-st.cache_data
-def load_data(url):
-    df = pd.read_csv(url)
-    return df
+#@st.cache_data
+#def load_data(url):
+#    df = pd.read_csv(url)
+#    return df
 
 df = load_data('https://drive.google.com/file/d/1TvB62joGvnJjfSfQ5GQSOcJ5vZzBxVar/view?usp=sharing')
 #st.dataframe(df)
 
-st.button("Rerun")
+#st.button("Rerun")
 
 #df = pd.read_csv('https://drive.google.com/file/d/1TvB62joGvnJjfSfQ5GQSOcJ5vZzBxVar/view?usp=sharing')
 #uploaded_file = st.file_uploader("Choose a file")
