f3cab05ae48d318c281cd23c50baef6670206f4d
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-06-06 17:09:50 +0200

    Update Dataviz_plots.py

--- Dataviz_plots.py
+++ Dataviz_plots.py
@@ -13,19 +13,18 @@ import plotly.express as px
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
    df = pd.read_csv(uploaded_file,low_memory=False).sample(n=100000)
-   df1 = pd.DataFrame(df)
-
+   
 st.markdown("""4.1 Visualisation of Accident severity trends across different years.
         - we can see that death rate in accidents is slightly getting reduced over years
         - Number of accidents also show a reducing trend over years 
 """)
 
 # Extract data for visualisation
-     
+  df1 = pd.DataFrame(df)     
           #df['AccidentId'] = df['AccidentId']
           #df['Year'] = df['Year'].astype(float)
-   df1['AccidentSeverity'] = df1['AccidentSeverity'].astype(str)
-   df1['AccidentSeverity'] = df1['AccidentSeverity']. replace(['1','2','3','4'], ['Not Injured','Died','Injured&Hospitalised','Slightly Injured'])
+  df1['AccidentSeverity'] = df1['AccidentSeverity'].astype(str)
+  df1['AccidentSeverity'] = df1['AccidentSeverity']. replace(['1','2','3','4'], ['Not Injured','Died','Injured&Hospitalised','Slightly Injured'])
 
    # Visualise extracted data
 road_accidents = pd.DataFrame({'AccidentSeverity','AccidentId','Year'})
