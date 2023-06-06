293f920863ddd64af34e2ba8ce06ad3d6d125cd4
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-06-07 00:47:52 +0200

    Update Multipage.py

--- Multipage.py
+++ Multipage.py
@@ -172,13 +172,63 @@ def modelling():
     elif display == 'Classification report':
         #st.table(classification_report(y_test, clf.predict(X_test)))
         st.text(scores(clf, display))
+def shap(): 
+    import shap
+    import streamlit as st
+    import streamlit.components.v1 as components
+    import xgboost
+    import pandas as pd
+    from sklearn.model_selection import train_test_split
+    from sklearn.preprocessing import StandardScaler
+    st.markdown(f'# {list(page_names_to_funcs.keys())[4]}')
+    
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+      df = pd.read_csv(uploaded_file)
+
+    df = df.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1)
+
+    def st_shap(plot, height=None):
+        shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
+        components.html(shap_html, height=height)
+
+        uploaded_file = st.file_uploader("Choose a file")
+        if uploaded_file is not None:
+          df = pd.read_csv(uploaded_file)
+
+        #df = df.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1)
+        #df = df.drop(['gravMerged'], axis=1, inplace=True)
+
+
+        y =df['grav']
+        X = df.drop(['grav','gravMerged'], axis = 1)
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+    st.title("SHAP Interpretation")
+
+    st.write('train XGBoost model')
+    model = xgboost.train({"learning_rate": 0.01}, xgboost.DMatrix(X, label=y), 100)
+
+    st.markdown('''explain the model's predictions using SHAP
+    same syntax works for LightGBM, CatBoost, scikit-learn and spark models)''')
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(X)
+
+    st.write('<p style="font-size:130%">visualize the first prediction explanation </p>', unsafe_allow_html=True)
+    st_shap(shap.force_plot(explainer.expected_value, shap_values[0,:], X.iloc[0,:]))
+
+    st.write('<p style="font-size:130%">visualize the training set predictions </p>', unsafe_allow_html=True)
+    st_shap(shap.force_plot(explainer.expected_value, shap_values, X), 400)
 
         
 page_names_to_funcs = {
     "—": intro,
     "Exploratory Data Analysis advanced": eda_advanced,
     "Data Visualization": data_viz,
-    "Machine Learning Models": modelling
+    "Machine Learning Models": modelling,
+    "Shapley Interpretation": shap
+    
 }
 
 page_name = st.sidebar.selectbox("Choose your page", page_names_to_funcs.keys())
