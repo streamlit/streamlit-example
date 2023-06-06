014a14a10a4397084cf87c1f3c00b3faf380fc69
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-06-06 16:12:01 +0200

    Update Dataviz_plots.py

--- Dataviz_plots.py
+++ Dataviz_plots.py
@@ -12,7 +12,7 @@ import plotly.express as px
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file,low_memory=False).sample(n=100000)
-  st.write(df)
+  st.dataframe(df)
 #df = DATA_URL
 
 st.markdown("""4.1 Visualisation of Accident severity trends across different years.
@@ -22,7 +22,6 @@ st.markdown("""4.1 Visualisation of Accident severity trends across different ye
 
     
 
-     #st.dataframe(df)
 
 # Extract data for visualisation
 df = pd.DataFrame(columns=['AccidentId', 'AccidentSeverity', 'Year'])
