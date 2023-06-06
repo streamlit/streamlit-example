117c05d6d3caef70f3a8ba59dd2bd77741544b1d
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-06-06 15:52:19 +0200

    Update Dataviz_plots.py

--- Dataviz_plots.py
+++ Dataviz_plots.py
@@ -8,15 +8,16 @@ import numpy as np
 import streamlit as st
 import plotly.express as px
 
-
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-  df = pd.read_csv(uploaded_file,low_memory=False).sample(n=200000)
-      
+     
 st.markdown("""4.1 Visualisation of Accident severity trends across different years.
         - we can see that death rate in accidents is slightly getting reduced over years
         - Number of accidents also show a reducing trend over years 
 """)
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+  DATA_URL = pd.read_csv(uploaded_file,low_memory=False).sample(n=100000)
+
+df = DATA_URL
 
 st.write(df)
 
