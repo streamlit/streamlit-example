5ebf639b851b89d1e9547cb3a9c004817a61e1f8
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-06-07 08:15:29 +0200

    Update R.py

--- R.py
+++ R.py
@@ -21,7 +21,7 @@ st.markdown("This application is a Streamlit dashboard that can be use to analyz
 
 from PIL import Image
 #st.image("https://upload.wikimedia.org/wikipedia/commons/2/2f/Multi_vehicle_accident_-_M4_Motorway%2C_Sydney%2C_NSW_%288076208846%29.jpg",
-            width=600 # Manually Adjust the width of the image as per requirement
+#            width=600 # Manually Adjust the width of the image as per requirement
 
         )
 st.image("https://www.simplilearn.com/ice9/free_resources_article_thumb/Data_Visualization_Tools.jpg", width=600)
