78914f81091cc509827393e2929fb3baae257eb2
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-06-07 08:23:20 +0200

    Update Multipage.py

--- Multipage.py
+++ Multipage.py
@@ -6,8 +6,8 @@ def intro():
     st.write("# Welcome to our first App")
     st.sidebar.success("Select pages")
     
-st.image("https://upload.wikimedia.org/wikipedia/commons/2/2f/Multi_vehicle_accident_-_M4_Motorway%2C_Sydney%2C_NSW_%288076208846%29.jpg",
-            width=600 # Manually Adjust the width of the image as per requirement
+    st.image("https://upload.wikimedia.org/wikipedia/commons/2/2f/Multi_vehicle_accident_-_M4_Motorway%2C_Sydney%2C_NSW_%288076208846%29.jpg",
+            width=700 # Manually Adjust the width of the image as per requirement
 
         )
     st.markdown(
