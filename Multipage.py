75798a49a3a6fb7d33e3e093f7ac033a3cb7ddc0
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-06-09 01:21:18 +0200

    Update Multipage.py

--- Multipage.py
+++ Multipage.py
@@ -55,70 +55,6 @@ def eda_advanced():
     st_profile_report(pr)
 
 def eda_basic():
-    import io
-    import pandas as pd
-    import streamlit as st
-
-    def df_info(df):
-        df.columns = df.columns.str.replace(' ', '_')
-        buffer = io.StringIO() 
-        df.info(buf=buffer)
-        s = buffer.getvalue() 
-
-        df_info = s.split('\n')
-
-        counts = []
-        names = []
-        nn_count = []
-        dtype = []
-        for i in range(5, len(df_info)-3):
-            line = df_info[i].split()
-            counts.append(line[0])
-            names.append(line[1])
-            nn_count.append(line[2])
-            dtype.append(line[4])
-
-        df_info_dataframe = pd.DataFrame(data = {'#':counts, 'Column':names, 'Non-Null Count':nn_count, 'Data Type':dtype})
-        return df_info_dataframe.drop('#', axis = 1)
-
-    def df_isnull(df):
-        res = pd.DataFrame(df.isnull().sum()).reset_index()
-        res['Percentage'] = round(res[0] / df.shape[0] * 100, 2)
-        res['Percentage'] = res['Percentage'].astype(str) + '%'
-        return res.rename(columns = {'index':'Column', 0:'Number of null values'})
-
-    def number_of_outliers(df):
-
-        df = df.select_dtypes(exclude = 'object')
-
-        Q1 = df.quantile(0.25)
-        Q3 = df.quantile(0.75)
-        IQR = Q3 - Q1
-
-        ans = ((df < (Q1 - 1.5 * IQR)) | (df > (Q3 + 1.5 * IQR))).sum()
-        df = pd.DataFrame(ans).reset_index().rename(columns = {'index':'column', 0:'count_of_outliers'})
-        return df
-
-    def space(num_lines=1):
-        for _ in range(num_lines):
-            st.write("")
-
-    def sidebar_space(num_lines=1):
-        for _ in range(num_lines):
-            st.sidebar.write("")
-
-
-    def sidebar_multiselect_container(massage, arr, key):
-
-        container = st.sidebar.container()
-        select_all_button = st.sidebar.checkbox("Select all for " + key + " plots")
-        if select_all_button:
-            selected_num_cols = container.multiselect(massage, arr, default = list(arr))
-        else:
-            selected_num_cols = container.multiselect(massage, arr, default = arr[0])
-
-        return selected_num_cols 
-    
     
     import streamlit as st
     import pandas as pd
@@ -128,7 +64,7 @@ def eda_basic():
     
     st.markdown(f'# {list(page_names_to_funcs.keys())[2]}')
 
-    st.set_page_config(layout = "wide", page_icon = 'logo.png', page_title='Multipage')
+    #st.set_page_config(layout = "wide", page_icon = 'logo.png', page_title='Multipage')
 
     st.header("Exploratory Data Analysis")
     st.image("https://datos.gob.es/sites/default/files/u322/grafico.jpg",width=600)
