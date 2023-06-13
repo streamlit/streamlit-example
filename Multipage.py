7f390fb0c3a00400affc3ca66f7f1f9550e5e94b
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-06-13 20:07:08 +0200

    Update Multipage.py

--- Multipage.py
+++ Multipage.py
@@ -38,14 +38,14 @@ def eda_advanced():
     from streamlit_pandas_profiling import st_profile_report
     
     st.markdown(f"# {list(page_names_to_funcs.keys())[1]}")
-    
+    st.image('https://editor.analyticsvidhya.com/uploads/74223Pandas%20Profiling.png',width=700)
     #@st.cache_data
     #def load_data(url):
     #    df = pd.read_csv(url)
     #    return df
     space()
     
-    st.image('https://editor.analyticsvidhya.com/uploads/74223Pandas%20Profiling.png',width=700)
+    
     
     #df = load_data('https://bol.mondial-assistance.gr/Files/EDA_advanced/EDA_advanced_sample_07062023.csv')
     uploaded_file = st.file_uploader("Choose a file")
