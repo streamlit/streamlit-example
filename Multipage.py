eab71861fc8fe8ca6fa44734125cd290a76a5ceb
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-06-13 21:15:43 +0200

    Update Multipage.py

--- Multipage.py
+++ Multipage.py
@@ -7,6 +7,7 @@ def intro():
     st.sidebar.success("Select pages")   
   
     st.markdown("![Alt Text](https://tenor.com/5kLp.gif)")
+    st.markdown("![Alt Text](https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif)")
     
     st.image("https://upload.wikimedia.org/wikipedia/commons/2/2f/Multi_vehicle_accident_-_M4_Motorway%2C_Sydney%2C_NSW_%288076208846%29.jpg",
             width=700 # Manually Adjust the width of the image as per requirement
