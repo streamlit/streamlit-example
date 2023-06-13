8b0e1912d916b184c86e7e4071b71bbeb301b3c9
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-06-13 21:41:56 +0200

    Update Multipage.py

--- Multipage.py
+++ Multipage.py
@@ -4,9 +4,8 @@ def intro():
     import base64
 
     st.write("# Road Accidents in France #")
-    st.sidebar.success("Select pages"st.markdown("![Alt Text](https://media.tenor.com/tuArNck3bKwAAAAC/car-crash.gif)"))   
-    
-        
+    st.sidebar.success("Select pages")   
+       
     st.image("https://upload.wikimedia.org/wikipedia/commons/2/2f/Multi_vehicle_accident_-_M4_Motorway%2C_Sydney%2C_NSW_%288076208846%29.jpg",
             width=700 # Manually Adjust the width of the image as per requirement
         )
@@ -30,7 +29,7 @@ def intro():
         
     """
     )
-
+    st.markdown("![Alt Text](https://media.tenor.com/tuArNck3bKwAAAAC/car-crash.gif)")
 def eda_advanced():
     import pandas as pd
     import pandas_profiling
