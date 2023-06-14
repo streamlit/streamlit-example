d12d2e1ef9d17e82c4176532813f8c4982732785
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-06-14 15:26:27 +0200

    Update EdaAdvanced.py

--- EdaAdvanced.py
+++ EdaAdvanced.py
@@ -4,20 +4,10 @@ import streamlit as st
 
 from streamlit_pandas_profiling import st_profile_report
 
-#@st.cache_data
-#def load_data(url):
-#    df = pd.read_csv(url)
-#    return df
 
-#df = load_data('https://drive.google.com/file/d/1TvB62joGvnJjfSfQ5GQSOcJ5vZzBxVar/view?usp=sharing')
-#st.dataframe(df)
-
-#st.button("Rerun")
-
-#df = pd.read_csv('https://drive.google.com/file/d/1TvB62joGvnJjfSfQ5GQSOcJ5vZzBxVar/view?usp=sharing')
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-  df = pd.read_csv(uploaded_file).sample(n=100)
+  df = pd.read_csv(uploaded_file).sample(n=1000)
   
 pr = df.profile_report()
 
