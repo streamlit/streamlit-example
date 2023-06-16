6a092fa2814ed465ef6e5a7bcb2f3c4b72ce9019
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-06-16 15:46:02 +0200

    Update Models.py

--- Models.py
+++ Models.py
@@ -52,45 +52,27 @@ if df is not None:
  
 
 if option=='Gradient Boosting':
-   st.write('Gradient Boosting score train 73.127 rmse train 0.518')
-   GBC=joblib.load('Modelling/GBC_model.joblib','rb')
+   st.write('Gradient Boosting score train 73.127')
+   GBC=joblib.load('Models/GBC_model.joblib')
    results(GBC)
 
 if option=='Gradient Boosting improved':
-   st.write('Gradient Boosting score train 78.535 rmse train 0.463')
-   GBCi=joblib.load('Modelling/GBC_improved_model.joblib','rb')
+   st.write('Gradient Boosting score train 78.535)
+   GBCi=joblib.load('Models/GBC_improved_model.joblib')
    results(GBCi)
 
 
 if option=='XGBOOST':
-   st.write('XGBOOST score train 78.733 rmse train 0.461')
-   xgb = joblib.load('Modelling/xgb_model.joblib','rb')
+   st.write('XGBOOST score train 78.733')
+   xgb = joblib.load('Models/xgb_model.joblib')
    results(xgb)
 
 
 if option=='XGBOOST improved':
-   st.write('XGBOOST score train 78.733 rmse train 0.461')
-   xgbi = joblib.load('Modelling/xgb_model.joblib','rb')
+   st.write('XGBOOST score train 78.733')
+   xgbi = joblib.load('Models/xgb_model.joblib')
    results(xgbi)
 
-# if option=='Random Forest':
-#    st.write('Random Forest score train 99.998 rmse train 0.004')
-#    RFC = RandomForestClassifier()
-#     #    RFC=joblib.load('C:\\python\\roadAccidentInFrance\\models\\RFC_model.joblib')
-#    results(RFC)
 
-# if option=='SVC':
-#    st.write('SVC score train 74.286 rmse train 0.507')
-#    SVC=joblib.load('C:\\python\\roadAccidentInFrance\\models\\SVC_model.joblib')
-#    results(SVC)
-
-# uploaded_file = st.file_uploader("Choose a file")
-# if uploaded_file is not None:
-#     df = pd.read_csv(uploaded_file).sample(n=10000)
-        
-#     @st.cache_data
-#     def load_data(url):
-#         df = pd.read_csv(url)
-#         return df
 
   
