3ba62cebb839409fd8a5e5cd9b0bd6b36da57dca
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-06-16 18:40:03 +0200

    Update Models.py

--- Models.py
+++ Models.py
@@ -1,6 +1,7 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
+from sklearn.externals import joblib
 import streamlit as st
 import pandas as pd
 import joblib
