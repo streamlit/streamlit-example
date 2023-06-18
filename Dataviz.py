6b0156ed0b0b730d93377c4baf0d28f1aa7c3638
IsiD11 <130159972+IsiD11@users.noreply.github.com>
2023-06-18 11:24:25 +0200

    Update Dataviz.py

--- Dataviz.py
+++ Dataviz.py
@@ -21,7 +21,7 @@ def load_data(url):
     return df
 
   
-df = load_data('Dataviz_12_06_2023.csv').sample(n=50000)
+df = load_data('Dataviz_12_06_2023.csv').sample(n=25000)
     
 df.dropna(subset=['LATITUDE', 'LONGITUDE','CRASH_DATE','CRASH_TIME'], inplace=True)
 
