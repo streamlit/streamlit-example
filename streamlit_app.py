f037a2a759d24cfd265fa8f8320f404c24882352
Osman Haji <osman6191@outlook.com>
2023-07-21 09:57:07 +1000

    test change

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ In the meantime, below is an example of what you can do with just a few lines of
 """
 
 
-with st.echo(code_location='below'):
+with st.echo(code_location='above'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
