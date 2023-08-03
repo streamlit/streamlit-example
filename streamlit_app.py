d69ed82d211841482ff5fdb2d1ea5de8c1e8430f
MoshikMash <moshikmash@gmail.com>
2023-08-03 15:31:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ file_list = os.listdir('test_files')
 file_name = st.selectbox('Select File', file_list)
 
 with open('test_files/' + file_name, 'rb') as file:
-    data = pickle.load('test_files/' + file_name)
+    data = pickle.load(file)
 text = data['text']
 html = f'<div style="direction: rtl; text-align: right;">{text}</div>'
 st.markdown(f"<div style='direction: rtl; text-align: right;'>{html}</div></br>", unsafe_allow_html=True)
