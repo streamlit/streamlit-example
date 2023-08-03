a45a8fd4e799f8113636fd051777e2acbfaef418
MoshikMash <moshikmash@gmail.com>
2023-08-03 16:16:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,9 @@ st.markdown(f"<div style='direction: rtl; text-align: right;'>{html}</div></br>"
 
     with open(file_name, 'rb') as file:
         data = pickle.load(file)
-    text = data['text']
-    html = f'<div style="direction: rtl; text-align: right;">{text}</div>'
-    st.markdown(f"<div style='direction: rtl; text-align: right;'>{html}</div></br>", unsafe_allow_html=True)
+text = data['text']
+html = f'<div style="direction: rtl; text-align: right;">{text}</div>'
+st.markdown(f"<div style='direction: rtl; text-align: right;'>{html}</div></br>", unsafe_allow_html=True)
 
 
 if __name__ == '__main__':
