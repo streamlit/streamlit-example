7b7275707a926f73ae58da86100ad72f0c37e09a
MoshikMash <moshikmash@gmail.com>
2023-08-03 15:49:56 -0400

    change file names

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,18 @@
 import streamlit as st
 import pickle
+import os
 
-file_list = os.listdir('test_files')
-file_name = st.selectbox('Select File', file_list)
 
-with open(file_name, 'rb') as file:
-    data = pickle.load(file)
-text = data['text']
-html = f'<div style="direction: rtl; text-align: right;">{text}</div>'
-st.markdown(f"<div style='direction: rtl; text-align: right;'>{html}</div></br>", unsafe_allow_html=True)
+def main():
+    file_list = os.listdir('files_from_streamlit')
+    file_name = st.selectbox('Select File', file_list)
 
+    with open(file_name, 'rb') as file:
+        data = pickle.load(file)
+    text = data['text']
+    html = f'<div style="direction: rtl; text-align: right;">{text}</div>'
+    st.markdown(f"<div style='direction: rtl; text-align: right;'>{html}</div></br>", unsafe_allow_html=True)
+
+
+if __name__ == '__main__':
+    main()
