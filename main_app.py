e188a9e93c6c4ea1710aad4028e5f6ebaf879ad9
Jordy-20035 <141092524+Jordy-20035@users.noreply.github.com>
2023-08-07 21:08:14 +0000

    modified:   main_app.py 	modified:   visualizer_app.py

--- main_app.py
+++ main_app.py
@@ -6,6 +6,11 @@ from hugchat.login import Login
 from time import sleep
 from hugchat_api import HuggingChat
 import os
+import streamlit as st
+import pandas as pd
+from langchain.chat_models import ChatOpenAI
+from langchain.agents import create_pandas_dataframe_agent
+from langchain.agents.agent_types import AgentType
 
 with st.sidebar:
     ti=st.title("Welcome to 😃AllTalK💬!")
@@ -96,14 +101,57 @@ def main():
                     st.error(f"An error occurred: {str(e)}")
                     st.session_state.messages.append({"role": "assistant", "content": f"An error occurred: {str(e)}"})
 
+
+    def visualizer():
+        # Page title
+        st.set_page_config(page_title='🦜🔗 Ask the Data App')
+        st.title('🦜🔗 Ask the Data App')
+
+        # Load CSV file
+        def load_csv(input_csv):
+            df = pd.read_csv(input_csv)
+            with st.expander('See DataFrame'):
+                st.write(df)
+            return df
+
+        # Generate LLM response
+        def generate_response(csv_file, input_query):
+            llm = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0.2, openai_api_key=openai_api_key)
+            df = load_csv(csv_file)
+            # Create Pandas DataFrame Agent
+            agent = create_pandas_dataframe_agent(llm, df, verbose=True, agent_type=AgentType.OPENAI_FUNCTIONS)
+            # Perform Query using the Agent
+            response = agent.run(input_query)
+            return st.success(response)
+
+        # Input widgets
+        uploaded_file = st.file_uploader('Upload a CSV file', type=['csv'])
+        question_list = [
+            'How many rows are there?',
+            'What is the range of values for MolWt with logS greater than 0?',
+            'How many rows have MolLogP value greater than 0.',
+            'Other']
+        query_text = st.selectbox('Select an example query:', question_list, disabled=not uploaded_file)
+        openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
+
+        # App logic
+        if query_text is 'Other':
+            query_text = st.text_input('Enter your query:', placeholder = 'Enter query here ...', disabled=not uploaded_file)
+        if not openai_api_key.startswith('sk-'):
+            st.warning('Please enter your OpenAI API key!', icon='⚠')
+        if openai_api_key.startswith('sk-') and (uploaded_file is not None):
+            st.header('Output')
+            generate_response(uploaded_file, query_text) 
+
+
     if page == "Select":
         st.write("Please select the services")
     elif page == "AI ChatBot":
         chatbot()
     #else:
     #    summarizer()
-    #else:
-    #    chatbot()  
+    else:
+        visualizer()  
   
 if __name__=='__main__':
     main()
---
+++ visualizer_app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import pandas as pd
+from langchain.chat_models import ChatOpenAI
+from langchain.agents import create_pandas_dataframe_agent
+from langchain.agents.agent_types import AgentType
+
+# Page title
+st.set_page_config(page_title='🦜🔗 Ask the Data App')
+st.title('🦜🔗 Ask the Data App')
+
+# Load CSV file
+def load_csv(input_csv):
+  df = pd.read_csv(input_csv)
+  with st.expander('See DataFrame'):
+    st.write(df)
+  return df
+
+# Generate LLM response
+def generate_response(csv_file, input_query):
+  llm = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0.2, openai_api_key=openai_api_key)
+  df = load_csv(csv_file)
+  # Create Pandas DataFrame Agent
+  agent = create_pandas_dataframe_agent(llm, df, verbose=True, agent_type=AgentType.OPENAI_FUNCTIONS)
+  # Perform Query using the Agent
+  response = agent.run(input_query)
+  return st.success(response)
+
+# Input widgets
+uploaded_file = st.file_uploader('Upload a CSV file', type=['csv'])
+question_list = [
+  'How many rows are there?',
+  'What is the range of values for MolWt with logS greater than 0?',
+  'How many rows have MolLogP value greater than 0.',
+  'Other']
+query_text = st.selectbox('Select an example query:', question_list, disabled=not uploaded_file)
+openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
+
+# App logic
+if query_text is 'Other':
+  query_text = st.text_input('Enter your query:', placeholder = 'Enter query here ...', disabled=not uploaded_file)
+if not openai_api_key.startswith('sk-'):
+  st.warning('Please enter your OpenAI API key!', icon='⚠')
+if openai_api_key.startswith('sk-') and (uploaded_file is not None):
+  st.header('Output')
+  generate_response(uploaded_file, query_text)
