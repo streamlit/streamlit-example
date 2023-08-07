a84a20c9fbe3788472ddffd28d04db8d9b55d75d
Jordy-20035 <141092524+Jordy-20035@users.noreply.github.com>
2023-08-07 04:13:51 +0000

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,172 +1,123 @@
+#pip install hugchat
+
 import streamlit as st
-import streamlit_chat
-from streamlit_chat import message
-from streamlit_extras.colored_header import colored_header
-from streamlit_extras.add_vertical_space import add_vertical_space
 from hugchat import hugchat
 from hugchat.login import Login
+from time import sleep
+from hugchat_api import HuggingChat
 
-st.set_page_config(page_title="HugChat - An LLM-powered Streamlit app")
+# App title
+st.set_page_config(page_title="😊 UsCHAT 💬")
 
-# Sidebar contents
+EMAIL = st.secrets["DB_EMAIL"]
+PASSWD = st.secrets["DB_PASS"]
+COOKIE_STORE_PATH = "./usercookies"
+
+#HUG= HuggingChat(max_thread=1)
+
+# Hugging Face Credentials
 with st.sidebar:
-    st.title('🤗💬 HugChat App')
-    st.markdown('''
-    ## About
-    This app is an LLM-powered chatbot built using:
-    - [Streamlit](https://streamlit.io/)
-    - [HugChat](https://github.com/Soulter/hugging-chat-api)
-    - [OpenAssistant/oasst-sft-6-llama-30b-xor](https://huggingface.co/OpenAssistant/oasst-sft-6-llama-30b-xor) LLM model
+    st.title('😊 UsCHAT 💬')
+    #st.header('UsCHAT Login')
+    #sign = Login(EMAIL, PASSWD)
+    #cookies = sign.login()
+    #cookies = sign.loadCookiesFromDir(cookie_dir_path=COOKIE_STORE_PATH)
     
-    💡 Note: No API key required!
-    ''')
-    add_vertical_space(5)
-    st.write('Made with ❤️ by [Jagadeesha](https://www.linkedin.com/in/jagadeesha-gowda-6a30382b)')
+    #if ('EMAIL' in st.secrets) and ('PASS' in st.secrets):
+    #    st.success('HuggingFace Login credentials already provided !', icon='✅')
+    #    hf_email = st.secrets['EMAIL']
+    #    hf_pass = st.secrets['PASS']
+    #else:
+    #    hf_email = st.text_input('Enter E-mail:', type='default')
+    #    hf_pass = st.text_input('Enter password:', type='password')
+    #    if not (hf_email and hf_pass):
+    #        st.warning('Please enter your credentials!', icon='⚠')
+    #    else:
+    #        st.success('Proceed to your chat!', icon='👉')
+    st.markdown('Contact me for enquiries [here](https://myportfolio.com)!')
 
-# Generate empty lists for generated and past.
-## generated stores AI generated responses
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = ["I'm HugChat, How may I help you?"]
-## past stores User's questions
-if 'past' not in st.session_state:
-    st.session_state['past'] = ['Hi!']
 
-# Layout of input/response containers
-input_container = st.container()
-colored_header(label='', description='', color_name='blue-30')
-response_container = st.container()
+# Store LLM Generated responses
+if "messages" not in st.session_state:
+    #st.session_state.messages = []
+    st.session_state.messages = [{"role": "assistant", "content": "Hey there, how can I help you?"}]
 
-# User input
-## Function for taking user provided prompt as input
-def get_text():
-    input_text = st.text_input("You: ", "", key="input")
-    return input_text
-## Applying the user input box
-with input_container:
-    user_input = get_text()
-# Log in to huggingface and grant authorization to huggingchat
-email="manyejordana@gmail.com"
-passwd="Analeticia21@"
-sign = Login(email, passwd)
-cookies = sign.login()
 
-# Save cookies to usercookies/<email>.json
-sign.saveCookies()
-# Response output
-## Function for taking user prompt as input followed by producing AI generated responses
-def generate_response(prompt):
+# Display  chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+
+# Funtion genrating LLM response
+def generate_response(dialogue_history):
+    #Hugging face login
+    sign = Login(EMAIL, PASSWD)
+    cookies = sign.login()
+    # Create ChatBot
     chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
-    response = chatbot.chat(prompt)
+    response = chatbot.chat(dialogue_history, stream=True)
     return response
+    #if isinstance(response, str):
+        #return response
+    #else:
+        #return response.delta.get("content", "")
+    #return chatbot.chat(prompt_input)
 
-## Conditional display of AI generated responses as a function of user provided prompts
-with response_container:
-    if user_input:
-        response = generate_response(user_input)
-        st.session_state.past.append(user_input)
-        st.session_state.generated.append(response)
-        
-    if st.session_state['generated']:
-        for i in range(len(st.session_state['generated'])):
-            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-            message(st.session_state["generated"][i], key=str(i))
+
+# User-provided prompt
+if prompt := st.chat_input("How may I help you ?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Append the dialogue history to the user's prompt
+    dialogue_history = "\n".join([message["content"] for message in st.session_state.messages])
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+
+# Genrate a new response if last message not from the assistant(chatbot)
+#if st.session_state.messages[-1]["role"] != "assistant":
+with st.chat_message("assistant"):
+    with st.spinner("Thinking..."):
+        message_placeholder = st.empty()
+        full_response = ""
+
+        try:
+            for response in generate_response(dialogue_history):
+                full_response += response
+                message_placeholder.markdown(full_response + " ")
+                sleep(0.01)
+            message_placeholder.markdown(full_response)
+
+            #checking if there are follow-up questions
+            if "?" in prompt:
+                #Update the chat history with the bot's response
+                st.session_state.messages.append({"role": "assistant", "content": full_response})
+                #Clear the chat input box
+                st.session_state.prompt = ""
+                #set the chat input box value to the assistant's response
+                st.chat_input("Follow-up question", value=full_response)
+            st.session_state.messages.append({"role": "assistant", "content": full_response})
+        except Exception as e:
+            st.error(f"An error occurred: {str(e)}")
+            st.session_state.messages.append({"role": "assistant", "content": f"An error occurred: {str(e)}"})
             
-            
-  #Response--------
-import streamlit as st
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
-
-# Load the pre-trained model and tokenizer
-model = GPT2LMHeadModel.from_pretrained("gpt2")
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-
-# Function to generate a response using the model
-def generate_response(prompt, conversation_history):
-    input_ids = tokenizer.encode(conversation_history + prompt, return_tensors="pt")
-    output = model.generate(input_ids, max_length=100, num_return_sequences=1)
-    response = tokenizer.decode(output[0], skip_special_tokens=True)
-    return response
-
-# Streamlit App
-st.title("Chatbot")
-
-# Conversation history
-conversation_history = ""
-
-# User input
-user_input = st.text_input("User Input")
-
-# Check if user input is provided
-if user_input:
-    # Generate response using the model
-    response = generate_response(user_input, conversation_history)
-
-    # Update conversation history
-    conversation_history += user_input + "\n"
-    conversation_history += response + "\n"
-
-    # Display conversation history
-    st.text_area("Conversation History", value=conversation_history, height=200)
+        #response = generate_response(prompt, hf_email, hf_pass)
+        #st.write(response)
+    #message = {"role": "assistant", "content": response}
+    #st.session_state.messages.append(message)
 
 
-#https://chat.openai.com/share/94495184-6ce3-443d-b397-d94e74fabeb3#
-    
-import streamlit as st
-import hug
 
-# Create a Hug chat instance
-chat = hug.Chat()
 
-# Define a function to generate the result
-def generate_result(text):
-  # Get the response from the chatbot
-  response = chat._get_response(text)
 
-  # Return the response
-  return response
 
-# Set the layout of the app
-st.layout = st.container()
 
-# Add a text input field
-text_input = st.text_input("Enter your message")
 
-# If the user enters a message, generate the result and display it
-if text_input:
-  # Get the user's query
-  query = text_input
 
-  # Generate the result
-  result = generate_result(query)
 
-  # Display the user's query, the result, and the conversation history
-  st.write("User query:", query)
-  st.write("Result:", result)
-  st.write("Conversation history:")
-  for message in chat._conversation_history:
-    st.write(message)
 
-  # Once the user clicks on enter, remove the query from the prompt box
-  st.text_input("Enter your message", key=None)
 
-  # Display the generated response in typewriter effect
-  st.write(result, unsafe_allow_html=True, key="generated_response")
-  st.markdown("""
-  <style>
-  #generated_response {
-  animation: typewriting 2s infinite;
-  }
 
-  @keyframes typewriting {
-    0% {
-      opacity: 0;
-    }
-    25% {
-      opacity: 1;
-    }
-    100% {
-      opacity: 0;
-    }
-  }
-  </style>
-  """, unsafe_allow_html=True)
+
