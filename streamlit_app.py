4ce76afe79f94d890999c4b71ea06a93793219a5
Jordy-20035 <141092524+Jordy-20035@users.noreply.github.com>
2023-08-07 03:50:12 +0000

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit_chat
 from streamlit_chat import message
 from streamlit_extras.colored_header import colored_header
 from streamlit_extras.add_vertical_space import add_vertical_space
