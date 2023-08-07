5dcff821ed8c111306066fafc3f71ae876f3b3d7
Jordy-20035 <141092524+Jordy-20035@users.noreply.github.com>
2023-08-07 00:38:37 +0000

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ with st.sidebar:
     st.title('😊 UsCHAT 💬')
     st.header('UsCHAT Login')
     sign = HUG.getSign(EMAIL, PASSWD)
-    cookies = sign.login(save=True, cookie_dir_path=COOKIE_STORE_PATH)
+    cookies = sign.login()
     #cookies = sign.loadCookiesFromDir(cookie_dir_path=COOKIE_STORE_PATH)
     
     #if ('EMAIL' in st.secrets) and ('PASS' in st.secrets):
