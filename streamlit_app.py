d96c11a9affc0b4cef51d3232944a57f83f8aa64
Jordy-20035 <141092524+Jordy-20035@users.noreply.github.com>
2023-08-07 02:21:28 +0000

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="🤗 UsCHAT 💬")
 # Hugging Face Credentials
 with st.sidebar:
     st.title('🤗 UsCHAT 💬')
-    if ('DB_EMAIL' in st.secrets) and ('DB_PASS' in st.secrets):
+    if ("DB_EMAIL" in st.secrets) and ("DB_PASS" in st.secrets):
         st.success('UsCHAT Login credentials already provided!', icon='✅')
         hf_email = st.secrets["DB_EMAIL"]
         hf_pass = st.secrets["DB_PASS"]
