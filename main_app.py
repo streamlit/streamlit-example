e7a668276831e906608eaee08135a9bebf6d672b
Jordy-20035 <141092524+Jordy-20035@users.noreply.github.com>
2023-08-07 19:32:16 +0100

    main_app

--- main_app.py
+++ main_app.py
@@ -95,7 +95,7 @@ def main():
                 except Exception as e:
                     st.error(f"An error occurred: {str(e)}")
                     st.session_state.messages.append({"role": "assistant", "content": f"An error occurred: {str(e)}"})
-                
+                    
     if page == "Select":
         st.write("Please select the services")
     elif page == "AI ChatBot":
