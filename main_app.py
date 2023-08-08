9bc979c3f85df960afef0c00db3b2da7fa7eb5cf
Jordy-20035 <141092524+Jordy-20035@users.noreply.github.com>
2023-08-08 14:16:46 +0100

    main_app

--- main_app.py
+++ main_app.py
@@ -14,6 +14,10 @@ from langchain.agents.agent_types import AgentType
 import requests
 import tabulate
 import classes
+from langchain import OpenAI
+from langchain.docstore.document import Document
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains.summarize import load_summarize_chain
 
  
 
@@ -239,6 +243,35 @@ def main():
         - I'm trained to help you the best i can and soon with more training i'll be a knowItAll !
         - Stick with me until then to have everything before everyone 💡 ! 
          ''')
+
+        def generate_response(txt):
+            # Instantiate the LLM model
+            llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
+            # Split text
+            text_splitter = CharacterTextSplitter()
+            texts = text_splitter.split_text(txt)
+            # Create multiple documents
+            docs = [Document(page_content=t) for t in texts]
+            # Text summarization
+            chain = load_summarize_chain(llm, chain_type='map_reduce')
+            return chain.run(docs)
+
+        # Text input
+        txt_input = st.text_area('Enter your text', '', height=200)
+
+        # Form to accept user's text input for summarization
+        result = []
+        with st.form('summarize_form', clear_on_submit=True):
+            openai_api_key = st.text_input('OpenAI API Key', type = 'password', disabled=not txt_input)
+            submitted = st.form_submit_button('Submit')
+            if submitted and openai_api_key.startswith('sk-'):
+                with st.spinner('Calculating...'):
+                    response = generate_response(txt_input)
+                    result.append(response)
+                    del openai_api_key
+
+        if len(result):
+            st.info(response)
 
 
 
