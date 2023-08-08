4f0d53ea18fa53b249485b79590d0f3d1f1785ec
Jordy-20035 <141092524+Jordy-20035@users.noreply.github.com>
2023-08-08 01:36:03 +0000

    modified:   main_app.py

--- main_app.py
+++ main_app.py
@@ -11,6 +11,7 @@ import pandas as pd
 from langchain.chat_models import ChatOpenAI
 from langchain.agents import create_pandas_dataframe_agent
 from langchain.agents.agent_types import AgentType
+import requests
 import tabulate
 
  
