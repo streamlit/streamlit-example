d44562237befd99d0cf65c4f6ec1bcd10c739116
Jordy-20035 <141092524+Jordy-20035@users.noreply.github.com>
2023-08-08 02:44:20 +0000

    modified:   main_app.py

--- main_app.py
+++ main_app.py
@@ -156,6 +156,8 @@ def main():
         #llm = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0.2, openai_api_key=openai_api_key)
         available_models = {"ChatGPT-3.5": "gpt-3.5-turbo",}
 
+        my_key = st.text_input(label = ":key: OpenAI Key:", help="Please ensure you have an OpenAI API account with credit. ChatGPT Plus subscription does not include API access.",type="password")
+
         # Radio buttons for dataset choice
         chosen_dataset = uploaded_file
 
