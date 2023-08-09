418b221d21133d82a46aa567103e50e1c6819371
hectorsestanley <62483627+hectorsestanley@users.noreply.github.com>
2023-08-09 15:33:31 +0100

    Update Streamlit_app1.py

--- Streamlit_app1.py
+++ Streamlit_app1.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Define a dictionary of valid usernames and passwords
 VALID_USERS = {
-    'lukeisgreat': 'password1',
+    'user1': 'password1',
     'user2': 'password2',
     'user3': 'password3'
 }
