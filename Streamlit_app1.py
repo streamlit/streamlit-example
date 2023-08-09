ee64fd2b92032c5904e5c0e54566c06fda5b1265
hectorsestanley <62483627+hectorsestanley@users.noreply.github.com>
2023-08-09 15:29:40 +0100

    Update Streamlit_app1.py

--- Streamlit_app1.py
+++ Streamlit_app1.py
@@ -7,14 +7,30 @@ VALID_USERS = {
     'user3': 'password3'
 }
 
-# Fake data for the dashboard
-fake_data = {
-    'Total Sales': 123456,
-    'Profit': 54321,
-    'Expenses': 98765,
-    'Customers': 789,
-    'Products': 456
-}
+# Fake data for candidate profiles
+candidate_profiles = [
+    {
+        'Name': 'John Doe',
+        'Age': 30,
+        'Experience': '5 years',
+        'Skills': 'Python, SQL, Machine Learning',
+        'Education': 'MSc in Computer Science'
+    },
+    {
+        'Name': 'Jane Smith',
+        'Age': 28,
+        'Experience': '3 years',
+        'Skills': 'JavaScript, React, UI/UX Design',
+        'Education': 'BSc in Web Development'
+    },
+    {
+        'Name': 'Alex Johnson',
+        'Age': 25,
+        'Experience': '2 years',
+        'Skills': 'Java, Spring Boot, Microservices',
+        'Education': 'BEng in Software Engineering'
+    }
+]
 
 def login():
     st.title("Login Page")
@@ -33,11 +49,34 @@ def login():
             st.error("Invalid username or password")
 
 def dashboard():
-    st.title("Dashboard")
-    st.write("Welcome to the Dashboard! Here are some fake numbers:")
+    st.title("Candidate Profiles")
+    st.write("Welcome to the Candidate Profiles Dashboard! Here are some fake profiles:")
 
-    for metric, value in fake_data.items():
-        st.metric(label=metric, value=value)
+    for profile in candidate_profiles:
+        st.subheader(profile['Name'])
+        st.write(f"Age: {profile['Age']}")
+        st.write(f"Experience: {profile['Experience']}")
+        st.write(f"Skills: {profile['Skills']}")
+        st.write(f"Education: {profile['Education']}")
+        st.write("-" * 50)
+
+    st.title("Upload Your Profile")
+    name = st.text_input("Name")
+    age = st.number_input("Age", min_value=0, max_value=150, value=25)
+    experience = st.text_input("Experience")
+    skills = st.text_input("Skills")
+    education = st.text_input("Education")
+
+    if st.button("Upload"):
+        new_profile = {
+            'Name': name,
+            'Age': age,
+            'Experience': experience,
+            'Skills': skills,
+            'Education': education
+        }
+        candidate_profiles.append(new_profile)
+        st.success("Profile uploaded successfully!")
 
 def main():
     login()
