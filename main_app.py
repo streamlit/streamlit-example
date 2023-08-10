783e052abf40bfefae7f9249e9f74fe0ccc6bccd
Jordy-20035 <manyejordana@gmail.com>
2023-08-10 15:24:45 +0100

    (use "git push" to publish your local commits) 	modified:   main_app.py

--- main_app.py
+++ main_app.py
@@ -164,7 +164,7 @@ def main():
         - I'm trained to help you the best i can and soon with more training i'll be a knowItAll !
         - Stick with me until then to have everything before everyone 💡 ! 
          ''')
-        @st.cache_data()
+        #@st.cache_data()
         def load_summarizer():
             model = pipeline("summarization", device=0)
             return model
@@ -196,7 +196,7 @@ def main():
 
         summarizer = load_summarizer()
         #st.title("Summarize Text")
-        sentence = st.text_area('Please paste your article :', height=30)
+        sentence = st.text_area('Please paste your article :', height=200)
         button = st.button("Summarize")
 
         max = st.sidebar.slider('Select max', 50, 500, step=10, value=150)
