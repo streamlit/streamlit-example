6bbd1d09d0c98c5f5bc38e9242b9f2469b5e3f53
joker45op <85115298+joker45op@users.noreply.github.com>
2023-08-10 07:25:13 +0000

    w

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import os
 
-#os.system("pip3 install -r ./alpaca-lora/requirements.txt")
-st.write("fds")
+os.system("pip3 install -r ./alpaca-lora/requirements.txt")
+st.write("fdssss")
