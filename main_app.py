90100b67ebe710184892f795d585f51ff76803f6
Jordy-20035 <141092524+Jordy-20035@users.noreply.github.com>
2023-08-10 02:51:48 +0000

    modified:   main_app.py

--- main_app.py
+++ main_app.py
@@ -28,6 +28,7 @@ from langchain.chains.summarize import load_summarize_chain
 from langchain.document_loaders import PyPDFLoader
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
+from transformers import pipeline
 
  
 
@@ -163,6 +164,54 @@ def main():
         - I'm trained to help you the best i can and soon with more training i'll be a knowItAll !
         - Stick with me until then to have everything before everyone 💡 ! 
          ''')
+        @st.cache(allow_output_mutation=True)
+        def load_summarizer():
+            model = pipeline("summarization", device=0)
+            return model
+
+
+        def generate_chunks(inp_str):
+            max_chunk = 500
+            inp_str = inp_str.replace('.', '.<eos>')
+            inp_str = inp_str.replace('?', '?<eos>')
+            inp_str = inp_str.replace('!', '!<eos>')
+            
+            sentences = inp_str.split('<eos>')
+            current_chunk = 0 
+            chunks = []
+            for sentence in sentences:
+                if len(chunks) == current_chunk + 1: 
+                    if len(chunks[current_chunk]) + len(sentence.split(' ')) <= max_chunk:
+                        chunks[current_chunk].extend(sentence.split(' '))
+                    else:
+                        current_chunk += 1
+                        chunks.append(sentence.split(' '))
+                else:
+                    chunks.append(sentence.split(' '))
+
+            for chunk_id in range(len(chunks)):
+                chunks[chunk_id] = ' '.join(chunks[chunk_id])
+            return chunks
+
+
+        summarizer = load_summarizer()
+        st.title("Summarize Text")
+        sentence = st.text_area('Please paste your article :', height=30)
+        button = st.button("Summarize")
+
+        max = st.sidebar.slider('Select max', 50, 500, step=10, value=150)
+        min = st.sidebar.slider('Select min', 10, 450, step=10, value=50)
+        do_sample = st.sidebar.checkbox("Do sample", value=False)
+        with st.spinner("Generating Summary.."):
+            if button and sentence:
+                chunks = generate_chunks(sentence)
+                res = summarizer(chunks,
+                                max_length=max, 
+                                min_length=min, 
+                                do_sample=do_sample)
+                text = ' '.join([summ['summary_text'] for summ in res])
+                # st.write(result[0]['summary_text'])
+                st.write(text)
 
         
         #Split text using character text split so it should increase token size
