d538692735b701640852abe19a5d72f2ca060f16
Jordy-20035 <141092524+Jordy-20035@users.noreply.github.com>
2023-08-10 02:00:13 +0000

    modified:   main_app.py

--- main_app.py
+++ main_app.py
@@ -230,10 +230,10 @@ def main():
 
         #Question andd answering criterion
         elif page_selection =="Question":
-            question = st.text_input("Enter your question", value="Enter your question here...")
+            question = st.text_input("Enter your question")
             combined_content = ''.join([p.page_content for p in pages])
             texts = text_splitter.split_text(combined_content)
-            embedding = OpenAIEmbeddings(openai_api_key = st.text_input('OpenAI API Key'))
+            embedding = OpenAIEmbeddings(openai_api_key)
             document_search = FAISS.from_texts(texts, embedding) #FAISS for efficient search of simlarity and clustering
             chain = load_qa_chain(llm, chain_type="stuff")
             docs = document_search.similarity_search(question)
