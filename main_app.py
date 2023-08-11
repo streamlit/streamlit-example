3c35468fd031eeb661525a950bb1470d24388367
Jordy-20035 <manyejordana@gmail.com>
2023-08-11 18:43:17 +0100

    modified:   main_app.py

--- main_app.py
+++ main_app.py
@@ -44,7 +44,6 @@ page=st.selectbox("WHAT I OFFER !",("Select","AI ChatBot","AI Summarizer","AI Vi
 #Hide main menu and footer
 hide_default_format = """
     <style>
-    #MainMenu {visibility: hidden; }
     footer {visibility: hidden;}
     </style>
 """
