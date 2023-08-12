de600b7d278ff0e0a3bb6360c90645b32f055d97
Jordy-20035 <141092524+Jordy-20035@users.noreply.github.com>
2023-08-12 01:38:31 +0100

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -61,7 +61,7 @@ page=st.selectbox("WHAT I OFFER !",("Select","AI ChatBot","AI Summarizer","AI Vi
 #Hide main menu and footer
 hide_default_format = """
     <style>
-    
+    #MainMenu {visibility: hidden;}
     footer {visibility: hidden;}
     </style>
 """
