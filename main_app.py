4c96481d8fcf99467fab0af0b33d731618f664ba
Jordy-20035 <manyejordana@gmail.com>
2023-09-03 02:48:59 +0200

    modified:   main_app.py

--- main_app.py
+++ main_app.py
@@ -274,7 +274,7 @@ def main():
         if page_selection == "Single page":
             if pdf_file is not None:
                 with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-                tmp_file.write(pdf_file.read())
+                    tmp_file.write(pdf_file.read())
                     llm = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0.2, openai_api_key=openai_api_key)
                     page_number = st.number_input("Enter page number", min_value=1, max_value=len(pages), value=1, step=1)
                     view = pages[page_number - 1]
