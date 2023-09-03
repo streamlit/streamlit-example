fbaeb7a26dea4f236041ec66764f5e36231d5885
Jordy-20035 <manyejordana@gmail.com>
2023-09-04 01:48:13 +0200

    modified:   main_app.py

--- main_app.py
+++ main_app.py
@@ -347,7 +347,7 @@ def main():
                     texts = text_splitter.split_text(combined_content)
                     #embedding = OpenAIEmbeddings(llm)
                     embedding = HuggingFaceEmbeddings(
-                        model_name="Sentence-tranformers/all-MiniLM-L6-v2"
+                        model_name="sentence-tranformers/all-MiniLM-L6-v2"
                         )
                     db = Chroma.from_documents(texts, embedding, persist_directory="db")
                     llm = GPT4ALL(
