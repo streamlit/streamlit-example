a8d0e7ffedb6bcfd9dfbbc8352a4d3d6267fb854
Jordy-20035 <manyejordana@gmail.com>
2023-09-03 18:19:36 +0200

    modified:   main_app.py

--- main_app.py
+++ main_app.py
@@ -254,7 +254,7 @@ def main():
     #PDF Uploader function    
         # Split text using character text split so it should increase token size
         text_splitter = RecursiveCharacterTextSplitter(
-            separator= "\n",
+            #separator= "\n",
             chunk_size = 800,
             chunk_overlap = 200,
             length_function = len,
