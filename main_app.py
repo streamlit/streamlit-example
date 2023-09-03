12860dded28391d1844a43150e56b502b7b4726f
Jordy-20035 <manyejordana@gmail.com>
2023-09-03 16:03:13 +0200

    modified:   main_app.py

--- main_app.py
+++ main_app.py
@@ -186,7 +186,7 @@ def main():
             st.header('Output')
             generate_response(uploaded_file, query_text)
 
-    @st.cache
+    @st.cache_data
     def summarizer():
         st.title("😊 CheatSheet 🧾")
         st.markdown('''
