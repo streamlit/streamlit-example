1efd90c43bb76cd668c39bb20eb4a28b643d3337
Jordy-20035 <manyejordana@gmail.com>
2023-09-05 12:39:21 +0200

    modified:   main_app.py

--- main_app.py
+++ main_app.py
@@ -171,7 +171,7 @@ def main():
 
         # Generate LLM response
         def generate_response(csv_file, input_query):
-            llm = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0.2, openai_api_key=openai_api_key)
+            llm = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0.2, openai_api_key=openai.api_key)
             df = load_csv(csv_file)
             # Create Pandas DataFrame Agent
             agent = create_pandas_dataframe_agent(llm, df, verbose=True, agent_type=AgentType.OPENAI_FUNCTIONS)
@@ -187,14 +187,15 @@ def main():
             'Are they missing values ?',
             'Other']
         query_text = st.selectbox('Select an example query:', question_list, disabled=not uploaded_file)
-        openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
+        #openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
+        openai.api_key = st.secrets['OPENAI_API_KEY']
 
         # App logic
         if query_text == 'Other':
             query_text = st.text_input('Enter your query:', placeholder = 'Enter query here ...', disabled=not uploaded_file)
-        if not openai_api_key.startswith('sk-'):
+        if not openai.api_key.startswith('sk-'):
             st.warning('Please enter your OpenAI API key!', icon='⚠')
-        if openai_api_key.startswith('sk-') and (uploaded_file is not None):
+        if openai.api_key.startswith('sk-') and (uploaded_file is not None):
             st.header('Output')
             generate_response(uploaded_file, query_text)
 
