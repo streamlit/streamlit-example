c3e27d76ef9b665e54b4b837baa38ac94f5f83e9
danlrich5230 <123052883+danlrich5230@users.noreply.github.com>
2023-09-07 18:46:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,30 +11,51 @@ from typing import List, Union
 from langchain.schema import AgentAction, AgentFinish
 import re
 import langchain
+import random
+import time
 
-
-
-st.title('🦜🔗 Quickstart App')
-
-openai_api_key = st.sidebar.text_input('OpenAI API Key')
+# .streamlit/secrets.toml
+OPENAI_API_KEY = "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"
 
 import streamlit as st
+import openai
 
-with st.chat_message("user"):
-    st.write("Hello 👋")
-  
-def generate_response(input_text):
-  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-  st.info(llm(input_text))
+st.title("ChatGPT-like clone")
 
-  
+# Set OpenAI API key from Streamlit secrets
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-'''
-with st.form('my_form'):
-  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
-  submitted = st.form_submit_button('Submit')
-  if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠')
-  if submitted and openai_api_key.startswith('sk-'):
-    generate_response(text)
-'''
+# Set a default model
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+
+for response in openai.ChatCompletion.create(
+        model=st.session_state["openai_model"],
+        messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
+        stream=True,
+    ):
+        full_response += response.choices[0].delta.get("content", "")
+        message_placeholder.markdown(full_response + "▌")
+    message_placeholder.markdown(full_response)
+st.session_state.messages.append({"role": "assistant", "content": full_response})
