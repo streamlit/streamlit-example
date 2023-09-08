32c494d9cb8e2b63366759cc0fb8ed0f9f82977b
zhiming-dai <108203956+zhiming-dev@users.noreply.github.com>
2023-09-07 22:56:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,8 @@ import pandas as pd
 import streamlit as st
 
 """
-# Stress Intensity Factor Calculator
+# Stress Intensity Factor Calculator (Proof of Concept)
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
 
