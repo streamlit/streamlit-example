45adb20d1ecbd85e7b648369002abefce4da1299
Cheng Wan <97571309+JornyWan@users.noreply.github.com>
2023-09-08 10:18:57 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# Stress Intensity Factor Calculator
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
