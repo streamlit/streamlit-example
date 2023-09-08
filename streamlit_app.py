38d00cd5729b17e35d68e027ee9b9cacffe4fa09
zhiming-dai <108203956+zhiming-dev@users.noreply.github.com>
2023-09-07 22:58:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,24 +16,24 @@ image_html_block = "<div class=\"col-lg-6 card my-2 px-3\" style=\"width: max-co
 
 test_iframe = st.components.v1.iframe(iframe_src_3d_url, height = 600, scrolling=False)
 
-test_image = st.components.v1.html(image_html_block, width=800, height=600, scrolling=False)
+# test_image = st.components.v1.html(image_html_block, width=800, height=600, scrolling=False)
 
-total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+# num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-Point = namedtuple('Point', 'x y')
-data = []
+# Point = namedtuple('Point', 'x y')
+# data = []
 
-points_per_turn = total_points / num_turns
+# points_per_turn = total_points / num_turns
 
-for curr_point_num in range(total_points):
-    curr_turn, i = divmod(curr_point_num, points_per_turn)
-    angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-    radius = curr_point_num / total_points
-    x = radius * math.cos(angle)
-    y = radius * math.sin(angle)
-    data.append(Point(x, y))
+# for curr_point_num in range(total_points):
+#     curr_turn, i = divmod(curr_point_num, points_per_turn)
+#     angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+#     radius = curr_point_num / total_points
+#     x = radius * math.cos(angle)
+#     y = radius * math.sin(angle)
+#     data.append(Point(x, y))
 
-st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-    .mark_circle(color='#0068c9', opacity=0.5)
-    .encode(x='x:Q', y='y:Q'))
+# st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+#     .mark_circle(color='#0068c9', opacity=0.5)
+#     .encode(x='x:Q', y='y:Q'))
