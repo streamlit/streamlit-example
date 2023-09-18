40e61b0f9f81b62dcd65dbc01865878e13d1c201
zhiming-dai <108203956+zhiming-dev@users.noreply.github.com>
2023-09-18 16:23:45 -0400

    Added sidebar and data input fields

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,19 +4,27 @@ import math
 import pandas as pd
 import streamlit as st
 
-"""
-# Stress Intensity Factor Calculator (Proof of Concept)
+# """
+# # Stress Intensity Factor Calculator (Proof of Concept)
 
-"""
+# """
 
+# add sideBar
+with st.sidebar:
+    st.write("**Stress Intensity Factor Calculator (PoC)**")
 
-# with st.echo(code_location='below'):
+    a = st.number_input("a; unit: μm", help="a; unit: μm", key="a")
+    b = st.number_input("b; unit: μm", help="b; unit: μm", key="b")
+    w = st.number_input("w; unit: μm", help="w; unit: μm", key="w")
+    LZero = st.number_input("L0; unit: μm", help="L0; unit: μm", key="l_zero")
+    LOne = st.number_input("L1; unit: μm", help="L1; unit: μm", key="l_one")
+    P = st.number_input("P; unit: mN", help="P; unit: mN", key="p")
+
 iframe_src_3d_url = "https://3dwarehouse.sketchup.com/embed/9658ccab-6ac3-4b89-a23f-635206942357"
 image_html_block = "<div class=\"col-lg-6 card my-2 px-3\" style=\"width: max-content;\"> <img src=\"https://hint1412.github.io/XLiu.github.io/SIF/images/Notched_cantilever_sketch.png\" class=\"img-fluid\" alt=\"Stress Intensity Factor Calculator\" /></div>"
 
-test_iframe = st.components.v1.iframe(iframe_src_3d_url, width=800, height = 600, scrolling=False)
-
-test_image = st.components.v1.html(image_html_block, width=800, height=600, scrolling=False)
+st.components.v1.iframe(iframe_src_3d_url, width=800, height = 600, scrolling=False)
+st.components.v1.html(image_html_block, width=800, height=600, scrolling=False)
 
 total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
 num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
