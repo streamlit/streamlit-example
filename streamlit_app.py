e1abd170dc141bf26e518c98b68eed4bd4933e79
Ulysses Chaves de Menezes Netto <33543701+ulysses1212@users.noreply.github.com>
2023-09-18 14:51:02 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,14 @@ df = pd.read_csv("./trabalho_microclimatologia.csv")
 # Título da página
 st.title("Apresentação dos Dados")
 
+# Exiba um texto explicativo antes da tabela de dados
+st.write("Aqui estão os dados do seu arquivo CSV:")
+
 # Exiba a tabela com os dados
 st.write(df)
+
+# Adicione mais texto explicativo após a tabela
+st.write("Agora, vamos realizar algumas transformações nos dados:")
 
 # Calcule as horas e minutos separadamente
 df['horas'] = df['Hora'].astype(int)  # Parte inteira como horas
@@ -20,11 +26,13 @@ df['minutos'] = ((df['Hora'] - df['horas']) * 60).astype(int)  # Parte decimal c
 # Combine as horas e minutos no formato 'hora:minuto'
 df['hora_min'] = df['horas'].astype(str).str.zfill(2) + ':' + df['minutos'].astype(str).str.zfill(2)
 
-# Agora, o DataFrame tem a nova coluna 'hora_min' no formato hora:minuto
-# Por exemplo, 22.75 em 'Hora' será convertido para '22:45' em 'hora_min'
-# Você pode continuar com a análise exploratória de dados conforme necessário
+# Mais transformações nos dados...
+
+# Exiba a tabela de dados transformados
+st.write("Aqui estão os dados transformados:")
 
 # Reorganize as colunas para ter 'hora_min' entre 'Hora' e 'h'
 df = df[['NDA', 'Dia', 'Mes', 'Ano', 'Hora', 'hora_min', 'h', 'Declinacao solar', 'hn', 'N', 'ns', 'ps', 'Zn', '(D/d)2', 'Ih', 'Qg', 'PARi', 'PARi corrigida', 'k', 'Tar', 'IAF', 'IAF.1', 'PARt', 'PARa', 'Assimilacao CO2 (milimol/m2.s)', 'Produçcao Glicose (g Glicose/m2.15min)']]
 
-df
+# Exiba a tabela com os dados transformados
+st.write(df)
