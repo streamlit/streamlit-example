e9e707922ccd36bb39138ac7730988f146a1531b
Ulysses Chaves de Menezes Netto <33543701+ulysses1212@users.noreply.github.com>
2023-09-18 14:46:02 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,19 @@ st.title("Apresentação dos Dados")
 
 # Exiba a tabela com os dados
 st.write(df)
+
+# Calcule as horas e minutos separadamente
+df['horas'] = df['Hora'].astype(int)  # Parte inteira como horas
+df['minutos'] = ((df['Hora'] - df['horas']) * 60).astype(int)  # Parte decimal como minutos
+
+# Combine as horas e minutos no formato 'hora:minuto'
+df['hora_min'] = df['horas'].astype(str).str.zfill(2) + ':' + df['minutos'].astype(str).str.zfill(2)
+
+# Agora, o DataFrame tem a nova coluna 'hora_min' no formato hora:minuto
+# Por exemplo, 22.75 em 'Hora' será convertido para '22:45' em 'hora_min'
+# Você pode continuar com a análise exploratória de dados conforme necessário
+
+# Reorganize as colunas para ter 'hora_min' entre 'Hora' e 'h'
+df = df[['NDA', 'Dia', 'Mes', 'Ano', 'Hora', 'hora_min', 'h', 'Declinacao solar', 'hn', 'N', 'ns', 'ps', 'Zn', '(D/d)2', 'Ih', 'Qg', 'PARi', 'PARi corrigida', 'k', 'Tar', 'IAF', 'IAF.1', 'PARt', 'PARa', 'Assimilacao CO2 (milimol/m2.s)', 'Produçcao Glicose (g Glicose/m2.15min)']]
+
+df
