063f4a4b5e60efc15357297129a4ba19788bcc67
Ulysses Chaves de Menezes Netto <33543701+ulysses1212@users.noreply.github.com>
2023-09-18 15:20:58 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,6 +62,6 @@ st.write("Podemos selecionar apenas as colunas de interesse, antes de chamar o m
 
 st.write(df[['Zn', 'k']].describe().round(2))
 
-
+st.write("Estatísticas Descritivas de uma só variável 'Tar')
 st.write(df['Tar'].describe().round(2))
 
