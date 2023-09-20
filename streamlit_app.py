fe1a63285ded2f69455bcc791785a7fc93c95909
Ulysses Chaves de Menezes Netto <33543701+ulysses1212@users.noreply.github.com>
2023-09-19 21:08:22 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -116,7 +116,7 @@ filtered_df = df[
     (df['NDA'].isin(nda_selected)) &
     (df['Dia'].isin(dia_selected)) &
     (df['Mes'].isin(mes_selected)) &
-    (df['Ano'] == ano_selected) &
+    (df['Ano'].isin(ano_selected)) &
     (df['Hora'].isin(hora_selected))
 ]
 
