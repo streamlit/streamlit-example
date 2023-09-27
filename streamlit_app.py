c081365e1ef0e20ce323f0d3a8384ba13179aa0e
zhiming-dai <108203956+zhiming-dev@users.noreply.github.com>
2023-09-27 15:22:23 -0400

    Optimized Grid and Added Logo

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,9 +13,12 @@ import numpy as np
 # """
 
 
-# Global variable
+# Global variables and settings
 model = None
 record_num = 0
+iframe_src_3d_url = "https://3dwarehouse.sketchup.com/embed/9658ccab-6ac3-4b89-a23f-635206942357"
+
+st.set_page_config(layout="wide")
 
 
 def load_model():
@@ -41,10 +44,24 @@ def emp(a, b, w, L, P):
     sigma = P * L * y / I
     return sigma * np.sqrt(np.pi * a) * F
 
+def runUiSetUp():
+    st.image("./public/web_title.png")
+    st.markdown("""
+    <style>
+            .block-container {
+                margin-top: -2rem;
+                
+            }
+    </style>
+    """, unsafe_allow_html=True)
+
 
 # add sideBar
 with st.sidebar:
-    st.write("**Stress Intensity Factor Calculator (PoC)**")
+    runUiSetUp()
+
+
+    # st.write("**Stress Intensity Factor Calculator (PoC)**")
 
     a = st.number_input("a (μm)", help="a; unit: μm", key="a")
     b = st.number_input("b (μm)", help="b; unit: μm", key="b")
@@ -69,10 +86,11 @@ with st.sidebar:
 
         # TODO: Check parsing json and apply model equation
 
+
+
 st.write("(PoC) Assuming model equation is: a + b + w + LZero + LOne + P. Result is: " )
 st.write(str(a + b + w + LZero + LOne + P))
 
-iframe_src_3d_url = "https://3dwarehouse.sketchup.com/embed/9658ccab-6ac3-4b89-a23f-635206942357"
 
 col1, col2 = st.columns(2, gap="medium")
 with col1:
@@ -83,28 +101,7 @@ with col2:
     st.components.v1.iframe(iframe_src_3d_url, scrolling=False)
 
 
-# image_html_block = "<div class=\"col-lg-6 card my-2 px-3\" style=\"width: max-content;\"> <img src=\"https://hint1412.github.io/XLiu.github.io/SIF/images/Notched_cantilever_sketch.png\" class=\"img-fluid\" alt=\"Stress Intensity Factor Calculator\" /></div>"
-
 # st.components.v1.iframe(iframe_src_3d_url, width=800, height = 600, scrolling=False)
 # st.components.v1.html(image_html_block, width=800, height=600, scrolling=False)
 
 
-total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-Point = namedtuple('Point', 'x y')
-data = []
-
-points_per_turn = total_points / num_turns
-
-for curr_point_num in range(total_points):
-    curr_turn, i = divmod(curr_point_num, points_per_turn)
-    angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-    radius = curr_point_num / total_points
-    x = radius * math.cos(angle)
-    y = radius * math.sin(angle)
-    data.append(Point(x, y))
-
-st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-    .mark_circle(color='#0068c9', opacity=0.5)
-    .encode(x='x:Q', y='y:Q'))
