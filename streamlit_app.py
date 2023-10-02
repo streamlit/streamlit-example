258e5cfe98c041160b4b1096f3559fadeda23c74
Ulysses Chaves de Menezes Netto <33543701+ulysses1212@users.noreply.github.com>
2023-10-02 16:46:29 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,9 @@
-# Exemplo de solicitação de dados meteorológicos da NASA Power
-
 import streamlit as st
 import requests
 from datetime import datetime
 import pandas as pd
 import json
 
-# Chave de API
-API_KEY = "divTM7hRMYjZtqcYXQdhXfWgK3sQj11pGjcF53QS"
 # URL da API do NASAPower
 nasapower_api_url = "https://power.larc.nasa.gov/api/temporal/hourly/point"
 
@@ -18,28 +14,6 @@ def format_date(date_str):
         return date_str
     except ValueError:
         st.error("Por favor, forneça uma data no formato YYYYMMDDHH.")
-        return None
-
-# Crie uma função para fazer solicitações à API
-def get_nasapower_data(start, end, latitude, longitude, community, parameters, format_type):
-    params = {
-        "start": start,
-        "end": end,
-        "latitude": latitude,
-        "longitude": longitude,
-        "community": community,
-        "parameters": parameters,
-        "format": format_type,
-    }
-
-    response = requests.get(nasapower_api_url, params=params)
-
-    if response.status_code == 200:
-        data = response.json()
-        return data
-    else:
-        st.error(f"Erro ao obter dados da API. Código de status: {response.status_code}")
-        st.write(response.text)  # Isso exibirá a resposta da API para diagnóstico
         return None
 
 st.title("Dados Meteorológicos da NASA Power")
@@ -55,27 +29,27 @@ format_type = st.selectbox("Formato de Saída:", ["json", "text/ascii", "text/cs
 
 # Crie um botão para fazer a solicitação à API
 if st.button("Obter Dados"):
-    # Valide e formate as datas antes de fazer a solicitação
     start_date_formatted = format_date(start_date)
     end_date_formatted = format_date(end_date)
 
     if start_date_formatted and end_date_formatted:
-        # Remova os parâmetros inválidos
-        parameters = parameters.replace("SRAD", "")
-        parameters = parameters.replace("VPD", "")
+        params = {
+            "start": start_date_formatted,
+            "end": end_date_formatted,
+            "latitude": latitude,
+            "longitude": longitude,
+            "community": community,
+            "parameters": parameters,
+            "format": format_type,
+        }
+        
+        response = requests.get(nasapower_api_url, params=params)
 
-        data = get_nasapower_data(
-            start_date_formatted,
-            end_date_formatted,
-            latitude,
-            longitude,
-            community,
-            parameters,
-            format_type,
-        )
-
-        if data:
-            # Converter dados JSON em um DataFrame do Pandas para exibi-lo em uma tabela
+        if response.status_code == 200:
+            data = response.json()
             df = pd.json_normalize(data["features"][0]["properties"]["parameter"])
             st.write("Dados Recebidos:")
             st.write(df)
+        else:
+            st.error(f"Erro ao obter dados da API. Código de status: {response.status_code}")
+            st.write(response.text)
