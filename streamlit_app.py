31558dd4893d75e336ba238e115083c831e8487c
ansza <137867309+ansza@users.noreply.github.com>
2023-10-09 11:19:07 +0000

    nambahin data

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,15 @@ import math
 import pandas as pd
 import streamlit as st
 
+
+from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
+
+from google.colab import files
+import io
+
 st.title("Ini punya kelompok FRIK")
+
+df = pd.read_csv("queryresult(2).csv")
+df = df.rename(columns={'sale_order_id': 'OrderID', 'product_default_code': 'Product'})
+df.head()
