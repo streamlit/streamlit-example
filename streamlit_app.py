88fd0f111ce1d41263dd00d2262f796bdc26a6a5
italian <frolovmaxim86@gmail.com>
2023-11-09 10:46:51 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,17 @@
 import io
 import streamlit as st
 from PIL import Image
+import numpy as np
 from tensorflow.keras.applications import EfficientNetB0
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
 
 
-def load_image():
-    """Создание формы для загрузки изображения"""
-    # Форма для загрузки изображения средствами Streamlit
-    uploaded_file = st.file_uploader(
-        label='Выберите изображение для распознавания')
-    if uploaded_file is not None:
-        # Получение загруженного изображения
-        image_data = uploaded_file.getvalue()
-        # Показ загруженного изображения на Web-странице средствами Streamlit
-        st.image(image_data)
-        # Возврат изображения в формате PIL
-        return Image.open(io.BytesIO(image_data))
-    else:
-        return None
-
-# загружает нейронную сеть
-# Декоратор @st.cache говорит Streamlit, что модель нужно загрузить только один раз, чтобы избежать утечек памяти
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model = EfficientNetB0(weights='imagenet')
-    return model
+    return EfficientNetB0(weights='imagenet')
 
-# выполняет предварительную обработку изображения для подготовки к распознаванию
+
 def preprocess_image(img):
     img = img.resize((224, 224))
     x = image.img_to_array(img)
@@ -36,33 +19,31 @@ def preprocess_image(img):
     x = preprocess_input(x)
     return x
 
-# печатает названия и вероятность для ТОП 3 классов, выданных моделью
+
+def load_image():
+    uploaded_file = st.file_uploader(label='Выберите изображение для распознавания')
+    if uploaded_file is not None:
+        image_data = uploaded_file.getvalue()
+        st.image(image_data)
+        return Image.open(io.BytesIO(image_data))
+    else:
+        return None
+
+
 def print_predictions(preds):
     classes = decode_predictions(preds, top=3)[0]
     for cl in classes:
         st.write(cl[1], cl[2])
 
-# Загружаем предварительно обученную модель
+
 model = load_model()
 
-# Выводим заголовок страницы средствами Streamlit     
-st.title('Классификация изображений')
 
-# Вызываем функцию создания формы загрузки изображения
-# Выводим форму загрузки изображения и получаем изображение
+st.title('Новая улучшенная классификации изображений в облаке Streamlit')
 img = load_image()
-
-# Показывам кнопку для запуска распознавания изображения
 result = st.button('Распознать изображение')
-
-# Если кнопка нажата, то запускаем распознавание изображения
 if result:
-    # Предварительная обработка изображения
     x = preprocess_image(img)
-    # Распознавание изображения
     preds = model.predict(x)
-    # Выводим заголовок результатов распознавания жирным шрифтом
-    # используя форматирование Markdown
     st.write('**Результаты распознавания:**')
-    # Выводим результаты распознавания
     print_predictions(preds)
