8c05370a19a1a8e9e7721596082e53010e31b341
italian <frolovmaxim86@gmail.com>
2023-11-09 11:11:04 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import io
 import streamlit as st
 from PIL import Image
 import numpy as np
-import keras.api as keras
+import keras
 from keras.applications import EfficientNetB0
 from keras.preprocessing import image
 from keras.applications.efficientnet import preprocess_input
