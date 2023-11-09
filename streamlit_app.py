21e0511a839c5bf38725e1aa9d13dbccfa71a38c
italian <frolovmaxim86@gmail.com>
2023-11-09 10:43:25 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,48 @@ def load_image():
     else:
         return None
 
+# загружает нейронную сеть
+# Декоратор @st.cache говорит Streamlit, что модель нужно загрузить только один раз, чтобы избежать утечек памяти
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model = EfficientNetB0(weights='imagenet')
+    return model
+
+# выполняет предварительную обработку изображения для подготовки к распознаванию
+def preprocess_image(img):
+    img = img.resize((224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
+
+# печатает названия и вероятность для ТОП 3 классов, выданных моделью
+def print_predictions(preds):
+    classes = decode_predictions(preds, top=3)[0]
+    for cl in classes:
+        st.write(cl[1], cl[2])
+
+# Загружаем предварительно обученную модель
+model = load_model()
+
 # Выводим заголовок страницы средствами Streamlit     
 st.title('Классификация изображений')
+
 # Вызываем функцию создания формы загрузки изображения
+# Выводим форму загрузки изображения и получаем изображение
 img = load_image()
+
+# Показывам кнопку для запуска распознавания изображения
+result = st.button('Распознать изображение')
+
+# Если кнопка нажата, то запускаем распознавание изображения
+if result:
+    # Предварительная обработка изображения
+    x = preprocess_image(img)
+    # Распознавание изображения
+    preds = model.predict(x)
+    # Выводим заголовок результатов распознавания жирным шрифтом
+    # используя форматирование Markdown
+    st.write('**Результаты распознавания:**')
+    # Выводим результаты распознавания
+    print_predictions(preds)
