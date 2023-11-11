2d9b5e0148f56aa51bae2581eafcd63c898466bd
Aniket Nale <62048929+aniketnale41@users.noreply.github.com>
2023-11-11 17:03:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,59 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import soundfile as sf
+from scipy.io.wavfile import write as write_wav
+from transformers import AutoProcessor, BarkModel
 
-"""
-# Welcome to Streamlit!
+processor = AutoProcessor.from_pretrained("suno/bark")
+model = BarkModel.from_pretrained("suno/bark")
+selected_voice_preset = "v2/en_speaker_6" #DEFAULT
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+def text_to_speech(text, voice_preset):
+    # Process the text
+    #OLD inputs = processor(text, voice_preset=voice_preset)
+    inputs = processor(
+        text=[text],
+        return_tensors="pt",
+        voice_preset=voice_preset  
+        )
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+    # Generate speech
+    with st.spinner('Generating speech...'):
+        #OLD speech = model.generate(**inputs)
+        speech = model.generate(
+            input_ids=inputs["input_ids"],
+            attention_mask=inputs["attention_mask"],
+            do_sample=True)   
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+    # Convert the output to an audio array and save it
+    audio_array = speech.cpu().numpy().squeeze()
+    sample_rate = model.generation_config.sample_rate
+    write_wav("output.wav", sample_rate, audio_array)
+    return "output.wav"
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+def run_app(): 
+    st.title('Text to Speech Converter')
+    selected_voice_preset = st.selectbox(
+        'Select voice',
+    ('v2/en_speaker_6', 'v2/hi_speaker_2', 'v2/hi_speaker_3', 'v2/hi_speaker_6'))
+    text = st.text_area("Enter text for speech conversion", height=300, max_chars=500)
+    if st.button('Convert'):
+        if text:
+            wav_file_path = text_to_speech(text,selected_voice_preset)
+            
+            # Read the saved .wav file for playback
+            with open(wav_file_path, "rb") as file:
+                st.audio(file.read(), format='audio/wav')
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+            # Provide a button for the user to download the .wav file
+            with open(wav_file_path, "rb") as file:
+                st.download_button(
+                    label="Download WAV",
+                    data=file,
+                    file_name="output.wav",
+                    mime="audio/wav"
+                )
+        else:
+            st.write("Please enter some text.")
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+if __name__ == "__main__":
+    run_app()
