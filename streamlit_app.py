54235448b856569fa9cc2c1f8347aeed957d3d26
Mark <mvr5109@gmail.com>
2023-11-16 20:58:18 +0000

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,58 @@
 import streamlit as st
 import pandas as pd
 import json
-import os
+
+
+def process_file(uploaded_file):
+    # Read the uploaded file
+    matches = json.load(uploaded_file)
+
+    # Normalize the JSON using pandas
+    data = pd.json_normalize(matches)
+    data.to_json("matches.json")
+
+    # ==================================
+    # Gathering the Data
+    # ==================================
+
+    # We are going to pull apart each type of "like" you can receive on the app from the
+    # messy JSON file that Hinge sends us.
+    # Basically, the logic I'm using is as follows:
+    #
+    # - If "like" is not null, and "match" is not null, then this is where we sent a like and got a match.
+    # - If "like" is not null, and "match" is null, then this is where we sent a like and got no match.
+    # - If "match" is not null, and "like" is null, then this is where we received a like and got a match.
+    # - If "match" is null, and "like" is null, then this is where we redeived a like but did not match.
+
+    # Get data
+    outgoing_matches = data.loc[(data["like"].isna() == False) & (data["match"].isna() == False)].reset_index()
+    outgoing_no_matches = data.loc[(data["like"].isna() == False) & (data["match"].isna() == True)].reset_index()
+    incoming_match = data.loc[(data["match"].isna() == False) & (data["like"].isna() == True)].reset_index()
+    incoming_no_match = data.loc[(data["like"].isna() == True) & (data["match"].isna() == True)].reset_index()
+
+    # Calculate metrics
+    total_likes_received = len(incoming_match) + len(incoming_no_match)
+    total_likes_sent = len(outgoing_matches) + len(outgoing_no_matches)
+    total_matches = len(outgoing_matches) + len(incoming_match)
+    total_paths = total_matches + len(outgoing_no_matches) + len(incoming_no_match)
+
+    # Return the calculated stats
+    return {
+        "incoming_match": len(incoming_match),
+        "incoming_no_match": len(incoming_no_match),
+        "total_likes_received": total_likes_received,
+        "percent_liked_back": len(incoming_match) / total_likes_received * 100 if total_likes_received > 0 else 0,
+        "percent_rejected": len(incoming_no_match) / total_likes_received * 100 if total_likes_received > 0 else 0,
+        "outgoing_matches": len(outgoing_matches),
+        "outgoing_no_matches": len(outgoing_no_matches),
+        "total_likes_sent": total_likes_sent,
+        "percent_they_matched": len(outgoing_matches) / total_likes_sent * 100 if total_likes_sent > 0 else 0,
+        "percent_they_rejected": len(outgoing_no_matches) / total_likes_sent * 100 if total_likes_sent > 0 else 0,
+        "total_matches": total_matches,
+        "total_paths": total_paths,
+        "percent_matches_of_paths": total_matches / total_paths * 100 if total_paths > 0 else 0,
+    }
+
 
 def text_output(stats):
 
@@ -62,7 +113,7 @@ def text_output(stats):
         with col1:
             st.subheader("Item", divider=div_color)
             st.write("Received Like, You Matched")
-            st.write("Revieved Match, You Rejected")
+            st.write("Revieved Like, You Rejected")
             st.markdown("**Total Likes Recieved**")
         
         with col2:
@@ -99,7 +150,6 @@ def text_output(stats):
             st.markdown(f'<span class="number-highlight-nb">{stats["outgoing_matches"] / stats["total_likes_sent"]:.0%}</span>', unsafe_allow_html=True)
             st.markdown(f'<span class="number-highlight-nb">{stats["outgoing_no_matches"] / stats["total_likes_sent"]:.0%}</span>', unsafe_allow_html=True)
 
-
     # GRAND TOTALS
     st.divider()
     st.header(":violet[Grand Totals]")
@@ -122,64 +172,11 @@ def text_output(stats):
             st.markdown(f'<span class="number-highlight-nb">{stats["total_matches"] / stats["total_paths"]:.0%}</span>', unsafe_allow_html=True)
 
 
-def process_file(uploaded_file):
-    # Read the uploaded file
-    matches = json.load(uploaded_file)
-
-    # Normalize the JSON using pandas
-    data = pd.json_normalize(matches)
-    data.to_json("matches.json")
-
-    # ==================================
-    # Gathering the Data
-    # ==================================
-
-    # We are going to pull apart each type of "like" you can receive on the app from the
-    # messy JSON file that Hinge sends us.
-    # Basically, the logic I'm using is as follows:
-    #
-    # - If "like" is not null, and "match" is not null, then this is where we sent a like and got a match.
-    # - If "like" is not null, and "match" is null, then this is where we sent a like and got no match.
-    # - If "match" is not null, and "like" is null, then this is where we received a like and got a match.
-    # - If "match" is null, and "like" is null, then this is where we redeived a like but did not match.
-
-    # Get data
-    outgoing_matches = data.loc[(data["like"].isna() == False) & (data["match"].isna() == False)].reset_index()
-    outgoing_no_matches = data.loc[(data["like"].isna() == False) & (data["match"].isna() == True)].reset_index()
-    incoming_match = data.loc[(data["match"].isna() == False) & (data["like"].isna() == True)].reset_index()
-    incoming_no_match = data.loc[(data["like"].isna() == True) & (data["match"].isna() == True)].reset_index()
-
-    # Calculate metrics
-    total_likes_received = len(incoming_match) + len(incoming_no_match)
-    total_likes_sent = len(outgoing_matches) + len(outgoing_no_matches)
-    total_matches = len(outgoing_matches) + len(incoming_match)
-    total_paths = total_matches + len(outgoing_no_matches) + len(incoming_no_match)
-
-    # Return the calculated stats
-    return {
-        "incoming_match": len(incoming_match),
-        "incoming_no_match": len(incoming_no_match),
-        "total_likes_received": total_likes_received,
-        "percent_liked_back": len(incoming_match) / total_likes_received * 100 if total_likes_received > 0 else 0,
-        "percent_rejected": len(incoming_no_match) / total_likes_received * 100 if total_likes_received > 0 else 0,
-        "outgoing_matches": len(outgoing_matches),
-        "outgoing_no_matches": len(outgoing_no_matches),
-        "total_likes_sent": total_likes_sent,
-        "percent_they_matched": len(outgoing_matches) / total_likes_sent * 100 if total_likes_sent > 0 else 0,
-        "percent_they_rejected": len(outgoing_no_matches) / total_likes_sent * 100 if total_likes_sent > 0 else 0,
-        "total_matches": total_matches,
-        "total_paths": total_paths,
-        "percent_matches_of_paths": total_matches / total_paths * 100 if total_paths > 0 else 0,
-    }
-
-
 def display_stats(stats):
     st.balloons()
     st.success('You did it!', icon="✅")
     st.divider()
     st.header("Step 2: Read your results", divider="grey")
-
-
     text_output(stats)
 
 
