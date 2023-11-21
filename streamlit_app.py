cb948b087ab775ddd87fc18e5ebf43a6fa81af57
JimenaVegaS <89529401+JimenaVegaS@users.noreply.github.com>
2023-11-21 16:09:56 -0500

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,6 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import numpy as np
+from PIL import Image
 
-"""
-# Analisis de Población identificada con DNI de mayor de edad por condición de donante de órganos
-
-Texto sobre la donación de órganos
-"""
-
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+st.title('Análisis de Población identificada con DNI de mayor de edad por condición de donante de órganos')
