258e3cdb642893d39add581f62a1b41e5c014e8a
JimenaVegaS <89529401+JimenaVegaS@users.noreply.github.com>
2023-11-21 17:00:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,6 @@ with pestaña3:
     with st.container():
         left_column, right_column = st.columns(2)
         with left_column:
-            st.button("2022", type="secondary")
+            st.button("2022", type="primary")
 
 st.link_button("Para más información de click aquí", "https://www.datosabiertos.gob.pe/dataset/reniec-poblaci%C3%B3n-identificada-con-dni-de-mayor-de-edad-por-condici%C3%B3n-de-donante-de-%C3%B3rganos")
