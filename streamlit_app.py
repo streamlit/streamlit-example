920ee0bfd34e858ec3597596261a292003f9bc08
JimenaVegaS <89529401+JimenaVegaS@users.noreply.github.com>
2023-11-22 17:31:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,6 +93,6 @@ with pestaña6:
     with st.container:
         left_column, right_column = st.columns(2)
     with left_column:
-        https://github.com/JimenaVegaS/streamlit-example/blob/master/Brigitte.jpeg
+        <span>![</span><span></span><span>]</span><span>(</span><span>https://github.com/JimenaVegaS/streamlit-example/blob/master/Brigitte.jpeg</span><span>)</span>
 
 st.link_button("Para más información de click aquí", "https://www.datosabiertos.gob.pe/dataset/reniec-poblaci%C3%B3n-identificada-con-dni-de-mayor-de-edad-por-condici%C3%B3n-de-donante-de-%C3%B3rganos")
