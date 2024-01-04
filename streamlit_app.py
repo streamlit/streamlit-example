69eeef0093c145e371d2c266ae13fcd5e9376761
TonyZhang <tonyzhang@12345s-MacBook-Air.local>
2024-01-04 12:22:43 +0700

    dd

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,11 +30,3 @@ df = pd.DataFrame({
     "rand": np.random.randn(num_points),
 })
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
