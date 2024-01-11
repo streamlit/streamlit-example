010ec2bf12449e651e2c52e254feb5ebdb8caa96
loonyx8 <66733801+loonyx8@users.noreply.github.com>
2024-01-12 00:05:47 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
-import numpy as np
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title('Explain my test results please!')
 st.header('Instructions')
@@ -10,14 +10,17 @@ st.markdown('Take a picture of your lab test results, upload it, and we will exp
 def load_image():
     uploaded_file = st.file_uploader(label='Upload your test results image below:')
     if uploaded_file is not None:
+        st.success("Image uploaded")  # Toast message
         image_data = uploaded_file.getvalue()
-        st.image(image_data, caption='', width=600)
+        st.image(image_data, caption='', use_column_width=True)  # Adjust width for mobile screens
         return image_data
         
 image = load_image()
 if  image is not None:
     image = np.asarray(Image.open(BytesIO(image)).convert('RGB'))
+    
 if st.button('Process Image'):
+    st.success("HAVEN'T WROTE CODE YET TO PROCESS")  # Toast message
     result = ocr_model.ocr(image)
     texts = [res[1][0] for res in result[0] if len(res[1][0]) > 1]
     result = llm(prompt_template_extract.format(text=",".join(texts)))
