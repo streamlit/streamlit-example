e2ff55457094120463824d977820fdfe11ab83f4
loonyx8 <66733801+loonyx8@users.noreply.github.com>
2024-01-11 22:56:25 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,6 @@
-import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-import streamlit as st
-import pandas as pd
-import numpy as np
 
 st.title('Explain my test results please!')
 st.header('Instructions')
