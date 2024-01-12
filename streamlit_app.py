fbfe353f18d52d5fffe45168d198033b8c316631
loonyx8 <66733801+loonyx8@users.noreply.github.com>
2024-01-13 00:40:56 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import os
+import time
 import base64
 from openai import OpenAI
-#from paddleocr import PaddleOCR
 import cv2
-from paddleocr import PPStructure,draw_structure_result,save_structure_res
+from paddleocr import PaddleOCR
 
 st.title('Explain my test results please!')
 st.header('Instructions')
@@ -15,22 +15,26 @@ st.markdown('Take a picture of your lab test results, upload it, and we will exp
 def load_image():
     uploaded_file = st.file_uploader(label='Upload your test results image below:')
     if uploaded_file is not None:
-        st.success("Image uploaded")  # Toast message
+        st.toast("Image uploaded")  # Toast message
         image_data = uploaded_file.getvalue()
         st.image(image_data, caption='', use_column_width=True)  # Adjust width for mobile screens
         return image_data
+
+ocr_model = PaddleOCR(use_angle_cls=True, lang='en')
         
 image = load_image()
-if  image is not None:
-    image = np.asarray(Image.open(BytesIO(image)).convert('RGB'))
     
 if st.button('Process Image'):
-    st.success("HAVEN'T WROTE CODE YET TO PROCESS")  # Toast message
+    start_time = time.time()
     result = ocr_model.ocr(image)
-    texts = [res[1][0] for res in result[0] if len(res[1][0]) > 1]
-    result = llm(prompt_template_extract.format(text=",".join(texts)))
-    print("result: ", result)
-    result = literal_eval(result)
-    result['drug'] = " ".join(result['drug'].split(" ")[:2])
+    inner_result = result[0]
+    extracted_text = ''
+    for idx in range(len(result)):
+        txt = result[idx][1][0]
+        extracted_text += txt + " "
+    end_time = time.time()
+    extract_time = int(end_time - start_time)
+    st.markdown(extracted_text)
+    st.success(f"Extracted values in {extract_time} seconds.")  # Use status instead of toast/success
     
 st.caption('Disclaimer: Not medical advice, not liable, blah')
