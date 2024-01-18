409ce2bc175931be3eb45ef7ea3dd837c32211d0
Marques Anderson <41755462+MarquesAnderson@users.noreply.github.com>
2024-01-17 22:01:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,29 +13,6 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-num_points = st.text_input("First", placeholder="Enter a number")
-num_points2 = st.text_input("Second", placeholder="Enter a number")
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+num_points = st.text_input("First Number", placeholder="Enter a number")
+num_points2 = st.text_input("Second Number", placeholder="Enter a number")
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
