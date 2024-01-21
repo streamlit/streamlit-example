3886b82e12bcb439da344996f100c7e36a946827
JEEVA <113780413+jeeva-18@users.noreply.github.com>
2024-01-21 12:48:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,8 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+# Movie Rating prediction!
+based on your comments about the movie
 """
 
 num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
