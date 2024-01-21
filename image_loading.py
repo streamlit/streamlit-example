c6794b3bccfe3ca52124cbd019ecf9ae305f9f37
loonyx8 <66733801+loonyx8@users.noreply.github.com>
2024-01-21 16:58:41 +0800

    Update image_loading.py

--- image_loading.py
+++ image_loading.py
@@ -9,7 +9,6 @@ import cv2
 from openai import OpenAI
 from paddleocr import PaddleOCR
 
-
 def load_image():
     uploaded_file = st.file_uploader(label='Upload your test results image below:')
     if uploaded_file is not None:
@@ -19,10 +18,13 @@ def load_image():
         
 
 def extract_text(image,ocr_model):
+    ocr_start_time = time.time()
     result = ocr_model.ocr(image)
-    result = result[0] #idk why this needs a result[0] instead of result for Github
+    #result = result[0] #idk why this needs a result[0] instead of result for Github
     extracted_text = ''
     for idx in range(len(result)):
         txt = result[idx][1][0]
         extracted_text += txt + " "
-    return extracted_text
+    ocr_end_time = time.time()
+    ocr_time = int(ocr_end_time - ocr_start_time)
+    return extracted_text, ocr_time
