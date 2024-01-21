f70324f1186c4427927baf0ca2eec4ee83ad054f
loonyx8 <66733801+loonyx8@users.noreply.github.com>
2024-01-21 14:59:39 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,119 +5,14 @@ import os
 import time
 import base64
 import json
-from openai import OpenAI
 import cv2
+from openai import OpenAI
 from paddleocr import PaddleOCR
-
-# REMOVE THIS BEFORE COPYING TO GITHUB!
-API_KEY = os.environ['API_KEY']
+from image_loading import load_image, extract_text
+from chatgpt_values import extract_values
 
 test_attributes = {}
 
-client = OpenAI(api_key=API_KEY)
-
-template_prompt = """
-Extract the items from the health screening result listed below into a json format, using the example json template. Ignore other items not listed in the json template. Output data types are "test_found" (True/False), "test_value" (float), "test_unit" (string), "test_ref_min" (float), "test_ref_max" (float). If test item is not found, output False for "test_found", and output False for the other values. If test is found but reference max, reference min, or reference range is not found, output False for "test_ref_min" and "test_ref_max". 
-
-Example output json template
-{
-	"ldl_cholesterol": {
-		"test_found":True,
-		"test_value":20,
-		"test_unit":"mmol/l",
-		"test_ref_min":False,
-		"test_ref_max":False
-		},
-	"hdl_cholesterol": {
-		"test_found":True,
-		"test_value":20,
-		"test_unit":"mmol/l",
-		"test_ref_min":False,
-		"test_ref_max":False
-		},
-	"total_cholesterol": {
-		"test_found":True,
-		"test_value":20,
-		"test_unit":"mmol/l",
-		"test_ref_min":False,
-		"test_ref_max":False
-		},
-	"mcv":{
-		"test_found":False,
-		"test_value":False,
-		"test_unit":False,
-		"test_ref_min":False,
-		"test_ref_max":False
-		},
-	"hb":{
-		"test_found":False,
-		"test_value":False,
-		"test_unit":False,
-		"test_ref_min":False,
-		"test_ref_max":False
-		},
-        "rbc_count":{
-		"test_found":False,
-		"test_value":False,
-		"test_unit":False,
-		"test_ref_min":False,
-		"test_ref_max":False
-		},
-	"uric_acid":{
-		"test_found":False,
-		"test_value":False,
-		"test_unit":False,
-		"test_ref_min":False,
-		"test_ref_max":False
-		},
-	"hba1c":{
-		"test_found":False,
-		"test_value":False,
-		"test_unit":False,
-		"test_ref_min":False,
-		"test_ref_max":False
-		},
-	"systolic_bp":{
-		"test_found":False,
-		"test_value":False,
-		"test_unit":False,
-		"test_ref_min":False,
-		"test_ref_max":False
-		},
-	"diastolic_bp":{
-		"test_found":False,
-		"test_value":False,
-		"test_unit":False,
-		"test_ref_min":False,
-		"test_ref_max":False
-		},
-	"height":{
-		"test_found":False,
-		"test_value":False,
-		"test_unit":False,
-		"test_ref_min":False,
-		"test_ref_max":False
-		},
-	"weight":{
-		"test_found":False,
-		"test_value":False,
-		"test_unit":False,
-		"test_ref_min":False,
-		"test_ref_max":False
-		}
-}
-
-Health screening result:
-"""
-
-def load_image():
-    uploaded_file = st.file_uploader(label='Upload your test results image below:')
-    if uploaded_file is not None:
-        st.toast("Image uploaded")  # Toast message
-        image_data = uploaded_file.getvalue()
-        st.image(image_data, caption='', use_column_width=True)  # Adjust width for mobile screens
-        return image_data
-        
 st.title('Explain my test results please!')
 st.header('Instructions')
 st.markdown('Answer the questions, take a picture of your lab test results, upload it, and we will explain it to you!')
@@ -132,8 +27,6 @@ diabetes = st.checkbox("Do you have a history of diabetes?")
 heart_attack = st.checkbox("Have you ever had a heart attack?")
 on_bp_meds = st.checkbox("Are you taking blood pressure medication?")
 systolic_bp = st.number_input("Enter your last recorded systolic blood pressure (leave blank if not available)", min_value=50,max_value=300,value=None)
-
-ocr_model = PaddleOCR(use_angle_cls=True, lang='en')
         
 image = load_image()
     
@@ -154,12 +47,7 @@ if st.button('Analyse my results'):
         st.error("Upload an image of your test results first!",icon="🚨")
     else: # Image uploaded
         ocr_start_time = time.time()
-        result = ocr_model.ocr(image)
-        result = result[0] #idk why this needs a result[0] instead of result for Github
-        extracted_text = ''
-        for idx in range(len(result)):
-            txt = result[idx][1][0]
-            extracted_text += txt + " "
+        extracted_text = extract_text(image)
         ocr_end_time = time.time()
         ocr_time = int(ocr_end_time - ocr_start_time)
         st.markdown(extracted_text)
@@ -169,16 +57,7 @@ if st.button('Analyse my results'):
         # Extract structured data from text using ChatGPT
         # TODO: PUT TRY AND ERROR IF FAIL 
         extract_start_time = time.time()
-        extract_prompt = f"{template_prompt} {extracted_text}"
-        response = client.chat.completions.create(
-            model="gpt-3.5-turbo-1106",
-            response_format={ "type": "json_object" },
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant designed to output JSON."},
-                {"role": "user", "content": extract_prompt}
-            ]
-        )
-        test_results = json.loads(response.choices[0].message.content)
+        response,test_results = extract_values(extracted_text) # use chatgpt to extract
         st.json(test_results)
         st.text(response.usage)
         extract_end_time = time.time()
