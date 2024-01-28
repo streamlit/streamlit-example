eb4a69f5fa5ae03c0b4d0b86ec7c7b456a8ee494
loonyx8 <66733801+loonyx8@users.noreply.github.com>
2024-01-28 17:40:01 +0800

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -34,10 +34,10 @@ def get_dm_advice(inputattributes, inputdict):
     output_phrase = "I didn't catch that."
     lifestyle = False
     #known dm
-    a1c_value = inputdict["hba1c"]["test_value"] if inputdict["hba1c"]["test_found"] else 0 
-    glucose_value = inputdict["glucose"]["test_value"] if inputdict["glucose"]["test_found"] else 0 
+    a1c_value = inputdict["hba1c"]["test_value"]
+    glucose_value = inputdict["glucose"]["test_value"]
     if inputattributes["diabetes"]:
-        if a1c_value >0:
+        if inputdict["hba1c"]["test_found"]:
             if inputattributes["age"] >= 80:
                 a1ctarget = 8
             elif inputattributes["age"] <=40:
@@ -45,33 +45,33 @@ def get_dm_advice(inputattributes, inputdict):
             else:
                 a1ctarget = 7.0 
             if a1c_value > a1ctarget:
-                output_phrase = "your HbA1c is above target, your diabetes can be controlled better. Aim for a HbA1c <7."
+                output_phrase = ":large_orange_circle:  Your HbA1c is above target, your diabetes can be controlled better. Aim for a HbA1c below 7."
                 lifestyle = True
             else:
-                output_phrase = "your HbA1c is within range for a diabetic, <7."
-        if glucose_value >0:
+                output_phrase = ":large_green_circle:  Your HbA1c is below 7, which is within expected range for a diabetic."
+        else:
             if glucose_value > 6:
-                output_phrase += "\nYour fasting glucose level is slightly high. Please consult your doctor for your specific glucose targets. Having a HbA1c measurement may be helpful to better evauate your diabetes control."
+                output_phrase += "  \nYour fasting glucose level is slightly high. Please consult your doctor for your specific glucose targets. Having a HbA1c measurement may be helpful to better evauate your diabetes control."
             else:
-                output_phrase += "\nYour fasting glucose level is within range, <6."
+                output_phrase += "  \nYour fasting glucose level is within range, <6."
     else:
         #diagnose dm
         if a1c_value >= 6.5 or glucose_value >= 7:
-            output_phrase = "You have diabetes. Consult a doctor for advice, you may need to be started on medications."
+            output_phrase = ":large_orange_circle:  You likely have diabetes. Consult a doctor for advice, you may need to be started on medications."
             lifestyle = True
-        elif a1c_value >= 6.1 or glucose_value >=6.1:
-            output_phrase = "You have prediabetes."
+        elif a1c_value >= 5.7 or glucose_value >=5.6:
+            output_phrase = ":large_yellow_circle:  You likely have prediabetes."
             if a1c_value >= 5.7:
-                output_phrase += " Your HbA1c is >6.0." 
+                output_phrase += " Your HbA1c is >5.7." 
             if glucose_value >=5.6:
-                output_phrase += " Your fasting glucose is >6.0. If this was a non-fasting sample, it may be difficult to assess. A fasting sample is preferred."
+                output_phrase += " Your fasting glucose is >5.6. If this was a non-fasting sample, it may be difficult to assess. A fasting sample is preferred."
             lifestyle = True
         else:
-            output_phrase = "You do not have diabetes."
+            output_phrase = ":large_green_circle: You likely do not have diabetes."
     if lifestyle:
         output_phrase += " Eat a healthy balanced diet - using My Healthy Plates (filling a quarter of the plate with wholegrains, quarter with good sources of protein, and half with fruit and vegetables). Maintain a healthy weight BMI ranging from 18.5 to 22.9 kg/m2. Exercise regularly, aiming for 150 minutes of moderate-intensity activity per week or 20 minutes of vigorous-intensity activity 3 or more days a week). Limit alcohol intake to 1 drink per day for females, and 2 drinks per day for males."
     if inputattributes["smoker"]:
-        output_phrase += " Quit smoking."
+        output_phrase += " You are highly encouraged to quit smoking."
     return output_phrase 
 
 #print(f"advice {get_dm_advice(test_attributes, testdict)}")
