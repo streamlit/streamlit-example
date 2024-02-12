278490419edd41e1df09ec5a2e4488de2a3466f3
sablindmitry <40712136+sablindmitry@users.noreply.github.com>
2024-02-12 18:48:51 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,104 @@
-import altair as alt
-import numpy as np
-import pandas as pd
+from enum import Enum
+
 import streamlit as st
+from sentence_transformers import SentenceTransformer, util
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+class EmbeddingModel(Enum):
+    ALL_MINILM_L12_V2 = "all-MiniLM-L12-v2"
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+class SimilarityChecker:
+    def __init__(self,  embedding_model: str = None, relevance_threshold=0.66, similarity_threshold=0.6) -> None:
+        self.model = SentenceTransformer(model_name_or_path=embedding_model)
+        self.relevance_threshold = relevance_threshold
+        self.similarity_threshold = similarity_threshold
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+    def get_embedded_sentence(self, text: str):
+        return self.model.encode(text, convert_to_tensor=True)
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+    def relevance_score(self, question_text: str, answer_text: str) -> bool:
+        question_embedding = self.get_embedded_sentence(question_text)
+        answer_embedding = self.get_embedded_sentence(answer_text)
+        score = util.cos_sim(question_embedding, answer_embedding).item()
+        return score >= self.relevance_threshold
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+    def text_similarity(self, text_one: str, text_two: str) -> bool:
+        text_one_embedding = self.get_embedded_sentence(text_one)
+        text_two_embedding = self.get_embedded_sentence(text_two)
+        score = util.cos_sim(text_one_embedding, text_two_embedding).item()
+        return score >= self.similarity_threshold
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+    def relevance_score_row_vectors_sklearn(self, question: str, answer: str) -> bool:
+        vectorizer = TfidfVectorizer()
+        tfidf_matrix = vectorizer.fit_transform([question, answer])
+        cosine_sim = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])
+        return cosine_sim[0, 0] >= self.relevance_threshold
+
+    def text_similarity_using_row_vectors(self, text_a: str, text_b: str) -> bool:
+        vectorizer = TfidfVectorizer()
+        tfidf_matrix = vectorizer.fit_transform([text_a, text_b])
+        cosine_sim = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])
+        return cosine_sim[0, 0] >= self.similarity_threshold
+
+# Streamlit app layout
+st.title("Suzy Similarity AI")
+option = st.selectbox(
+    'Choose your model',
+    ( EmbeddingModel.ALL_MINILM_L12_V2.value,))
+
+calculation_type_option = st.selectbox(
+    'Choose sklearn calculation or llm embedding model',
+    ("sklearn", "llm"))
+checker = SimilarityChecker(embedding_model=option)
+
+# Question - Answer Similarity
+st.header("Question - Answer Relevance")
+question = st.text_input("Question")
+answer = st.text_input("Answer")
+
+if st.button("Check Relevance"):
+    if calculation_type_option == "llm":
+        if question and answer:
+            is_relevant = checker.relevance_score(question, answer)
+            if is_relevant:
+                st.text("The answer is relevant to the question.")
+            else:
+                st.error("The answer is not relevant to the question.")
+        else:
+            st.text("Please enter both a question and an answer.")
+    else:
+        if question and answer:
+            is_relevant = checker.relevance_score_row_vectors_sklearn(question, answer)
+            if is_relevant:
+                st.text("The answer is relevant to the question.")
+            else:
+                st.error("The answer is not relevant to the question.")
+        else:
+            st.text("Please enter both a question and an answer.")
+# Text Similarity
+
+st.header("Text Similarity")
+text_a = st.text_area("Text A")
+text_b = st.text_area("Text B")
+if st.button("Calculate Similarity Score"):
+    if calculation_type_option == "llm":
+        if question and answer:
+            is_relevant = checker.text_similarity(text_a, text_b)
+            if not is_relevant:
+                st.text("Texts are not similar")
+            else:
+                st.error("Texts seems to be duplicated")
+        else:
+            st.text("Please enter both Text A  and Text B for comparison")
+    else:
+        if question and answer:
+            is_relevant = checker.text_similarity_using_row_vectors(text_a, text_b)
+            if not is_relevant:
+                st.text("Texts are not similar")
+            else:
+                st.error("Texts seems to be duplicated")
+        else:
+            st.text("Please enter both Text A  and Text B for comparison")
